492c5cda54cdb4f1769dcadc5eb0a965d13f572a
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-02-17 20:16:58 +0000

    Must have missed that bug.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@7289 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/fish/aroach/aroach_rev.h
@@ -0,0 +1,6 @@
+#define	VERSION		1
+#define	REVISION        0
+#define	DATE	"24.6.93"
+#define	VERS	"ARoach 1.0"
+#define	VSTRING	"ARoach 1.0 (24.6.93)\n\r"
+#define	VERSTAG	"\0$VER: ARoach 1.0 (24.6.93)"
