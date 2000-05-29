d7cdd823ec28a8d7fb17608e1ee704435ea19a42
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-05-29 00:46:08 +0000

    Needed for AROS build process.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8187 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/bgui/include/bgui_proto_aros.h
@@ -0,0 +1,25 @@
+#ifndef BGUI_PROTO_H
+#define BGUI_PROTO_H
+/*
+ * @(#) $Header$
+ *
+ * $VER: proto/bgui.h 41.10 (20.1.97)
+ * bgui.library prototypes. For use with AROS only.
+ *
+ *
+ *
+ */
+
+#ifndef AROS_SYSTEM_H
+#include <aros/system.h>
+#endif
+
+#include <clib/bgui_protos.h>
+
+#if defined(_AMIGA) && defined(__GNUC__)
+#include <inline/icon.h>
+#else
+#include <defines/icon.h>
+#endif
+
+#endif /* PROTO_BGUI_H */
