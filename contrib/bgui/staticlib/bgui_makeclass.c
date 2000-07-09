516dea3274e2da5b175e9bb6d40e3ef5b70dee61
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2000-07-09 03:05:54 +0000

    Added that file.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@8397 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/bgui/staticlib/bgui_makeclass.c
@@ -0,0 +1,51 @@
+/*
+    Copyright (C) 1995-2000 AROS - The Amiga Research OS
+    $Id$
+
+    Desc:
+    Lang: english
+*/
+
+#define AROS_TAGRETURNTYPE Class *
+#include <utility/tagitem.h>
+#include <proto/alib.h>
+
+/*****************************************************************************
+
+    NAME */
+#define NO_INLINE_STDARG /* turn off inline def */
+#include <proto/bgui.h>
+extern struct Library * BGUIBase;
+
+	Class * BGUI_MakeClass (
+
+/*  SYNOPSIS */
+	Tag tag1,
+	...)
+
+/*  FUNCTION
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+*****************************************************************************/
+{
+    AROS_SLOWSTACKTAGS_PRE(tag1)
+
+    retval = BGUI_MakeClassA(AROS_SLOWSTACKTAGS_ARG(tag1));
+
+    AROS_SLOWSTACKTAGS_POST
+} /* BGUI_NewObject */
