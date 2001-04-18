a472d9b54f4afd5425f41dc45bae9f466bf1cacc
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-18 19:07:38 +0000

    Show totals (as figures and graphically) plus new images which display the sizes since the beginning of AROS.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10640 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- www/www.aros.org/data/status.py
+++ www/www.aros.org/data/status.py
@@ -13,13 +13,24 @@ def gen ():
     body = []
     body.append (Heading (2, 'Status'))
     body.append (Paragraph (
-	'This status was generated %s' % time.strftime ('%d. %B %Y',
-	    time.localtime (time.time ()))
+	'There is a total of %d functions of which %d (%5.2f%%) are finished,'
+	' %d (%5.2f%%) are being worked on and %d (%5.2f%%) are still open.'
+	' This status was generated %s' % (
+	    jobs.jobsTotal, jobs.jobsDone, jobs.status[2],
+	    jobs.jobsInWork, jobs.status[1],
+	    jobs.jobsTodo, jobs.status[0],
+	    time.strftime ('%d. %B %Y', time.localtime (time.time ()))
+	)
     ))
+
+    data = []
+    data.append (('Total',
+	jobs.jobsTotal,
+	jobs.status[2], jobs.status[1], jobs.status[0])
+    )
 
     keys = jobs.jobs.keys ()
     keys.sort ()
-    data = []
     for key in keys:
 	lib = jobs.jobs[key]
 	data.append ((lib.longName,
@@ -30,7 +41,7 @@ def gen ():
     dataList = MyDataList ()
     dataList.segment_names = ('Done', 'In Work', 'Todo')
     dataList.load_tuples (data)
-    dataList.sort ()
+    #dataList.sort ()
     chart = MyStackedBarChart (dataList)
     chart.label_shade = lightBlue
     chart.value_shade = darkBlue
@@ -40,15 +51,19 @@ def gen ():
     body.append (chart)
     body.append (BR ())
     body.append (Heading (2, 'Size of AROS sources'))
-    img = Image ('aros_size.png')
-    page.imagesToFix.append (img)
-    body.append (Paragraph (img))
+
+    body.append (Paragraph (Image ('aros_size.png')))
     body.append (Paragraph (Href ('aros_size.jpg', 'Also as JPEG')))
+    body.append (Paragraph (Image ('aros_allsize.png')))
+    body.append (Paragraph (Href ('aros_allsize.jpg', 'Also as JPEG')))
+
     body.append (Heading (2, 'Size of contributed sources'))
-    img = Image ('contrib_size.png')
-    page.imagesToFix.append (img)
-    body.append (Paragraph (img))
+
+    body.append (Paragraph (Image ('contrib_size.png')))
     body.append (Paragraph (Href ('contrib_size.jpg', 'Also as JPEG')))
+    body.append (Paragraph (Image ('contrib_allsize.png')))
+    body.append (Paragraph (Href ('contrib_allsize.jpg', 'Also as JPEG')))
+
     page.meat = page.meat + body
     page.write ('status.html')
 
