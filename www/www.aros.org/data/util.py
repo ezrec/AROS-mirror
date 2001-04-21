8ac94ed9883bbf1837fb0e673572365bfddcc067
digulla <digulla@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-04-21 19:29:15 +0000

    Use the new paths module
    git-svn-id: https://svn.aros.org/svn/aros/trunk@10713 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- www/www.aros.org/data/util.py
+++ www/www.aros.org/data/util.py
@@ -19,58 +19,7 @@ import GifImagePluginH, JpegImagePluginH, PngImagePluginH
 # Import HTML generator
 from HTMLcolors import *
 from HTMLgen import *
-# Import global config (mostly paths)
-import config
-
-_varprog = None
-
-# Copy of os.path.expandvars
-def expandvars (path, dict=os.environ):
-    """Expand shell variables of form $var and ${var}.  Unknown variables
-are left unchanged"""
-    global _varprog
-    if '$' not in path:
-        return path
-    if not _varprog:
-        import re
-        _varprog = re.compile(r'\$(\w+|\{[^}]*\})')
-    i = 0
-    while 1:
-        m = _varprog.search(path, i)
-        if not m:
-            break
-        i, j = m.span(0)
-        name = m.group(1)
-        if name[:1] == '{' and name[-1:] == '}':
-            name = name[1:-1]
-        if dict.has_key(name):
-            tail = path[j:]
-            path = path[:i] + dict[name]
-            i = len(path)
-            path = path + tail
-        else:
-            i = j
-    return path
-
-def expandpath (path):
-    return os.path.expanduser (expandvars (expandvars (path, config.__dict__)))
-
-# Copy the "interesting" items of the config into local variables.
-htmldir = expandpath (config.HTMLDIR)
-ftpdir = expandpath (config.FTPDIR)
-cvsrootdir = expandpath (config.CVSROOTDIR)
-convert = config.__dict__.get ('CONVERT', None)
-basedir = config.__dict__.get ('BASEDIR', None)
-if not basedir:
-    basedir = os.path.dirname (os.getcwd ())
-basedir = os.path.abspath (basedir)
-datadir = os.path.join (basedir, 'data')
-sys.path.append (datadir)
-
-# Allow to read in python modules from AROS
-arosdir = os.path.abspath (expandpath (config.AROSDIR))
-path = os.path.join (arosdir, 'docs', 'src')
-sys.path.append (path)
+from paths import *
 
 # The relative URL from htmldir to the screenshots dir.
 screenshoturl = 'pics/screenshots'
