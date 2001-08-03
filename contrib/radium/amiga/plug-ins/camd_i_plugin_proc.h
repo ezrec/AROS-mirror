4267abbfe6c680e63c948eabfe1f26c2fda1596b
ksvalast <ksvalast@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-08-03 13:35:13 +0000

    Radium is in my opinion the - without doubt - best note-based music editor available for any plattform. :) It was very easy to make it compile for AROS, but I have no idea if it works, because currently I am not able to compile up aros.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11594 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/radium/amiga/plug-ins/camd_i_plugin_proc.h
@@ -0,0 +1,37 @@
+
+
+#ifndef TRACKER_INCLUDE
+
+void CAMDchangeTrackPan(int newpan,struct Tracks *track);
+void CAMDchangevelocity(int velocity,struct Tracks *track,struct Notes *note);
+void CAMDstopnote(int notenum,int velocity, struct Tracks *track,struct Notes *note);
+void CAMDplaynote(int notenum, int velocity, struct Tracks *track,struct Notes *note);
+void CAMDclosePatch(void);
+
+extern void MyPutMidi(
+	struct MidiLink *midilink,
+	ULONG msg,
+	int maxbuff,
+	int skip
+);
+
+#endif
+
+
+
+
+/***************** General ****************************/
+
+#define PutMidi3(a,b,c,d,s) MyPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)|((d)<<8)),s,0)
+#define PutMidi2(a,b,c,s) MyPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)),s,0)
+
+#define PutMidi3_FX(a,b,c,d,s,skip) MyPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)|((d)<<8)),s,skip)
+#define PutMidi2_FX(a,b,c,s,skip) MyPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)),s,skip)
+
+#define D_PutMidi3(a,b,c,d) GoodPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)|((d)<<8)),1000)
+#define D_PutMidi2(a,b,c) GoodPutMidi(a,(ULONG)(((b)<<24)|((c)<<16)),1000)
+
+#define R_PutMidi3(a,b,c,d) PutMidi(a,(ULONG)(((b)<<24)|((c)<<16)|((d)<<8)))
+#define R_PutMidi2(a,b,c) PutMidi(a,(ULONG)(((b)<<24)|((c)<<16)))
+
+
