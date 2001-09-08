1f2151be0f957d74ae107ba778ddcb7b708bc2e6
falemagn <falemagn@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-08 00:51:05 +0000

    Do not use seek/tell so that it even works with pipes and not seekable streams in general
    git-svn-id: https://svn.aros.org/svn/aros/trunk@11922 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/aminet/util/arc/untar/untar.c
+++ contrib/aminet/util/arc/untar/untar.c
@@ -5,74 +5,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
-#include <proto/dos.h>
+#include <string.h>
 
-struct FileHeader {
-  int  type;
-  long size;
-  char name[100];
-};
-
-FILE *tar,*f;
-long pos;
-struct FileHeader hdr;
-
-void AbsSeek(f,pos)
-FILE *f;
-long pos;
+typedef struct
 {
-  fsetpos(f,&pos);
-}
+    int  type;
+    long size;
+    char name[100];
+} FileHeader;
 
-void RelSeek(f,pos)
-FILE *f;
-long pos;
+
+int CopyFile(FILE *tar, FILE *to, long size)
 {
-  long a;
+    char buf[512];
 
-  fgetpos(f,&a); a+=pos;
-  fsetpos(f,&a);
-}
+    while(size>512)
+    {
+	if
+	(
+	    fread(buf, 512, 1, tar)  == 0 ||
+ 	    fwrite(buf, 512, 1, to) == 0
+	)
+	return 0;
 
-void CopyData(f1,f2,size)
-FILE *f1,*f2;
-long size;
-{
-  char buf[512];
-
-  while(size>512) {
-    fread(buf,512,1,f1);
-    fwrite(buf,512,1,f2);
-    size-=512;
-  }
-  fread(buf,size,1,f1);
-  fwrite(buf,size,1,f2);
-}
-
-long OctalToDec(s)
-char *s;
-{
-  int i;
-  long n=0;
-
-  for(i=0;i<strlen(s);i++)
-    if(s[i]!=' ') {
-      n<<=3; n+=s[i]-'0';
+ 	size-=512;
     }
-  return n;
+
+    if
+    (
+        fread(buf, 512, 1, tar)  == 0 ||
+        fwrite(buf, size, 1, to) == 0
+    )
+    return 0;
+
+    return 1;
 }
 
-void ReadHeader(hdr)
-struct FileHeader *hdr;
+long OctalToDec(char *s)
 {
-  char s[16];
+    int i;
+    long n=0;
 
-  fread(hdr->name,1,100,tar);
-  fread(s,1,8,tar); s[8]=0; hdr->type=OctalToDec(s);
-  RelSeek(tar,16);
-  fread(s,1,12,tar); s[12]=0; hdr->size=OctalToDec(s);
-  RelSeek(tar,376);
-  pos+=512;
+    for(i=0;i<strlen(s);i++)
+    {
+        if(s[i]!=' ')
+	{
+            n<<=3;
+	    n+=s[i]-'0';
+	}
+    }
+
+    return n;
+}
+
+
+int ReadHeader(FILE *tar, FileHeader *hdr)
+{
+    char buf[512];
+    char s[16];
+
+    if (fread(buf, 512, 1, tar) == 0) return 0;
+
+    memcpy(hdr->name, buf, 100);
+
+    buf[100+8] = 0;
+    hdr->type=OctalToDec(buf+100);
+
+    buf[100+8+16+12] = 0;
+    hdr->size=OctalToDec(buf+100+8+16);
+
+    return 1;
 }
 
 int mkdirhier(char *path, int mode)
@@ -112,59 +114,55 @@ int mkdirhier(char *path, int mode)
     return 0;
 }
 
-int main(argc,argv)
-int argc;
-char **argv;
+int main(int argc, char **argv)
 {
+    FILE *tar;
+    FileHeader hdr;
+
     if(argc==1)
     {
-        fputs(stderr, "Required argument missing");
+        puts("Required argument missing");
         exit(20);
     }
 
-    if((tar=fopen(argv[1],"r")))
+    tar = fopen(argv[1], "r");
+    if (!tar)
     {
-        printf("Extracting files from archive: %s\n",argv[1]);
-        pos=0;
-        ReadHeader(&hdr);
-
-	while(ftell(tar)!=EOF && hdr.name[0])
-	{
-	    char *fp = PathPart(hdr.name);
-
-	    if (*fp == '/')
-	    {
-		*fp = '\0';  /* remove trailing '/' */
-        	mkdirhier(hdr.name,0);
-		*fp = '/';   /* put it back */
-      	    }
-
-	    if (fp != hdr.name + strlen(hdr.name) - 1)
-	    {
-	        printf("Extracting (%8d bytes) %s\n",hdr.size,hdr.name);
-	        if((f=fopen(hdr.name,"w")))
-	        {
-                    CopyData(tar,f,hdr.size);
-          	    fclose(f);
-	        }
-	        else
-	        {
-		    perror("Couldn't create file");
-	        }
-	    }
-
-	    pos+=(hdr.size+511) & -512;
-            AbsSeek(tar,pos);
-
-	    ReadHeader(&hdr);
-	 }
-
-	 fclose(tar);
-
-	 exit(0);
-
+        printf("File not found: %s\n",argv[1]);
+        return 20;
     }
-    printf("File not found: %s\n",argv[1]);
 
-    exit(20);
+    printf("Extracting files from archive: %s\n",argv[1]);
+
+    while(ReadHeader(tar, &hdr) && hdr.name[0])
+    {
+	char *fp = rindex(hdr.name, '/');
+
+	if (fp)
+	{
+	    *fp = '\0';  /* remove trailing '/' */
+	    mkdirhier(hdr.name,0);
+	    *fp = '/';   /* put it back */
+        }
+
+	if (!fp || fp[1] != '\0')
+	{
+	    FILE *f;
+
+	    printf("Extracting (%8ld bytes) %s\n",hdr.size,hdr.name);
+	    if((f=fopen(hdr.name,"w")))
+	    {
+                CopyFile(tar, f, hdr.size);
+                fclose(f);
+	    }
+	    else
+	    {
+		perror("Couldn't create file");
+	    }
+	}
+    }
+
+    fclose(tar);
+
+    return 0;
 }
