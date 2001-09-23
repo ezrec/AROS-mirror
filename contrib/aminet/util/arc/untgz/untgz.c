9ae82b761bf94797fdd9e2f969fffefeb876aa77
hkiel <hkiel@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2001-09-23 20:37:09 +0000

    Added a minimal gui. To use it append the keyword "GUI" to the normal call of untgz
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12015 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/aminet/util/arc/untgz/untgz.c
+++ contrib/aminet/util/arc/untgz/untgz.c
@@ -1,12 +1,16 @@
 /* Un-TAR a Tape ARchive file */
 /* By Andrew Church           */
 /* 95ACHURCH@vax.mbhs.edu     */
+/*                            */
+/* GUI added by Henning Kiel  */
+/* hkiel@aros.org             */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <string.h>
 #include <zlib.h>
+#include <sys/stat.h>
 
 typedef struct
 {
@@ -15,6 +19,83 @@ typedef struct
     char name[100];
 } FileHeader;
 
+
+#include <intuition/intuition.h>
+#include <intuition/intuitionbase.h>
+#include <proto/intuition.h>
+#include <proto/graphics.h>
+#include <proto/dos.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+extern struct GfxBase *GfxBase;
+extern struct IntuitionBase *IntuitionBase;
+int archlen = 0;
+int use_gui = 0;
+struct Window *win;
+struct Screen *scr;
+struct RastPort *rp;
+char *title;
+#define G_WIDTH 150
+#define G_HEIGHT 16
+
+void close_gui( )
+{
+    if (scr)   UnlockPubScreen(NULL, scr);
+    if (win)   CloseWindow(win);
+    if (title) free(title);
+}
+
+int open_gui( char *file )
+{
+    char ttitle[256];
+
+    sprintf(ttitle, "Decompressing %s...", file);
+
+    title = strdup(ttitle);
+
+    if (title)
+    {
+        scr = LockPubScreen(NULL);
+
+        if (scr)
+        {
+            win = OpenWindowTags(NULL,
+	        WA_Title, title,
+	        WA_InnerWidth, G_WIDTH,
+	        WA_InnerHeight, G_HEIGHT,
+	        WA_Top, scr->Height/2-G_HEIGHT/2,
+	        WA_Left, scr->Width/2-G_WIDTH/2,
+	        WA_GimmeZeroZero, TRUE,
+	        WA_Activate, TRUE,
+	        WA_DragBar, TRUE,
+	        TAG_END
+            );
+
+	    if (win)
+	    {
+	        rp = win->RPort;
+
+                SetAPen(rp, 3);
+    	        return 1;
+	    }
+        }
+    }
+
+    close_gui();
+
+    return 0;
+}
+void update_gui( int pos )
+{
+    static int oldpos = 0;
+
+    if(pos>oldpos)
+    {
+	RectFill( rp, 0, 0, (pos*G_WIDTH)/archlen, G_HEIGHT );
+	oldpos = pos;
+    }
+}
 
 int CopyFile(gzFile *tgz, FILE *to, long size)
 {
@@ -63,7 +144,6 @@ long OctalToDec(char *s)
 int ReadHeader(gzFile *tgz, FileHeader *hdr)
 {
     char buf[512];
-    char s[16];
 
     if (gzread(tgz, buf, 512) < 0) return 0;
 
@@ -103,7 +183,15 @@ int mkdirhier(char *path, int mode)
 	        return err;
 	}
 	else
-	    printf("Created directory %s\n", path);
+	{
+	    if(use_gui==1)
+	    {
+	    }
+	    else
+	    {
+		printf("Created directory %s\n", path);
+	    }
+	}
 
 	if (putback)
 	{
@@ -117,8 +205,10 @@ int mkdirhier(char *path, int mode)
 
 int main(int argc, char **argv)
 {
+    int fd;
     gzFile *tgz;
     FileHeader hdr;
+    struct stat statbuf;
 
     if(argc==1)
     {
@@ -126,19 +216,46 @@ int main(int argc, char **argv)
         exit(20);
     }
 
-    tgz = gzopen(argv[1], "rb");
-    if (!tgz)
+    fd = open(argv[1], O_RDONLY);
+    if (fd==-1 || !(tgz = gzdopen(fd, "rb")))
     {
-        printf("Couldn't open %s",argv[1]);
+        printf("Couldn't open %s\n",argv[1]);
         return 20;
     }
 
-    printf("Extracting files from archive: %s\n",argv[1]);
+    if(argc==3)
+    {
+	if(strcasecmp("GUI",argv[2])==0)
+	{
+	    use_gui = 1;
+	}
+    }
+
+    /* open gui */
+    if(use_gui==1)
+    {
+        fstat(fd, &statbuf);
+        archlen = statbuf.st_size;
+        if (!open_gui(argv[1]))
+	{
+	    printf("Couldn't open the gui\n");
+	    gzclose(tgz);
+	    return 20;
+	}
+    }
+    else
+    {
+        printf("Extracting files from archive: %s\n",argv[1]);
+    }
 
     while(ReadHeader(tgz, &hdr) && hdr.name[0])
     {
 	char *fp = rindex(hdr.name, '/');
 
+	if(use_gui==1)
+	{
+	    update_gui(lseek(fd,0L,SEEK_CUR));
+	}
 	if (fp)
 	{
 	    *fp = '\0';  /* remove trailing '/' */
@@ -150,7 +267,13 @@ int main(int argc, char **argv)
 	{
 	    FILE *f;
 
-	    printf("Extracting (%8ld bytes) %s\n",hdr.size,hdr.name);
+	    if(use_gui==1)
+	    {
+	    }
+	    else
+	    {
+		printf("Extracting (%8ld bytes) %s\n",hdr.size,hdr.name);
+	    }
 	    if((f=fopen(hdr.name,"wb")))
 	    {
                 CopyFile(tgz, f, hdr.size);
@@ -164,6 +287,11 @@ int main(int argc, char **argv)
     }
 
     gzclose(tgz);
+
+    if(use_gui==1)
+    {
+        close_gui();
+    }
 
     return 0;
 }
