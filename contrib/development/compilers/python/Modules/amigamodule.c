03d35627201fef1e2402d090568c2487e5ca28bc
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-01-10 14:24:32 +0000

    Don't include the docstrings if PYTHON_EXCLUDE_DOCSTRINGS is defined. This results in an (stripped) object file that is ~ 25% (2,5 kB) smaller. Not used yet, though. Yes, I'm edging closer to a tiny Python distribution for a boot disk. ;-)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@12968 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/development/compilers/python/Modules/amigamodule.c
+++ contrib/development/compilers/python/Modules/amigamodule.c
@@ -295,9 +295,13 @@ amiga_do_stat(PyObject *self, PyObject *args, char *format, int (*statfunc)(cons
 
 /* AMIGA methods */
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_chdir__doc__[] =
 "chdir(path) -> None\n\
 Change the current working directory to the specified path.";
+#else
+#define amiga_chdir__doc__ NULL
+#endif
 
 static PyObject *
 amiga_chdir(PyObject *self, PyObject *args)
@@ -305,9 +309,13 @@ amiga_chdir(PyObject *self, PyObject *args)
     return amiga_1str(args, "s:chdir", chdir);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_chmod__doc__[] =
 "chmod(path, mode) -> None\n\
 Change the access permissions of a file.";
+#else
+#define amiga_chmod__doc__ NULL
+#endif 
 
 static PyObject *
 amiga_chmod(PyObject *self, PyObject *args)
@@ -316,9 +324,13 @@ amiga_chmod(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_CHOWN
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_chown__doc__[] =
 "chown(path, uid, gid) -> None\n\
 Change the owner and group id of path to the numeric uid and gid.";
+#else
+#define amiga_chown__doc__ NULL
+#endif
 
 static PyObject *
 amiga_chown(PyObject *self, PyObject *args)
@@ -345,9 +357,13 @@ amiga_chown(PyObject *self, PyObject *args)
 #endif /* HAVE_CHOWN */
 
 #ifdef HAVE_GETCWD
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getcwd__doc__[] =
 "getcwd() -> path\n\
 Return a string representing the current working directory.";
+#else
+#define amiga_getcwd__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getcwd(PyObject *self, PyObject *args)
@@ -366,9 +382,13 @@ amiga_getcwd(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_LINK
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_link__doc__[] =
 "link(src, dst) -> None\n\
 Create a hard link to a file.";
+#else
+#define amiga_link__doc__ NULL
+#endif 
 
 static PyObject *
 amiga_link(PyObject *self, PyObject *args)
@@ -377,6 +397,7 @@ amiga_link(PyObject *self, PyObject *args)
 }
 #endif
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_listdir__doc__[] =
 "listdir(path) -> list_of_strings\n\
 Return a list containing the names of the entries in the directory.\n\
@@ -384,6 +405,9 @@ Return a list containing the names of the entries in the directory.\n\
 	path: path of directory to list\n\
 \n\
 The list is in arbitrary order.";
+#else
+#define amiga_listdir__doc__ NULL
+#endif
 
 static PyObject *
 amiga_listdir(PyObject *self, PyObject *args)
@@ -426,9 +450,13 @@ amiga_listdir(PyObject *self, PyObject *args)
     return amiga_error();
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_mkdir__doc__[] =
 "mkdir(path [, mode=0777]) -> None\n\
 Create a directory.";
+#else
+#define amiga_mkdir__doc__ NULL
+#endif
 
 static PyObject *
 amiga_mkdir(PyObject *self, PyObject *args)
@@ -448,9 +476,13 @@ amiga_mkdir(PyObject *self, PyObject *args)
     Py_INCREF(Py_None); return Py_None;
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_rename__doc__[] =
 "rename(old, new) -> None\n\
 Rename a file or directory.";
+#else
+#define amiga_rename__doc__ NULL
+#endif
 
 static PyObject *
 amiga_rename(PyObject *self, PyObject *args)
@@ -458,9 +490,13 @@ amiga_rename(PyObject *self, PyObject *args)
     return amiga_2str(args, "ss:rename", rename);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_rmdir__doc__[] =
 "rmdir(path) -> None\n\
 Remove a directory.";
+#else
+#define amiga_rmdir__doc__ NULL
+#endif
 
 static PyObject *
 amiga_rmdir(PyObject *self, PyObject *args)
@@ -468,9 +504,13 @@ amiga_rmdir(PyObject *self, PyObject *args)
     return amiga_1str(args, "s:rmdir", rmdir);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_stat__doc__[] =
 "stat(path) -> (mode,ino,dev,nlink,uid,gid,size,atime,mtime,ctime)\n\
 Perform a stat system call on the given path.";
+#else
+#define amiga_stat__doc__ NULL
+#endif
 
 static PyObject *
 amiga_stat(PyObject *self, PyObject *args)
@@ -479,9 +519,13 @@ amiga_stat(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_SYSTEM
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_system__doc__[] =
 "system(command) -> exit_status\n\
 Execute the command (a string) in a subshell.";
+#else
+#define amiga_system__doc__ NULL
+#endif
 
 static PyObject *
 amiga_system(PyObject *self, PyObject *args)
@@ -498,9 +542,13 @@ amiga_system(PyObject *self, PyObject *args)
 #endif
 
 #if defined AMITCP || defined INET225 || defined AROS
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_umask__doc__[] =
 "umask(new_mask) -> old_mask\n\
 Set the current numeric umask and return the previous umask.";
+#else
+#define amiga_umask__doc__
+#endif
 
 static PyObject *
 amiga_umask(PyObject *self, PyObject *args)
@@ -523,9 +571,13 @@ amiga_umask(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_UNAME
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_uname__doc__[] =
 "uname() -> (sysname, nodename, release, version, machine)\n\
 Return a tuple identifying the current operating system.";
+#else
+#define amiga_uname__doc__
+#endif
 
 static PyObject *
 amiga_uname(PyObject *self, PyObject *args)
@@ -548,6 +600,7 @@ amiga_uname(PyObject *self, PyObject *args)
 }
 #endif
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_unlink__doc__[] =
 "unlink(path) -> None\n\
 Remove a file (same as remove(path)).";
@@ -555,6 +608,10 @@ Remove a file (same as remove(path)).";
 static char amiga_remove__doc__[] =
 "remove(path) -> None\n\
 Remove a file (same as unlink(path)).";
+#else
+#define amiga_unlink__doc__ NULL
+#define amiga_remove__doc__ NULL
+#endif
 
 static PyObject *
 amiga_unlink(PyObject *self, PyObject *args)
@@ -563,11 +620,15 @@ amiga_unlink(PyObject *self, PyObject *args)
 }
 
 #if defined AMITCP  || defined INET225 || defined AROS
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_utime__doc__[] =
 "utime(path, (atime, utime)) -> None\n\
 utime(path, None) -> None\n\
 Set the access and modified time of the file to the given values.  If the\n\
 second form is used, set the access and modified times to the current time.";
+#else
+#define amiga_utime__doc__
+#endif
 
 static PyObject *
 amiga_utime(PyObject *self, PyObject *args)
@@ -631,9 +692,13 @@ amiga_utime(PyObject *self, PyObject *args)
 
 
 #ifdef HAVE_GETEGID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getegid__doc__[] =
 "getegid() -> egid\n\
 Return the current process's effective group id.";
+#else
+#define amiga_getegid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getegid(PyObject *self, PyObject *args)
@@ -652,9 +717,13 @@ amiga_getegid(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_GETEUID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_geteuid__doc__[] =
 "geteuid() -> euid\n\
 Return the current process's effective user id.";
+#else
+#define amiga_geteuid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_geteuid(PyObject *self, PyObject *args)
@@ -673,9 +742,13 @@ amiga_geteuid(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_GETGID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getgid__doc__[] =
 "getgid() -> gid\n\
 Return the current process's group id.";
+#else
+#define amiga_getgid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getgid(PyObject *self, PyObject *args)
@@ -693,9 +766,13 @@ amiga_getgid(PyObject *self, PyObject *args)
 }
 #endif
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getpid__doc__[] =
 "getpid() -> pid\n\
 Return the current process id";
+#else
+#define amiga_getpid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getpid(PyObject *self, PyObject *args)
@@ -706,9 +783,13 @@ amiga_getpid(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_GETPGRP
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getpgrp__doc__[] =
 "getpgrp() -> pgrp\n\
 Return the current process group id.";
+#else
+#define amiga_getpgrp__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getpgrp(PyObject *self, PyObject *args)
@@ -731,9 +812,13 @@ amiga_getpgrp(PyObject *self, PyObject *args)
 #endif /* HAVE_GETPGRP */
 
 #ifdef HAVE_SETPGRP
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_setpgrp__doc__[] =
 "setpgrp() -> None\n\
 Make this process a session leader.";
+#else 
+#define amiga_setpgrp__doc__ NULL
+#endif
 
 static PyObject *
 amiga_setpgrp(PyObject *self, PyObject *args)
@@ -760,9 +845,13 @@ amiga_setpgrp(PyObject *self, PyObject *args)
 #endif /* HAVE_SETPGRP */
 
 #ifdef HAVE_GETPPID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getppid__doc__[] =
 "getppid() -> ppid\n\
 Return the parent's process id.";
+#else
+#define amiga_getppid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getppid(PyObject *self, PyObject *args)
@@ -774,9 +863,13 @@ amiga_getppid(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_GETUID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_getuid__doc__[] =
 "getuid() -> uid\n\
 Return the current process's user id.";
+#else
+#define amiga_getuid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_getuid(PyObject *self, PyObject *args)
@@ -795,9 +888,13 @@ amiga_getuid(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_POPEN
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_popen__doc__[] =
 "popen(command [, mode='r' [, bufsize]]) -> pipe\n\
 Open a pipe to/from a command returning a file object.";
+#else 
+#define amiga_popen__doc__ NULL
+#endif
 
 static PyObject *
 amiga_popen(PyObject *self, PyObject *args)
@@ -822,9 +919,13 @@ amiga_popen(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_SETUID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_setuid__doc__[] =
 "setuid(uid) -> None\n\
 Set the current process's user id.";
+#else
+#define amiga_setuid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_setuid(PyObject *self, PyObject *args)
@@ -847,9 +948,13 @@ amiga_setuid(PyObject *self, PyObject *args)
 #endif /* HAVE_SETUID */
 
 #ifdef HAVE_SETGID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_setgid__doc__[] =
 "setgid(gid) -> None\n\
 Set the current process's group id.";
+#else
+#define amiga_setgid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_setgid(PyObject *self, PyObject *args)
@@ -871,9 +976,13 @@ amiga_setgid(PyObject *self, PyObject *args)
 }
 #endif /* HAVE_SETGID */
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_lstat__doc__[] =
 "lstat(path) -> (mode,ino,dev,nlink,uid,gid,size,atime,mtime,ctime)\n\
 Like stat(path), but do not follow symbolic links.";
+#else
+#define amiga_lstat__doc__ NULL
+#endif
 
 static PyObject *
 amiga_lstat(PyObject *self, PyObject *args)
@@ -886,9 +995,13 @@ amiga_lstat(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_READLINK
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_readlink__doc__[] =
 "readlink(path) -> path\n\
 Return a string representing the path to which the symbolic link points.";
+#else
+#define amiga_readlink__doc__ NULL
+#endif
 
 static PyObject *
 amiga_readlink(PyObject *self, PyObject *args)
@@ -908,9 +1021,13 @@ amiga_readlink(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_SYMLINK
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_symlink__doc__[] =
 "symlink(src, dst) -> None\n\
 Create a symbolic link.";
+#else
+#define amiga_symlink__doc__ NULL
+#endif
 
 static PyObject *
 amiga_symlink(PyObject *self, PyObject *args)
@@ -920,9 +1037,13 @@ amiga_symlink(PyObject *self, PyObject *args)
 #endif
 
 #ifdef HAVE_SETSID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_setsid__doc__[] =
 "setsid() -> None\n\
 Call the system call setsid().";
+#else 
+#define amiga_setsid__doc__ NULL
+#endif
 
 static PyObject *
 amiga_setsid(PyObject *self, PyObject *args)
@@ -944,9 +1065,13 @@ amiga_setsid(PyObject *self, PyObject *args)
 #endif /* HAVE_SETSID */
 
 #ifdef HAVE_SETPGID
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_setpgid__doc__[] =
 "setpgid(pid, pgrp) -> None\n\
 Call the system call setpgid().";
+#else
+#define amiga_setpgid__doc__
+#endif
 
 static PyObject *
 amiga_setpgid(PyObject *self, PyObject *args)
@@ -963,9 +1088,13 @@ amiga_setpgid(PyObject *self, PyObject *args)
 
 /* Functions acting on file descriptors */
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_open__doc__[] =
 "open(filename, flag [, mode=0777]) -> fd\n\
 Open a file (for low level IO).";
+#else
+#define amiga_open__doc__ NULL
+#endif
 
 static PyObject *
 amiga_open(PyObject *self, PyObject *args)
@@ -984,9 +1113,13 @@ amiga_open(PyObject *self, PyObject *args)
     return PyInt_FromLong((long)fd);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_close__doc__[] =
 "close(fd) -> None\n\
 Close a file descriptor (for low level IO).";
+#else
+#define amiga_close__doc__ NULL
+#endif
 
 static PyObject *
 amiga_close(PyObject *self, PyObject *args)
@@ -1004,9 +1137,13 @@ amiga_close(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_FTRUNCATE
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_ftruncate__doc__[] =
 "ftruncate(fd, length) -> None\n\
 Truncate a file to a specified length.";
+#else
+#define amiga_ftruncate__doc__ NULL
+#endif
 
 static PyObject *amiga_ftruncate( PyObject *self, PyObject *args )
 {
@@ -1032,9 +1169,13 @@ static PyObject *amiga_ftruncate( PyObject *self, PyObject *args )
 #endif
 
 #if defined AMITCP || defined INET225 || defined AROS
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_dup__doc__[] =
 "dup(fd) -> fd2\n\
 Return a duplicate of a file descriptor.";
+#else
+#define amiga_dup__doc__ NULL
+#endif
 
 static PyObject *
 amiga_dup(PyObject *self, PyObject *args)
@@ -1060,9 +1201,13 @@ amiga_dup(PyObject *self, PyObject *args)
     return PyInt_FromLong((long)fd);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_dup2__doc__[] =
 "dup2(fd, fd2) -> None\n\
 Duplicate file descriptor.";
+#else
+#define amiga_dup2__doc__ NULL
+#endif
 
 static PyObject *
 amiga_dup2(PyObject *self, PyObject *args)
@@ -1090,9 +1235,13 @@ amiga_dup2(PyObject *self, PyObject *args)
 }
 #endif
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_lseek__doc__[] =
 "lseek(fd, pos, how) -> newpos\n\
 Set the current position of a file descriptor.";
+#else
+#define amiga_lseek__doc__ NULL
+#endif
 
 static PyObject *
 amiga_lseek(PyObject *self, PyObject *args)
@@ -1118,9 +1267,13 @@ amiga_lseek(PyObject *self, PyObject *args)
 	return PyInt_FromLong(res);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_read__doc__[] =
 "read(fd, buffersize) -> string\n\
 Read a file descriptor.";
+#else
+#define amiga_read__doc__ NULL
+#endif
 
 static PyObject *
 amiga_read(PyObject *self, PyObject *args)
@@ -1143,9 +1296,13 @@ amiga_read(PyObject *self, PyObject *args)
 	return buffer;
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_write__doc__[] =
 "write(fd, string) -> byteswritten\n\
 Write a string to a file descriptor.";
+#else
+#define amiga_write__doc__ NULL
+#endif
 
 static PyObject *
 amiga_write(PyObject *self, PyObject *args)
@@ -1162,9 +1319,13 @@ amiga_write(PyObject *self, PyObject *args)
 	return PyInt_FromLong((long)size);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_fstat__doc__[]=
 "fstat(fd) -> (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime)\n\
 Like stat(), but for an open file descriptor.";
+#else 
+#define amiga_fstat__doc__ NULL
+#endif
 
 static PyObject *
 amiga_fstat(PyObject *self, PyObject *args)
@@ -1192,9 +1353,13 @@ amiga_fstat(PyObject *self, PyObject *args)
 			(long)st.st_ctime);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_fdopen__doc__[] =
 "fdopen(fd, [, mode='r' [, bufsize]]) -> file_object\n\
 Return an open file object connected to a file descriptor.";
+#else
+#define amiga_fdopen__doc__ NULL
+#endif
 
 static PyObject *
 amiga_fdopen(PyObject *self, PyObject *args)
@@ -1240,9 +1405,13 @@ static int pipe(int *fildes)
 	return -1;
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_pipe__doc__[] =
 "pipe() -> (read_end, write_end)\n\
 Create a pipe.";
+#else
+#define amiga_pipe__doc__ NULL
+#endif
 
 static PyObject *
 amiga_pipe(PyObject *self, PyObject *args)
@@ -1287,9 +1456,13 @@ amiga_fullpath(PyObject *self, PyObject *args)
 	else return PyString_FromString(buf);
 }
 
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_putenv__doc__[] =
 "putenv(key, value) -> None\n\
 Change or add an environment variable.";
+#else
+#define amiga_putenv__doc__ NULL
+#endif
 
 static PyObject *amiga_putenv(PyObject *self, PyObject *args)
 {
@@ -1305,9 +1478,13 @@ static PyObject *amiga_putenv(PyObject *self, PyObject *args)
 }
 
 #ifdef HAVE_STRERROR
+#ifndef PYTHON_EXCLUDE_DOCSTRINGS
 static char amiga_strerror__doc__[] =
 "strerror(code) -> string\n\
 Translate an error code to a message string.";
+#else
+#define amiga_strerror__doc__ NULL
+#endif
 
 PyObject *
 amiga_strerror(PyObject *self, PyObject *args)
