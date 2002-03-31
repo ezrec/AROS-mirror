2fdbbecafa4e4ed6e7dfb13b0def4f9792fd3a71
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-03-31 17:12:02 +0000

    Splitted list.c into list.c and listview.c
    git-svn-id: https://svn.aros.org/svn/aros/trunk@13907 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/mui/libs/muimaster/classes/list.c
+++ contrib/mui/libs/muimaster/classes/list.c
@@ -13,72 +13,7 @@
 #include "muimaster_intern.h"
 #include "support.h"
 
-#ifdef _AROS
-
 extern struct Library *MUIMasterBase;
-
-/**************** Listview ***************/
-
-struct MUI_ListviewData
-{
-    int dummy;
-};
-
-/**************************************************************************
- OM_NEW
-**************************************************************************/
-static IPTR Listview_New(struct IClass *cl, Object *obj, struct opSet *msg)
-{
-    struct MUI_ListviewData   *data;
-    struct TagItem  	    *tag, *tags;
-    
-    obj = (Object *)DoSuperMethodA(cl, obj, (Msg)msg);
-    if (!obj) return FALSE;
-    
-    data = INST_DATA(cl, obj);
-
-    /* parse initial taglist */
-
-    for (tags = msg->ops_AttrList; (tag = NextTagItem(&tags)); )
-    {
-	switch (tag->ti_Tag)
-	{
-    	}
-    }
-    
-    return (IPTR)obj;
-}
-
-#ifndef _AROS
-__asm IPTR Listview_Dispatcher( register __a0 struct IClass *cl, register __a2 Object *obj, register __a1 Msg msg)
-#else
-AROS_UFH3S(IPTR,Listview_Dispatcher,
-	AROS_UFHA(Class  *, cl,  A0),
-	AROS_UFHA(Object *, obj, A2),
-	AROS_UFHA(Msg     , msg, A1))
-#endif
-{
-    switch (msg->MethodID)
-    {
-	case OM_NEW:
-	    return Listview_New(cl, obj, (struct opSet *)msg);
-	    
-    }
-    
-    return DoSuperMethodA(cl, obj, msg);
-}
-
-/*
- * Class descriptor.
- */
-const struct __MUIBuiltinClass _MUI_Listview_desc = { 
-    MUIC_Listview, 
-    MUIC_Group, 
-    sizeof(struct MUI_ListviewData), 
-    (void*)Listview_Dispatcher 
-};
-
-/**************** List ******************/
 
 struct MUI_ListData
 {
@@ -139,4 +74,3 @@ const struct __MUIBuiltinClass _MUI_List_desc = {
     (void*)List_Dispatcher 
 };
 
-#endif
