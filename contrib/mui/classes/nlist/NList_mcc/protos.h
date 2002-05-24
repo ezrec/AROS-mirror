abd7ae1f8ca4ee6d6f05a1790f8c814e70403a3b
stegerg <stegerg@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-05-24 14:26:33 +0000

    AROS clib has stpcpy, so no stpcpy prototype.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@14651 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/mui/classes/nlist/NList_mcc/protos.h
+++ contrib/mui/classes/nlist/NList_mcc/protos.h
@@ -155,7 +155,7 @@ extern ULONG MyCallHookPkt(Object *obj,BOOL hdata,struct Hook *hook,APTR object,
 extern ULONG MyCallHookPktA(Object *obj,struct Hook *hook,APTR message,...);
 extern LONG DeadKeyConvert(struct NLData *data,struct IntuiMessage *msg,UBYTE *buf,LONG bufsize,struct KeyMap *kmap);
 extern char *ltoa(ULONG val, char *buffer, int len);
-#ifndef __SASC
+#if !defined(__SASC) && !defined(_AROS)
 extern char *stpcpy(char *to,char *from);
 #endif
 //$$$Sensei: new memory handling functions.
