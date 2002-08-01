17d469f5dc034290513406f53adfd88980d3d528
sheutlin <sheutlin@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-08-01 17:02:50 +0000

    AROS specific debugging
    git-svn-id: https://svn.aros.org/svn/aros/trunk@15076 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/aminet/disk/cdrom/AmiCDROM/src/debug.h
+++ contrib/aminet/disk/cdrom/AmiCDROM/src/debug.h
@@ -4,16 +4,28 @@
 #define NDEBUG
 //#define DEBUG_SECTORS
 
-#ifdef NDEBUG
-#	define BUG(x) /* nothing */
+#ifdef _AROS
+#	undef DEBUG
+#	define DEBUG 0
+#	include <aros/debug.h>
+#	if DEBUG>0
+#		define BUG(x) x
+#		define dbprintf kprintf
+#	else
+#		define BUG(x)
+#	endif
+#	define BUG2(x)
 #else
-#	define BUG(x) x
-#endif
-
-#if !defined(NDEBUG) || defined(DEBUG_SECTORS)
-#	define BUG2(x) x
-#else
-#	define BUG2(x) /* nothing */
+#	ifdef NDEBUG
+#		define BUG(x) /* nothing */
+#	else
+#		define BUG(x) x
+#	endif
+#	if !defined(NDEBUG) || defined(DEBUG_SECTORS)
+#		define BUG2(x) x
+#	else
+#		define BUG2(x) /* nothing */
+#	endif
 #endif
 
 #endif /* ACDR_DEBUG_H */
