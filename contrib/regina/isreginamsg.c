2dac8ea01fe2ec54b92771a31eceff56217637b0
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2002-12-24 21:10:52 +0000

    Moved isreginamsg from aros_funcs to here. Adapted source code for the new build_module.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@16130 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/regina/isreginamsg.c
@@ -0,0 +1,60 @@
+/*
+    (C) 2001 AROS - The Amiga Research OS
+    $Id$
+*/
+
+#include <exec/types.h>
+#include <rexx/storage.h>
+#include <proto/rexxsyslib.h>
+
+#include "rexx.h"
+
+/*****************************************************************************
+
+    NAME */
+
+BOOL IsReginaMsg (struct RexxMsg *msg)
+
+/*  FUNCTION
+        Check to see if message was generated from the regina interpreter.
+        This function will look at the rm_Private1 and rm_Private2 fields
+        to see if they are valid.
+
+    INPUTS
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+*****************************************************************************/
+{
+    tsd_node_t *node;
+    BOOL ok;
+    struct Library *RexxSysBase;
+    
+    RexxSysBase = OpenLibrary("rexxsyslib.library", 0);
+    if (RexxSysBase == NULL) return FALSE;
+    ok = IsRexxMsg(msg);
+    CloseLibrary(RexxSysBase);
+    if (!ok) return FALSE;
+
+    ok = FALSE;
+    ForeachNode(__regina_tsdlist, node)
+    {
+	if (node->TSD == (tsd_t *)msg->rm_Private2)
+	{
+	    ok = TRUE;
+	    break;
+	}
+    }
+
+    return ok;
+}
