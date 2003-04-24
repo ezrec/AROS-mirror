93cfa2213f6d138772f5766d8859afd97b05cdf6
bergers <bergers@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-04-24 01:03:22 +0000

    47.1 instead of 45.1.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@17308 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/aminet/dev/gui/gtlayout/Source/gtlayout.library_rev.h
+++ contrib/aminet/dev/gui/gtlayout/Source/gtlayout.library_rev.h
@@ -1,6 +1,6 @@
-#define	VERSION		45
+#define	VERSION		47
 #define	REVISION	1
 #define	DATE	"11.7.98"
-#define	VERS	"gtlayout.library 45.1"
-#define	VSTRING	"gtlayout.library 45.1 (11.7.98)\r\n"
-#define	VERSTAG	"\0$VER: gtlayout.library 45.1 (11.7.98)"
+#define	VERS	"gtlayout.library 47.1"
+#define	VSTRING	"gtlayout.library 47.1 (11.7.98)\r\n"
+#define	VERSTAG	"\0$VER: gtlayout.library 47.1 (11.7.98)"
