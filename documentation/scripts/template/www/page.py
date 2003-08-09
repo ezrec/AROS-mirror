585e519aca73e84ad4bb0555bd7cacbbe19877b0
chodorowski <chodorowski@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-08-09 14:56:07 +0000

    Removed forums link in menu.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19156 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/scripts/template/www/page.py
+++ documentation/scripts/template/www/page.py
@@ -59,7 +59,7 @@ def makePage( _T, _N, _M, MIRRORS_DATA ):
         Tree \
         ( [
             A( _N['mailing-lists'], href='%(BASE)scontact.php#mailing-lists' ),
-            A( _N['forums'], href='%(BASE)scontact.php#forums' ),
+            #A( _N['forums'], href='%(BASE)scontact.php#forums' ),
             A( _N['irc-channels'], href='%(BASE)scontact.php#irc-channels' )
         ] ),
         BR(),
