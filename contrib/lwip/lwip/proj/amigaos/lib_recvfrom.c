568a40bb79f4c83fc7cd7a336af674902dd6afce
sebauer <sebauer@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2003-09-02 20:09:59 +0000

    Implemented recvfrom
    git-svn-id: https://svn.aros.org/svn/aros/trunk@19581 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/lwip/lwip/proj/amigaos/lib_recvfrom.c
+++ contrib/lwip/lwip/proj/amigaos/lib_recvfrom.c
@@ -5,15 +5,26 @@
     $Id: lib_recvfrom.c,v 1.1 2002/07/11 17:59:24 sebauer Exp $
 */
 
+/* We redefine the timeval here, because lwip defines an own timeval struct,
+   should be changed in the future */
+#define timeval timeval_amiga
+
 #include <exec/types.h>
+#include <proto/exec.h>
 
 #include "socket_intern.h"
-#include "calling.h"
+
+#undef timeval
+
+#include "lwip/sys.h"
+#include "lwip/sockets.h"
+
+/* #define MYDEBUG */
+#include "debug.h"
 
 /*****************************************************************************
 
     NAME */
-
 #ifndef __AROS__
 __asm int LIB_recvfrom(register __d0 long s, register __a0 unsigned char *buf, register __d1 long len, register __d2 long flags, register __a1 struct sockaddr *addr, register __a2 long *addrlen)
 #else
@@ -26,6 +37,7 @@ __asm int LIB_recvfrom(register __d0 long s, register __a0 unsigned char *buf, r
 	AROS_LHA(long, flags, D2),
 	AROS_LHA(struct sockaddr *, addr, A1),
 	AROS_LHA(long *, addrlen, A2),
+
 /*  LOCATION */
 	struct Library *, SocketBase, 72, Socket)
 #endif
@@ -51,11 +63,24 @@ __asm int LIB_recvfrom(register __d0 long s, register __a0 unsigned char *buf, r
 
 *****************************************************************************/
 {
+	  int rc;
+	  void *ud;
+	  struct Task *t;
+
     AROS_LIBFUNC_INIT
     AROS_LIBBASE_EXT_DECL(struct Library *,SocketBase)
 
-//    return CallStackFunction(SOCKB(SocketBase), LIBMSG_SOCKET, 6, s, buf, len, flags, addr, addrlen);
-	return -1;
+    t = FindTask(NULL);
+    ud = t->tc_UserData;
+    /* set user data (used by sys_arch functions) */
+    t->tc_UserData = SOCKB(SocketBase)->data;
+
+    rc = lwip_recvfrom(s,buf,len,flags,addr,(int*)addrlen);
+
+    D(bug("recvfrom()=%ld\n",rc));
+    /* restore old user data */
+    t->tc_UserData = ud;
+    return rc;
 
     AROS_LIBFUNC_EXIT
 
