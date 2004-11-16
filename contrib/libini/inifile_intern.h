f380f7a0b13b37d07fb7982abb7f389f14966e89
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2004-11-16 21:57:51 +0000

    Do not use libcore/base.h anymore in this library. We don't need an internal libbase struct anymore.
    Use autoinit to initialize global variables
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@22495 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/libini/inifile_intern.h
+++ contrib/libini/inifile_intern.h
@@ -14,20 +14,7 @@
 #include <aros/asmcall.h>
 #include <dos/dos.h>
 #include <utility/utility.h>
-#include <libcore/base.h>
 
 extern struct ExecBase *SysBase;
-
-struct inifileBase_intern
-{
-    struct Library    library;
-};
-
-/****************************************************************************************/
-
-#undef INB
-#define INB(b)	((struct inifileBase_intern *)(b))
-
-/****************************************************************************************/
 
 #endif /* INIFILE_INTERN_H */
