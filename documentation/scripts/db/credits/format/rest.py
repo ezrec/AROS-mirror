224f7f772801ab532d84b05f407f2d656b2d6944
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-01-21 11:27:55 +0000

    Script adds a comment to the generated file to prevent it from beeing manually modified
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24000 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/scripts/db/credits/format/rest.py
+++ documentation/scripts/db/credits/format/rest.py
@@ -1,11 +1,12 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright © 2002, The AROS Development Team. All rights reserved.
+# Copyright © 2002-2006, The AROS Development Team. All rights reserved.
 # $Id$
 
 def format( credits ):
     result  = '=======\n' \
             + 'Credits\n' \
-            + '======='
+            + '=======\n\n' \
+            + ".. This document is automatically generated from db/credits."
               
     for area in credits:
         result += '\n\n' + area[0] + '\n' + '=' * (len( area[0] ) + 3) + '\n\n' 
