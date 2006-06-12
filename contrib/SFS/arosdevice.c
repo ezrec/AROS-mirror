f5621271178df4ebe28c4f3cb96cd13047b08ee1
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-06-12 20:36:08 +0000

    SFS handler is now built with %build_module. Changed arosdevice.c to use the symbolsets for module initialization.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24538 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/SFS/arosdevice.c
+++ contrib/SFS/arosdevice.c
@@ -1,6 +1,6 @@
 #include <proto/exec.h>
 #include <aros/libcall.h>
-#include <aros/asmcall.h>
+#include <aros/symbolsets.h>
 #include <dos/filesystem.h>
 #include <exec/errors.h>
 #include <exec/memory.h>
@@ -10,81 +10,18 @@
 // #include "cdrom.h"
 //#include "devsupp.h"
 
-extern const char asfsname[];
-extern const char asfsversion[];
-extern const APTR asfsinittab[4];
-static struct ASFSBase *AROS_SLIB_ENTRY(init,asfsdev)();
-static void AROS_SLIB_ENTRY(open,asfsdev)();
-static BPTR AROS_SLIB_ENTRY(close,asfsdev)();
-static BPTR AROS_SLIB_ENTRY(expunge,asfsdev)();
-static int AROS_SLIB_ENTRY(null,asfsdev)();
-static void AROS_SLIB_ENTRY(beginio,asfsdev)();
-static LONG AROS_SLIB_ENTRY(abortio,asfsdev)();
-extern const char asfshandlerend;
-static AROS_LP2(struct ASFSBase *, init,
-        AROS_LPA(struct ASFSBase *, asfsbase, D0),
-        AROS_LPA(BPTR,             segList, A0),
-        struct ExecBase *, SysBase, 0, asfsdev);
+#include LC_LIBDEFS_FILE
+
 void ASFS_work(struct ASFSBase *);
 void *ASFS_GetData(struct ASFSBase *);
 
-int ASFS_entry(void)
+AROS_SET_LIBFUNC(GM_UNIQUENAME(Init), LIBBASETYPE, asfsbase)
 {
-        /* If the device was executed by accident return error code. */
-        return -1;
-}
-
-const struct Resident ASFS_resident=
-{
-        RTC_MATCHWORD,
-        (struct Resident *)&ASFS_resident,
-        (APTR)&asfshandlerend,
-        RTF_COLDSTART | RTF_AFTERDOS | RTF_AUTOINIT,
-        41,
-        NT_DEVICE,
-        -122,
-        (char *)asfsname,
-        (char *)&asfsversion[6],
-        (ULONG *)asfsinittab
-};
-
-const char asfsname[]="sfs.handler";
-const char asfsversion[]="$VER: sfs 1.84 (30.11.2005)\r\n";
-
-static const UBYTE asfsdatatable = 0;
-
-static void *const asfsfunctable[]=
-{
-        &AROS_SLIB_ENTRY(open,asfsdev),
-        &AROS_SLIB_ENTRY(close,asfsdev),
-        &AROS_SLIB_ENTRY(expunge,asfsdev),
-        &AROS_SLIB_ENTRY(null,asfsdev),
-        &AROS_SLIB_ENTRY(beginio,asfsdev),
-        &AROS_SLIB_ENTRY(abortio,asfsdev),
-        (void *)-1
-};
-
-const APTR asfsinittab[4]=
-{
-        (APTR)sizeof(struct ASFSBase),
-        (APTR)asfsfunctable,
-        (APTR)&asfsdatatable,
-        &AROS_SLIB_ENTRY(init,asfsdev)
-};
-
-static
-AROS_LH2(struct ASFSBase *, init,
- AROS_LHA(struct ASFSBase *, asfsbase, D0),
- AROS_LHA(BPTR,             segList, A0),
-      struct ExecBase *, SysBase, 0, asfsdev)
-{
-        AROS_LIBFUNC_INIT
+        AROS_SET_LIBFUNC_INIT
 
         struct Task *task;
         APTR stack;
 
-        asfsbase->seglist = segList;
-        asfsbase->SysBase = SysBase;
         asfsbase->DOSBase = (struct DOSBase *)OpenLibrary("dos.library",41);
         if (asfsbase->DOSBase)
         {
@@ -128,15 +65,15 @@ AROS_LH2(struct ASFSBase *, init,
                                 NEWLIST(&asfsbase->process_list);
                                 asfsbase->GetData=ASFS_GetData;
                                 if (NewAddTask(task,ASFS_work,NULL,tags) != NULL)
-                                        return asfsbase;
+                                        return TRUE;
                                 FreeMem(stack, AROS_STACKSIZE);
                         }
                                     FreeMem(task, sizeof(struct Task));
                 }
                 CloseLibrary((struct Library *)asfsbase->DOSBase);
         }
-        return NULL;
-        AROS_LIBFUNC_EXIT
+        return FALSE;
+        AROS_SET_LIBFUNC_EXIT
 }
 
 #ifdef SysBase
@@ -144,17 +81,17 @@ AROS_LH2(struct ASFSBase *, init,
 #endif
 #define SysBase asfsbase->SysBase
 
-static
-AROS_LH3(void, open,
- AROS_LHA(struct IOFileSys *, iofs, A1),
- AROS_LHA(ULONG,              unitnum, D0),
- AROS_LHA(ULONG,              flags, D1),
-           struct ASFSBase *, asfsbase, 1,asfsdev)
+
+AROS_SET_OPENDEVFUNC(GM_UNIQUENAME(Open),
+		     LIBBASETYPE, asfsbase,
+		     struct IOFileSys, iofs,
+		     unitnum,
+		     flags
+)
 {
-        AROS_LIBFUNC_INIT
+        AROS_SET_DEVFUNC_INIT
 
         unitnum = flags = 0;
-        asfsbase->device.dd_Library.lib_OpenCnt++;
         asfsbase->rport.mp_SigTask=FindTask(NULL);
 
         iofs->IOFS.io_Command = -1;
@@ -164,13 +101,12 @@ AROS_LH3(void, open,
         if (iofs->io_DosError == NULL)
         {
                 iofs->IOFS.io_Device = &asfsbase->device;
-                asfsbase->device.dd_Library.lib_Flags &= ~LIBF_DELEXP;
                 iofs->IOFS.io_Error = 0;
-                return;
+                return TRUE;
         }
-        asfsbase->device.dd_Library.lib_OpenCnt--;
         iofs->IOFS.io_Error = IOERR_OPENFAIL;
-        AROS_LIBFUNC_EXIT
+        return FALSE;
+        AROS_SET_DEVFUNC_EXIT
 }
 
 #ifdef DOSBase
@@ -178,40 +114,25 @@ AROS_LH3(void, open,
 #endif
 #define DOSBase asfsbase->DOSBase
 
-static
-AROS_LH0(BPTR, expunge, struct ASFSBase *, asfsbase, 3, asfsdev)
+AROS_SET_LIBFUNC(GM_UNIQUENAME(Expunge), LIBBASETYPE, asfsbase)
 {
-        AROS_LIBFUNC_INIT
+        AROS_SET_LIBFUNC_INIT
 
-        BPTR retval;
-
-        if (asfsbase->device.dd_Library.lib_OpenCnt) {
-                asfsbase->device.dd_Library.lib_Flags |= LIBF_DELEXP;
-                return 0;
-        }
         RemTask(asfsbase->port.mp_SigTask);
         FreeMem(((struct Task *)asfsbase->port.mp_SigTask)->tc_SPLower,AROS_STACKSIZE);
         FreeMem(asfsbase->port.mp_SigTask, sizeof(struct Task));
         CloseLibrary((struct Library *)DOSBase);
-        Remove(&asfsbase->device.dd_Library.lib_Node);
-        retval=asfsbase->seglist;
-        FreeMem
-        (
-                (char *)asfsbase-asfsbase->device.dd_Library.lib_NegSize,
-                        asfsbase->device.dd_Library.lib_NegSize+
-                        asfsbase->device.dd_Library.lib_PosSize
-        );
-        return retval;
+        return TRUE;
 
-        AROS_LIBFUNC_EXIT
+        AROS_SET_LIBFUNC_EXIT
 }
 
-static
-AROS_LH1(BPTR, close,
- AROS_LHA(struct IOFileSys *, iofs, A1),
-      struct ASFSBase *, asfsbase, 2, asfsdev)
+AROS_SET_CLOSEDEVFUNC(GM_UNIQUENAME(Close),
+		      LIBBASETYPE, asfsbase,
+		      struct IOFileSys, iofs
+)
 {
-        AROS_LIBFUNC_INIT
+        AROS_SET_DEVFUNC_INIT
 
         asfsbase->rport.mp_SigTask=FindTask(NULL);
         iofs->IOFS.io_Command = -2;
@@ -219,30 +140,17 @@ AROS_LH1(BPTR, close,
         WaitPort(&asfsbase->rport);
         (void)GetMsg(&asfsbase->rport);
         if (iofs->io_DosError)
-                return 0;                               // there is still something to do on this volume
+                return FALSE;                               // there is still something to do on this volume
         iofs->IOFS.io_Device=(struct Device *)-1;
-        if (!--asfsbase->device.dd_Library.lib_OpenCnt)
-        {
-                /* Delayed expunge pending? */
-                if (asfsbase->device.dd_Library.lib_Flags & LIBF_DELEXP)
-                {
-                        /* Then expunge the device */
-                        return expunge();
-                }
-        }
-        return 0;
-        AROS_LIBFUNC_EXIT
+        return TRUE;
+        AROS_SET_DEVFUNC_EXIT
 }
 
-static
-AROS_LH0I(int, null, struct asfsbase *, asfsbase, 4, asfsdev)
-{
-        AROS_LIBFUNC_INIT
-        return 0;
-        AROS_LIBFUNC_EXIT
-}
+ADD2INITLIB(GM_UNIQUENAME(Init),0) 
+ADD2OPENDEV(GM_UNIQUENAME(Open),0) 
+ADD2CLOSEDEV(GM_UNIQUENAME(Close),0) 
+ADD2EXPUNGELIB(GM_UNIQUENAME(Expunge),0) 
 
-static
 AROS_LH1(void, beginio,
  AROS_LHA(struct IOFileSys *, iofs, A1),
            struct ASFSBase *, asfsbase, 5, asfsdev)
@@ -257,7 +165,6 @@ AROS_LH1(void, beginio,
         AROS_LIBFUNC_EXIT
 }
 
-static
 AROS_LH1(LONG, abortio,
  AROS_LHA(struct IOFileSys *, iofs, A1),
            struct ASFSBase *, asfsbase, 6, asfsdev)
@@ -266,5 +173,3 @@ AROS_LH1(LONG, abortio,
         return 0;
         AROS_LIBFUNC_EXIT
 }
-
-const char asfshandlerend = 0;
