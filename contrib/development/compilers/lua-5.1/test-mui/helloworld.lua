22cca5a40a9890fbb807eb60e329f79229cbaf5e
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-06-29 22:41:23 +0000

    Simple example for Lua-Mui
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24565 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/development/compilers/lua-5.1/test-mui/helloworld.lua
@@ -0,0 +1,32 @@
+require "muidefs"
+require "muifuncs"
+
+function creategui()
+  local button = mui.SimpleButton("_Ok")
+  local text = mui.TextObject(mui.MUIA_Text_Contents, "\27cHello world!\nHow are you?")
+  local window = mui.WindowObject(
+    mui.MUIA_Window_Title, "Hello world!",
+    mui.MUIA_Window_RootObject, mui.VGroup(
+      mui.Child, text,
+      mui.Child, button
+    )
+  )
+  app = mui.ApplicationObject(
+    mui.MUIA_Application_Window, window
+  )
+  assert(app, "Cant create applicaton")
+  window:domethod(mui.MUIM_Notify, mui.MUIA_Window_CloseRequest, true,
+    app, 2, mui.MUIM_Application_ReturnID, mui.MUIV_Application_ReturnID_Quit)
+  button:domethod(mui.MUIM_Notify, mui.MUIA_Pressed, false,
+    app, 2, mui.MUIM_Application_ReturnID, mui.MUIV_Application_ReturnID_Quit)
+  window:set(mui.MUIA_Window_Open, true)
+end
+
+function main()
+  creategui()
+  app:domethod(mui.MUIM_Application_Execute)
+end
+
+_, err = pcall(main)
+if err then print("Error: " .. err) end
+if app then app:dispose() end
