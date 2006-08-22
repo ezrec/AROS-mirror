40b3ec0ba009fd4b68cfec7bcaa088bf93771891
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-22 23:45:24 +0000

    Added docutils 0.4, patched to support our --target-suffix argument. This upgrade is necessary to support the Russian translation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24659 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/scripts/docutils/parsers/rst/directives/references.py
@@ -0,0 +1,27 @@
+# Author: David Goodger, Dmitry Jemerov
+# Contact: goodger@users.sourceforge.net
+# Revision: $Revision$
+# Date: $Date$
+# Copyright: This module has been placed in the public domain.
+
+"""
+Directives for references and targets.
+"""
+
+__docformat__ = 'reStructuredText'
+
+from docutils import nodes
+from docutils.transforms import references
+from docutils.parsers.rst import directives
+
+
+def target_notes(name, arguments, options, content, lineno,
+                 content_offset, block_text, state, state_machine):
+    """Target footnote generation."""
+    pending = nodes.pending(references.TargetNotes)
+    pending.details.update(options)
+    state_machine.document.note_pending(pending)
+    nodelist = [pending]
+    return nodelist
+
+target_notes.options = {'class': directives.class_option}
