b83793963e9daa98c13763d7070d6cb8b278cfd9
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-27 07:17:35 +0000

    first version of this file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24674 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/index.pl
@@ -0,0 +1,10 @@
+
+.. Include:: introduction/index-abstract.pl
+
+`Czytaj wiêcej... <introduction/index>`__
+
+.. Include:: documentation/developers/contribute-abstract.pl
+
+`Czytaj wiêcej... <documentation/developers/contribute>`__
+
+.. Include:: news/index.pl
