bd83969454a6aa5bbfa47c8c23936713b6f1d666
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-28 17:40:48 +0000

    translated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24688 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/developers/contribute-abstract.pl
@@ -0,0 +1,14 @@
+Potrzebujemy Twojej pomocy!
+===========================
+
+Mamy niewielu aktywnych programistów, co niestety oznacza powolny postêp.
+Potrzebujemy wiêcej osób do pomocy! Jest olbrzymia liczba zadañ, które wymagaj±
+przypisanych do nich programistów. Zakres tych zadañ jest ró¿ny, od ma³ych 
+projektów po du¿e, od zadañ dotycz±cych sprzêtu po systemy wysokiego poziomu 
+i tworzenie aplikacji. W zasadzie dla ka¿dego chêtnego do pomocy znajdzie siê 
+odpowiednia praca, niezale¿nie od jego bieg³o¶ci w programowaniu!
+
+Dla tych z Was, którzy nie s± programistami jest tak¿e sporo zadañ, w których mo¿ecie
+pomóc! Pisanie dokumentacji, t³umaczenie programów i dokumentacji na inne jêzyki, 
+tworzenie ³adnej grafiki i polowanie na b³êdy. 
+Te zadania s± tak samo wa¿ne jak programowanie!
