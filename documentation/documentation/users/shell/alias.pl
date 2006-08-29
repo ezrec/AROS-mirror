faa8719dbe8df79ac7f0b78bb0de5694a0dc5e90
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-29 09:06:09 +0000

    new file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24697 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/users/shell/alias.pl
@@ -0,0 +1,81 @@
+.. This document is automatically generated. Don't edit it!
+
+`Index <index>`_ `Prev <addbuffers>`_ `Next <ask>`_ 
+
+---------------
+
+
+=====
+Alias
+=====
+
+Sk³adnia
+~~~~~~~~
+::
+
+
+	NAME,STRING/F
+
+
+¦cie¿ka
+~~~~~~~
+::
+
+
+	Workbench:c
+
+
+Funkcja
+~~~~~~~
+::
+
+
+     Alias umo¿liwia Ci stworzenie alternatywnej nazwy dla innych komend DOSa.
+     Je¶li Alias zostanie u¿yty bez parametrów, wy¶wietla aktualn± listê
+     aliasów zdefiniowanych w aktywnym oknie shella.
+
+     U¿ycie pary kwadratowych nawiasów wewn¹trz aliasu umo¿liwia Ci
+     dodaæ 'now±' komendê dosa z parametrami.
+
+     Je¶li nie zostan¹ wyspecyfikowane parametry, zostanie wy¶wietlona aktualna lista aliasów.
+     
+
+
+Parametry
+~~~~~~~~~
+::
+
+
+	NAME    - Nazwa do przypisania.
+
+	STRING  - Warto¶æ dla NAME.
+
+
+Rezultat
+~~~~~~~~
+::
+
+
+	Standardowe kody b³êdów DOS.
+
+
+Przyk³ad
+~~~~~~~~
+::
+
+
+	Alias DF "Type [] numer"
+
+	Wpisuj±c "DF S:Shell-Startup" w konsoli, wykonujesz komende
+	"Type S:Shell-Startup numer". To dzia³anie wy¶wietli zawarto¶æ
+	pliku S:Shell-Startup w konsoli z liczb± linii po lewej stronie.
+
+     
+Zobacz tak¿e
+~~~~~~~~~~~~
+::
+
+
+     Unalias
+
+
