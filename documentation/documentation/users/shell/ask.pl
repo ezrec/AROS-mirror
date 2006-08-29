7097bd2e7251cd178eaadcd96ed0566c7e9cb314
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-29 09:06:50 +0000

    new file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24698 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/users/shell/ask.pl
@@ -0,0 +1,52 @@
+.. This document is automatically generated. Don't edit it!
+
+`Index <index>`_ `Prev <alias>`_ `Next <assign>`_ 
+
+---------------
+
+
+===
+Ask
+===
+
+Sk³adnia
+~~~~~~~~
+::
+
+
+	PROMPT/A
+
+
+¦cie¿ka
+~~~~~~~
+::
+
+
+	Workbench:C
+
+
+Funkcja
+~~~~~~~~
+::
+
+	Oczekuje od u¿ytkownika na wej¶cie. Mo¿liwymi wej¶ciamu s±
+	y dla yes (tak) i n lub Return dla no (nie). Wybieraj±c y
+	zwracany jest kod b³êdu 5.
+
+
+Parametry
+~~~~~~~~~
+::
+
+
+	PROMPT	--	ci±g jest wy¶wietlany w oknie
+
+
+Zobacz tak¿e
+~~~~~~~~~~~~
+::
+
+     
+	RequestChoice
+     
+
