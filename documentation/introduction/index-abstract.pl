5e766ccff73dd66ee410404c924f507fd7704860
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-08-29 08:31:44 +0000

    corrected encoding
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24694 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/introduction/index-abstract.pl
+++ documentation/introduction/index-abstract.pl
@@ -1,6 +1,6 @@
 Wprowadzenie
 ============
 
-AROS jest przenoœnym i ogólnodostêpnym systemem operacyjnym, posiadaj¹cym graficzny interfejs u¿ytkownika, 
-d¹¿¹cym do kompatybilnoœci z AmigaOS 3.1, jednoczeœnie przewy¿szaj¹cym go w wielu obszarach. Kod Ÿród³owy 
-jest dostêpny pod otwart¹ licencj¹, która pozwala go, dowolnie modyfikowaæ.
+AROS jest przeno¶nym i ogólnodostêpnym systemem operacyjnym, posiadaj±cym graficzny interfejs u¿ytkownika, 
+d±¿±cym do kompatybilno¶ci z AmigaOS 3.1, jednocze¶nie przewy¿szaj±cym go w wielu obszarach. Kod ¼ród³owy 
+jest dostêpny pod otwart± licencj±, która pozwala go, dowolnie modyfikowaæ.
