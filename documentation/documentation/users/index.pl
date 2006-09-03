39af6ddb834ccd28fec01389312908411396d96e
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-03 10:54:58 +0000

    new file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24735 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/users/index.pl
@@ -0,0 +1,13 @@
+===============
+Dla u¿ytkownika
+===============
+
++ `Jak zainstalowaæ`__
++ `Jak u¿ywaæ`__
++ `Komendy Shella`__
++ `Najczê¶ciej zadawane pytania (FAQ)`__
+
+__ installation  
+__ using  
+__ shell/index
+__ faq  
