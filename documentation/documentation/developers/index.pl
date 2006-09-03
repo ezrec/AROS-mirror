660b143996e22d317101998a3ccf3875966f093f
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-03 10:50:57 +0000

    new file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24734 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/developers/index.pl
@@ -0,0 +1,32 @@
+================
+Dla programistów
+================
+
++ `Jak do³±czyæ`__
++ `SVN - dostêp i u¿ywanie`__
++ `Jak kompilowaæ`__
++ `Podrêcznik programowania`__
++ `Zune - podrêcznik programowania`__
++ `System development manual`__
++ `PCI subsystem manual`__
++ `Debugging manual`__
++ `Reference`__
++ `Specyfikacje`__
++ `Documenting`__
++ `Summaries`__
++ `Przydatne linki`__
+
+__ contribute
+__ svn
+__ compiling
+__ app-dev/index
+__ zune-application-development
+__ system-development
+__ hardware/pci
+__ debugging
+__ autodocs/index
+__ specifications/index
+__ documenting
+__ summaries/index
+__ links
+
