f35d470a02e18b9be6a047354b880e55e864231c
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-09 08:50:40 +0000

    new file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24759 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/pictures/screenshots/20060901/AROSTCPConfig-Profile.pl
@@ -0,0 +1,3 @@
+Postêp prac nad koncepcyjnym GUI dla narzêdzia konfiguracyjnego TCPIP (pracuje
+nad nim Kalamatee). Strona profilu pozwala wybraæ/importowaæ/eksportowaæ
+kompletny profil/konfiguracjê Pozwala tak¿e ustawiæ zmienne Env dotycz±ce AROSTCP.
