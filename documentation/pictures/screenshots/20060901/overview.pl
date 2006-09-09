65a8d0871e6191feebb9955c01574cb1133c3cd2
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-09 08:28:54 +0000

    encoding corrected
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24756 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/pictures/screenshots/20060901/overview.pl
+++ documentation/pictures/screenshots/20060901/overview.pl
@@ -1,4 +1,4 @@
 2006-09-01
 ----------
 
-Poni¿ej kilka prac z roku 2006. AROS przedstawia wspania³y program rysunkowy Hogne "M0ns00n" Titlestad'a, Lunapaint v0.2.3. Rosyjska lokalizacja AROS-a. Postêp prac nad koncepcyjnym GUI dla konfiguratora TCPIP i MesaGL (pracuje nad nimi Kalamatee). ZapowiedŸ klienta FTP z GUI nad którym pracuje Suppah (aka voidstar).
+Poni¿ej kilka prac z roku 2006. AROS przedstawia wspania³y program rysunkowy Hogne "M0ns00n" Titlestad'a, Lunapaint v0.2.3. Rosyjska lokalizacja AROS-a. Postêp prac nad koncepcyjnym GUI dla konfiguratora TCPIP i MesaGL (pracuje nad nimi Kalamatee). Zapowied¼ klienta FTP z GUI nad którym pracuje Suppah (aka voidstar).
