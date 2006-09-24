36e03ee04dcc46ea658913849fbdbfc73acc9fe1
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-24 13:38:37 +0000

    translated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24824 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/news/data/20060915.pl
@@ -0,0 +1,59 @@
+=============
+Status update
+=============
+
+:Autor:   Paolo Besser
+:Data:     2006-09-15
+
+T³umaczenie Aros.org trwa. Samuel Atlan pracuje nad t³umaczeniem na jêzyk
+`francuski`__ . Przygotowanie ca³o¶ci zajmie trochê czasu, ale pierwsze strony
+s± ju¿ dostêpne z menu po lewej stronie. Nale¿y odnotowaæ tak¿e now± cechê
+aros.org: przet³umaczone strony mo¿na wczytaæ dodaj±c swój narodowy prefiks
+do standardowego adresu url. Np. mo¿na otworzyæ w³osk± stronê g³ówn± przy
+pomocy *www.aros.org/it*, rosyjsk± *www.aros.org/ru* itd.
+
+__ http://www.aros.org/fr/
+
+AROS w newsach
+--------------
+
+Przeczytajcie proszê sympatyczn± `recenzjê`__ Dmitara Butrovskiego, któr±
+opublikowa³ na `OSnews.com`__. To jeden z bardziej kompletnych artyku³ów
+AROS-a, opisuj±cy wszystkie jego w³a¶ciwo¶ci i wszystkie strony naszego
+sympatycznego systemu operacyjnego. Je¶li jeszcze nie wiesz czym jest AROS, 
+to lektura tego artyku³u mo¿e okazaæ siê dobrym pomys³em.
+
+__ http://osnews.com/story.php?news_id=15819
+__ http://osnews.com
+
+Oprogramowanie
+--------------
+
+Nowa wersja 1.20 znanego emulatora 8-bitowych komputerów Commodore *VICE* 
+jest dostêpna dla wszystkich platform amigowych, w tym AROS-a. Mo¿esz j± 
+znale¼æ `tutaj`__.
+
+*WinAros* to preinstalowane ¶rodowisko AROS-a w postaci obrazu dysku,
+kompatybilne ze znanymi maszynami wirtualnymi QEMU i VirtualPC Microsoftu,
+obu bezp³atnie dostêpnymi w sieci. Mo¿esz pobraæ `QEMU Winaros tutaj`__
+i `VirtualPC Winaros tutaj`__. Heinz-Raphael Reinke przygotowa³ kompletn±
+instrukcjê `AROS installation on hard disk`__ w formacie PDF. Je¶li wolisz, 
+jest tak¿e `wersja niemiecka`__. Potrzebujesz Adobe Acrobat Reader, FoxIt 
+Reader lub aPDF/xPDF by j± przeczytaæ. 
+
+*Installation Kit for AROS (IKAROS)* to zestaw wirtualnych obrazów dysków
+ró¿nych emulatorów jak QEMU i VMware, podzielonych ju¿ na partycje,
+sformatowanych, i gotowych do instalacji AROS-a. Co wa¿ne archiwum ma
+niewielki rozmiar, gdy¿ nie zawiera wielu plików. Szybka instalacja
+¶wie¿ych wersji AROS-a, mo¿e byæ przydatna dla celów testowych nightly
+builds. Pakiet umo¿liwia ³atw± instalacjê nowych wersji bez potrzeby 
+partycjonowania. Do³±czono instrukcjê instalacji.
+Proszê sprawdzaj `Aros-Exec`__ w poszukiwaniu informacji o uaktualnieniach.
+
+
+__ http://www.viceteam.org/amigaos.html
+__ http://amidevcpp.amiga-world.de/WinAros/WinAros_Light_QEMU.zip
+__ http://amidevcpp.amiga-world.de/WinAros/WinAros_Light_VPC.zip
+__ http://amidevcpp.amiga-world.de/WinAros/Aros_HD_Install_English.pdf
+__ http://amidevcpp.amiga-world.de/WinAros/Aros_HD_Installation.pdf
+__ http://archives.aros-exec.org/?function=showfile&file=emulation/misc/arosik02.zip
