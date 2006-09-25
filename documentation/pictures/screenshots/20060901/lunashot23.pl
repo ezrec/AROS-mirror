9a677ce1af9111b16c937c8b535df362e7c04469
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-09-25 22:29:07 +0000

    Added the missing properties, split up 1161-column lines etc.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@24830 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/pictures/screenshots/20060901/lunashot23.pl
+++ documentation/pictures/screenshots/20060901/lunashot23.pl
@@ -1,2 +1,2 @@
-Wspania³y program rysunkowy "M0ns00n" Titlestad'a, Lunapaint v0.2.3 wersja robocza.
-Pe³na obs³uga datatypów... i wiele innych zmian.
+Wspania³y program rysunkowy "M0ns00n" Titlestad'a, Lunapaint v0.2.3
+wersja robocza. Pe³na obs³uga datatypów... i wiele innych zmian.
