a352b4a41be089de9d36550092719822b1971518
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2006-12-16 18:55:51 +0000

    corrected
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25087 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/news/data/20061210.pl
+++ documentation/news/data/20061210.pl
@@ -3,13 +3,13 @@ Status update
 =============
 
 :Autor:   Paolo Besser
-:Date:     2006-12-10
+:Data:     2006-12-10
 
 Ostatni Status Update ma ju¿ 3 miesi±ce, lecz to nie znaczy, ¿e nic nie
 robili¶my. Wykonano sporo poprawek i usprawnieñ w wielu komponentach 
 AROSa.
 
-Nightly builds dla wersji PPC-hosted AROSa jest ju¿ dostêpna na naszej 
+Nightly build dla wersji PPC-hosted AROSa jest ju¿ dostêpne na naszej 
 stronie `Download`__.
 
 Christoph Szczecina doda³ do Wanderera inicjaln± wersjê kopiowania 
@@ -21,13 +21,13 @@ dodali zapamiêtywanie pozycji okna i wsparcie dla listowania ikon.
 
 Staf Verhaegen zaktualizowa³ dokumentacjê dla programistów. Wiele rzeczy 
 zmieni³o siê w ci±gu minionych 24 miesiêcy i nasze strony z dokumentacj± 
-by³y ju¿ trochê nieaktualne. Dziêki Staf'owi, nowi programi¶ci AROSa 
+by³y ju¿ trochê nieaktualne. Dziêki Stafowi, nowi programi¶ci AROSa 
 mogê znale¼æ na tej stronie przydatne informacje.
 
 Dziêki pracy Bernda Roescha i Georga Stegera, wszystkie wa¿ne w³a¶ciwo¶ci 
 niezbêdne do zast±pienia MUI w wielu amigowych programach zosta³y dodane 
 do Zune. Micha³ Schulz doda³ wsparcie dla zapisu do datatypu PNG, czyni±c
-³atwiejszym tworzenie ikon i edycjê obrazów.
+³atwiejszymi tworzenie ikon i edycjê obrazów.
 
 __ http://aros.sourceforge.net/download.php
 
@@ -51,13 +51,13 @@ Oprogramowanie
 developerskiej w formacie AutoDoc zosta³ przeniesiony na AROSa. 
 Marcin Kielesiñski portowa³ *AmiGG* 0.44.3. To pierwszy komunikator dla AROSa 
 umo¿liwiaj±cy prowadzenie rozmów z u¿ytkownikami sieci Gadu-Gadu i Tlen.
-AmiGG umo¿liwia ponadto otrzymywanie o przychodz±cej poczcie e-mail. Mo¿esz 
-obejrzeæ AmiGG na 2 zrzutach ekranu: `tutaj`__ i `tutaj`__, a binaria znajdziesz 
-`tutaj`__.
+AmiGG umo¿liwia ponadto otrzymywanie powiadomieñ o przychodz±cej poczcie e-mail. 
+Mo¿esz obejrzeæ AmiGG na 2 zrzutach ekranu: `tutaj`__ i `tutaj`__, a binaria 
+znajdziesz `tutaj`__.
 
 *Murks!* to zintegrowane ¶rodowisko programistyczne dla AROSa. Murks! u¿ywa 
 tego samego formatu zapisu projektów co AmiDevCpp, jest wiêc mo¿liwa 
-miêdzy nimi wymiana danych. By u¿yæ Murks!, potrzebujesz natywnej wersji GCC; 
+miêdzy nimi wymiana danych. By u¿yæ Murks!, potrzebujesz natywnej wersji GCC. 
 U¿ytkownicy Windows mog± wypróbowaæ Murks! u¿ywaj±c ostatniej wersji WinAros 
 zawieraj±cej natywne GCC. Pe³na historia na `AROS-EXEC`__.
 
