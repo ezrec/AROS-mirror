a9725cb02c50213887a4b3b27eb43e425230220b
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-01-07 13:09:26 +0000

    translated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25142 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/news/data/20061223.pl
@@ -0,0 +1,22 @@
+======================
+Aktualno¶ci ¶wi±teczne
+======================
+
+:Autor:   Paolo Besser
+:Data:     2006-12-23
+
+Weso³ych ¦wi±t Bo¿ego Narodzenia i Szczê¶liwego Nowego Roku! To by³ 
+wa¿ny rok w rozwoju AROSa i chcemy podziêkowaæ wszystkim fanom 
+AROSa, testerom i wspó³pracownikom za wy¶mienite wyniki, jakie  
+osi±gn±³ nasz system operacyjny.
+
+W oczekiwaniu na pierwsze wie¶ci z 2007 roku dlaczego nie pos³uchaæ 
+kolêd z AROSAmp, pierwszego odtwarzacza mp3 z graficznym interfejsem 
+u¿ytkownika dla AROSa? Mo¿esz go znale¼æ `tutaj`__
+
+A je¶li chcesz obejrzeæ swoje stare zdjêcia cyfrowe, mo¿e zainteresujesz 
+siê `przegl±dark± Paw³a Stefanskiego`__ dla Morphosa, który zosta³ 
+ostatnio portowany na AROSa. 
+
+__ http://archives.aros-exec.org/index.php?function=browse&cat=audio
+__ http://archives.aros-exec.org/index.php?function=browse&cat=graphics/viewer
