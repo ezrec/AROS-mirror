6f05dbc7db0a066ddd6fc43103fb20cb0b3b067c
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-03-02 12:25:29 +0000

    translated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25313 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/news/data/20070206.pl
@@ -0,0 +1,56 @@
+=============
+Status Update
+=============
+
+:Autor:   Paolo Besser
+:Data:     2007-02-06
+
+Rozpocz±³ siê nowy rok rozwoju AROS-a i prawdopodobnie nie móg³ siê rozpocz±æ 
+lepiej. S± dwie du¿e nowo¶ci dla wszystkich u¿ytkowników AROS-a.
+
+Pierwsza, ¿e dzia³a funkcja drag & drop! To oznacza, ¿e mo¿na u¿yæ Wanderera
+zarówno do kopiowania plików z jednego katalogu do drugiego jak i otwieraæ pliki  
+upuszczaj±c je nad ikon± programu. Ten "cud" stworzyli Christoph Szczecina 
+i Darius Brewka. Oto `krótki film`__ pokazuj±cy dzia³anie drag & drop w AROS-ie!
+
+Problemy ze stabilno¶ci± wersji natywnej s± aktualnie ¶ledzone i powinny 
+byæ rozwi±zane w najbli¿szych dniach. 
+
+Robert Norris ukoñczy³ pracê na sieci± w wersji linux hosted. 
+Interfejs Tap pracuje i mo¿na go znale¼æ w ostatnich nightly builds.
+
+William Ouwehand stworzy³ `holendersk± wersjê Aros.org`__.
+Zosta³a te¿ dodana `Instrukcja kompilacji AROS-a`__ dla Ubuntu Linux, 
+któr± przygotowa³ Christoph Szczecina.
+
+Nic Andrews przygotowa³ inicjaln± wersjê driverów dla kart sieciowych
+RTL 8139 i VIA Rhine. Nadal s± w trakcie tworzenia, lecz prawdopodobnie
+ich ukoñczenie nie zajmie du¿o czasu. TeamAROS utworzy³ bounty dla sterownika
+VIA Rhine.
+
+Informacje o nowych archiwach
+-----------------------------
+
+Z wa¿niejszych Ostatnio dodanych archiwów nale¿y wymieniæ: 
+
+- `Lunapaint`__ jest coraz lepszy! Dostêpna jest wersja 0.3.1, 
+  zawieraj±ca kilka nowych narzêdzi i nowych cech.
+
+- `Martin's Server Suite`__, port dla AROS-a i innych systemów operacyjnych
+  serwerów Snug HTTP server i Yet Another FTP Server.
+  Mo¿esz obejrzeæ `zrzut ekranu`__ pokazuj¹cy serwery pracuj±ce w systemie AROS. 
+
+- `Jhead`__ jest komend± do obróbki tagów Exif, "niegraficznej" czê¶ci plików JPEG,
+  które tworzy wiêkszo¶æ aparatów cyfrowych.
+
+- `Potrace`__ to narzêdzie do tracingu obrazów rastrowych, czyli transformacji 
+  rastrów do wyg³adzonych obrazów wektorowych. 
+
+__ http://archives.aros-exec.org/share/video/aros-dnd.avi
+__ http://www.aros.org/nl
+__ http://snipurl.com/19hjr
+__ http://www.sub-ether.org/lunapaint/
+__ http://xelectrox.homeip.net/files/MSS.AROS-i386.zip
+__ http://snipurl.com/19hk8
+__ http://archives.aros-exec.org/share/graphics/misc/jhead.i386-aros.zip
+__ http://archives.aros-exec.org/share/graphics/convert/potrace.i386-aros.zip
