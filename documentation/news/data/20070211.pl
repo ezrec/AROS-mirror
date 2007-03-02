d18bd0dfeca5632fe5fee6a349976a5969ba2a7c
FOrest <FOrest@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-03-02 14:29:02 +0000

    translated
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25315 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/news/data/20070211.pl
@@ -0,0 +1,17 @@
+=============
+Status Update
+=============
+
+:Autor:   Paolo Besser
+:Data:     2007-02-11
+
+Tym razem tylko ma³e uzupe³nienie. B³êdy w inicjalnym drag'n'drop 
+w natywnej wersji AROS-a zosta³y wyeliminowane. Wanderer jest ju¿ 
+prawie w pe³ni funkcjonalny i nie powoduje zawieszeñ podczas 
+kopiowania plików z jednego okna do drugiego. Zosta³o takze dodane
+okno postêpu operacji.
+
+Wszystkie te nowe cechy s± ju¿ dostêpne w ostatnich nightly 
+builds (nie ma ich w bie¿±cym snapshot), tak wiêc sugerujemy ka¿demu
+zainteresowanemu spróbowaniem AROS-a ¶ci±ganie kompilacji z ostatniej 
+nocy.
