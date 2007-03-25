91294ea834a4e5c5d7adbde34f8d002e7ef539f8
NicJA <NicJA@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-03-25 12:18:42 +0000

    Bumped version
    git-svn-id: https://svn.aros.org/svn/aros/trunk@25570 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/Networking/Stacks/AROSTCP/bsdsocket/version.h
+++ contrib/Networking/Stacks/AROSTCP/bsdsocket/version.h
@@ -16,15 +16,15 @@
 #define STR(A) _STR(A)
 #endif
 #define VERSION         4
-#define REVISION        52
-#define DATE    "24.03.2007"
+#define REVISION        53
+#define DATE    "25.03.2007"
 #define VERS    "bsdsocket.library 4.52"
 #define VSTRING "bsdsocket.library" STR(VERSION) "." STR(REVISION) "(" DATE ")"
-#define VERSTAG "\0$VER: bsdsocket.library 4.52 (" DATE ")"
+#define VERSTAG "\0$VER: bsdsocket.library 4.53 (" DATE ")"
 
 #define MIAMI_VERSION 13
-#define MIAMI_REVISION 4
+#define MIAMI_REVISION 5
 #define MIAMI_VSTRING  "miami.library" STR(MIAMI_VERSION) "." STR(MIAMI_REVISION) "(" DATE ")"
 
-#define STACK_RELEASE "AROSTCP kernel v0.21 " CPU_TYPE " (" DATE ")"
+#define STACK_RELEASE "AROSTCP kernel v0.22 " CPU_TYPE " (" DATE ")"
 
