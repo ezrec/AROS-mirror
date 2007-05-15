2bc8e873ac83e8c85f9d5707327173bd40d6bcd9
mithrandir <mithrandir@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-05-15 18:38:09 +0000

    additional bugfix
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26097 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/targets/www/rsfeed/aeforum.php
+++ documentation/targets/www/rsfeed/aeforum.php
@@ -1,17 +1,3 @@
 <?php
-	
-        require_once("/home/groups/a/ar/aros/htdocs/rsfeed/rsslib.php");
-	require_once("/home/groups/a/ar/aros/htdocs/rsfeed/customerror.php");
-	set_error_handler("customError");
+echo "<script language=\"JavaScript\" src=\"http://landmark-project.com/feed2js/feed2js.php?src=http%3A%2F%2Faros-exec.org%2Fmodules%2Fnewbb%2Frss.php&num=15&targ=n\" type=\"text/javascript\"></script>"
 ?>
-
-<?php
-
-
-	$url= "http://aros-exec.org/modules/newbb/rss.php?c=0";
-	echo RSS_Links($url, 15);
-?>
-
-
-
-
