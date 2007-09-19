2e26653c72d3f30e8d7795f30c4072b55198ef4b
peta <peta@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-09-19 21:45:17 +0000

    Czech translations
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26734 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/news/archive/index.cs
@@ -0,0 +1,15 @@
+===============
+Novinky archivù
+===============
+
+Zde mù¾ete najtí staré zpravodajské èlánky, které byly publikovány na tomto webu
+bìhem tohoto roku a døívìj¹ích let. Jsou dostupné èlánky z následujících let:
+
++ `2007 <2007>`__
++ `2006 <2006>`__
++ `2005 <2005>`__
++ `2004 <2004>`__
++ `2003 <2003>`__
++ `2002 <2002>`__
++ `2001 <2001>`__
++ `2000 <2000>`__
