1706001b1193df7be557c4d2e24d4d79a06b06f1
peta <peta@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-09-22 22:20:06 +0000

    Czech translations
    git-svn-id: https://svn.aros.org/svn/aros/trunk@26816 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/users/index.cs
@@ -0,0 +1,15 @@
+==================
+U¾ivatelské zdroje
+==================
+
++ `Jak na instalaci`__
++ `Jak na pou¾ívání`__
++ `Pøíkazy Shellu`__
++ `Aplikace`__
++ `FAQ`__
+
+__ installation  
+__ using  
+__ shell/index
+__ applications/index
+__ faq  
---
+++ documentation/documentation/users/links.cs
@@ -0,0 +1,10 @@
+======
+Odkazy
+======
+
+:Authors:   Aaron Digulla, Adam Chodorowski 
+:Copyright: Copyright © 1995-2002, The AROS Development Team
+:Version:   $Revision: 22880 $
+:Date:      $Date: 2005-02-10 09:29:31 +0100 (ƒçt, 10 II 2005) $
+:Status:    Empty.
+
