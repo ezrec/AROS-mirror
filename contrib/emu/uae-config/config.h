6b31dd00c38652c8eccd0b5ef19981e4be4c4d9c
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-10-16 00:59:55 +0000

    Intermediate commit
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27072 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/emu/uae-config/config.h
+++ contrib/emu/uae-config/config.h
@@ -8,6 +8,20 @@
 
 #include <exec/types.h>
 
+enum {CPU_68000, CPU_68010, CPU_68020, CPU_68020_81, CPU_68040, CPU_CNT};
+enum {SPD_MAX=21, SPD_REAL, SPD_CNT};
+enum {MZ3_NONE, MZ3_1, MZ3_2, MZ3_4, MZ3_8, MZ3_16, MZ3_32, MZ3_64, MZ3_128, MZ3_256, MZ3_512, MZ3_CNT};
+enum {MCHP_256, MCHP_512, MCHP_1M, MCHP_2M, MCHP_4M, MCHP_8M, MCHP_CNT};
+enum {MFST_NONE, MFST_1, MFST_2, MFST_4, MFST_8, MFST_CNT};
+enum {MSLW_NONE, MSLW_512, MSLW_1M, MSLW_1_5M, MSLW_CNT};
+enum {MGFX_NONE, MGFX_1, MGFX_2, MGFX_4, MGFX_8, MGFX_16, MGFX_32, MGFX_CNT};
+enum {CHP_OCS, CHP_ECSAGNUS, CHP_ECSFULL, CHP_AGA, CHP_CNT};
+enum {COLL_NONE, COLL_SPRITES, COLL_PLAYFIELDS, COLL_FULL, COLL_CNT};
+enum {SND_NONE, SND_MUTED, SND_NORMAL, SND_EXACT, SND_CNT};
+enum {SNDCH_MONO, SNDCH_STEREO, SNDCH_MIXED, SNDCH_CNT};
+enum {SNDBIT_8, SNDBIT_16, SNDBIT_CNT};
+enum {JP_JOY0, JP_JOY1, JP_MOUSE, JP_NUMERIC, JP_KEYS0, JP_KEYS1, JP_CNT};
+
 struct UAEConfigdata
 {
     ULONG cpu_type;
@@ -39,7 +53,10 @@ struct UAEConfigdata
     ULONG prt_gameport1;
 };
 
+void config_init(struct UAEConfigdata *cfg);
+void config_reset(struct UAEConfigdata *cfg);
 BOOL config_write(struct UAEConfigdata *cfg, STRPTR filename);
 BOOL config_read(struct UAEConfigdata *cfg, STRPTR filename);
+void config_dispose(struct UAEConfigdata *cfg);
 
 #endif
