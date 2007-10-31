16386c7721eed59d140a3298396d19f94039c68b
NicJA <NicJA@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2007-10-31 12:30:37 +0000

    Attempt to fix varrargs funcs
    git-svn-id: https://svn.aros.org/svn/aros/trunk@27125 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/Networking/Stacks/AROSTCP/syslog/str.c
+++ contrib/Networking/Stacks/AROSTCP/syslog/str.c
@@ -38,25 +38,31 @@ STRPTR strnew(APTR pool, STRPTR original)
 }
 
 
-STRPTR vfmtnew(APTR pool, STRPTR fmt, va_list args)
+STRPTR vfmtnew(APTR pool, STRPTR fmt, ...)
 {
 	ULONG l = 0;
 	STRPTR s;
+
+	va_list args;
+
+#if defined(__AROS__)
+	va_start(args, fmt);
+	static UBYTE strng_tmp[1024];
+	sprintf((STRPTR)strng_tmp, fmt, args);
+	va_end(args);
+	l = strlen(strng_tmp);
+#else
 	va_list copy;
 
 	__va_copy(copy, args);
-#if defined(__AROS__)
-	static UBYTE strng_tmp[1024];
-	sprintf((STRPTR)strng_tmp, fmt, args);
-	l = strlen(strng_tmp);
-#else
+
 	VNewRawDoFmt(fmt, (APTR(*)(APTR, UBYTE))RAWFMTFUNC_COUNT, (STRPTR)&l, args);
 #endif
 
 	if (s = AllocVecPooled(pool, l + 1))
 	{
 #if defined(__AROS__)
-		sprintf(s, fmt, copy);
+		strcpy(s, strng_tmp);
 #else
 		VNewRawDoFmt(fmt, RAWFMTFUNC_STRING, s, copy);
 #endif
