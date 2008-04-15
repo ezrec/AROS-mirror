0f9e7695c786fac0f63832242a4e24b2a51cb0b6
NicJA <NicJA@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-04-15 21:46:13 +0000

    Fix source formating
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28321 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/Demo/Firework/firework.c
+++ contrib/Demo/Firework/firework.c
@@ -18,28 +18,27 @@
 
 /***********************************************************************************/
 
-struct IntuitionBase 	*IntuitionBase;
-struct GfxBase 		*GfxBase;
-struct Library 		*CyberGfxBase;
-struct Screen 		*scr;
-struct Window 		*win;
-struct RastPort 	*rp;
-ULONG 			cgfx_coltab[256];
-UBYTE	    	    	remaptable[256];
-UBYTE 			Keys[128];
-char			s[256];
-WORD	    	    	winx = -1, winy = -1;
-BOOL	    	    	forcescreen, forcewindow;
-BOOL	    	    	mustremap, truecolor, remapped, wbscreen = TRUE;
+struct IntuitionBase    *IntuitionBase;
+struct GfxBase 	        *GfxBase;
+struct Library 	        *CyberGfxBase;
+struct Screen           *scr;
+struct Window           *win;
+struct RastPort         *rp;
 
-static void cleanup(char *msg);
+ULONG                   cgfx_coltab[256];
+UBYTE                   remaptable[256];
+UBYTE                   Keys[128];
+char                    s[256];
+WORD                    winx = -1, winy = -1;
+BOOL                    forcescreen, forcewindow;
+BOOL                    mustremap, truecolor, remapped, wbscreen = TRUE;
 
 /***********************************************************************************/
 
-#define RANDOM		((WORD)(rand() & 0xFFFF))
+#define RANDOM          ((WORD)(rand() & 0xFFFF))
 
-#define W            	320
-#define H            	200
+#define W               320
+#define H               200
 #define PARTICLES       4096
 
 struct Particle
@@ -51,8 +50,8 @@ struct Particle
     UBYTE particlecolor;
 };
 
-#define yspeed speed
 #define xspeed counter
+#define yspeed speed
 
 static UBYTE chunkybufferarray[W * (H + 2)];
 static UBYTE *chunkybuffer = &chunkybufferarray[W];
@@ -67,46 +66,45 @@ static void refresh(void)
 {
     if (truecolor)
     {
-	WriteLUTPixelArray(chunkybuffer,
-			   0,
-			   0,
-			   W,
-			   rp,
-			   cgfx_coltab,
-			   win->BorderLeft,
-			   win->BorderTop,
-			   W,
-			   H,
-			   CTABFMT_XRGB8);
+        WriteLUTPixelArray(chunkybuffer,
+                0,
+                0,
+                W,
+                rp,
+                cgfx_coltab,
+                win->BorderLeft,
+                win->BorderTop,
+                W,
+                H,
+                CTABFMT_XRGB8);
     }
     else if (mustremap)
     {
-	LONG i;
-	UBYTE *src = chunkybuffer;
-	UBYTE *dest = chunkybuffer_remapped;
+        LONG i;
+        UBYTE *src = chunkybuffer;
+        UBYTE *dest = chunkybuffer_remapped;
 
-	for(i = 0; i < W * H; i++)
-	{
-	    *dest++ = remaptable[*src++];
-	}
-	WriteChunkyPixels(rp,
-	    	    	  win->BorderLeft,
-			  win->BorderTop,
-			  win->BorderLeft + W - 1,
-			  win->BorderTop + H - 1,
-			  chunkybuffer_remapped,
-			  W);
-
+        for(i = 0; i < W * H; i++)
+        {
+            *dest++ = remaptable[*src++];
+        }
+        WriteChunkyPixels(rp,
+                win->BorderLeft,
+                win->BorderTop,
+                win->BorderLeft + W - 1,
+                win->BorderTop + H - 1,
+                chunkybuffer_remapped,
+                W);
     }
     else
     {
-	WriteChunkyPixels(rp,
-	    	    	  win->BorderLeft,
-			  win->BorderTop,
-			  win->BorderLeft + W - 1,
-			  win->BorderTop + H - 1,
-			  chunkybuffer,
-			  W);
+        WriteChunkyPixels(rp,
+                win->BorderLeft,
+                win->BorderTop,
+                win->BorderLeft + W - 1,
+                win->BorderTop + H - 1,
+                chunkybuffer,
+                W);
     }
 }
 
@@ -115,66 +113,65 @@ static void refresh(void)
 static void initpalette(void)
 {
     static UBYTE pal[256*3];
-    
+
     ULONG ecx = 1024;
     ULONG eax = 0xA000;
     ULONG temp;
     ULONG palindex = 0;
-    
+
     for(ecx = 1024; ecx; ecx--)
     {
-    	eax = (eax >> 8) | (eax & 0xFF) << 24;
-	if ((eax & 255) >= 63)
-	{
-	    eax &= ~0xFF; eax |= 63;
-	    temp = ((eax & 0xFF00) + 0x400) & 0xFF00;
-	    eax &= ~0xFF00; eax |= temp;
-	}
-	
-	if (ecx & 3)
-	{
-	    pal[palindex++] = (UBYTE)eax;
-	}	
+        eax = (eax >> 8) | (eax & 0xFF) << 24;
+        if ((eax & 255) >= 63)
+        {
+            eax &= ~0xFF; eax |= 63;
+            temp = ((eax & 0xFF00) + 0x400) & 0xFF00;
+            eax &= ~0xFF00; eax |= temp;
+        }
+
+        if (ecx & 3)
+        {
+            pal[palindex++] = (UBYTE)eax;
+        }	
     }
-    
+
     for(palindex = 0; palindex < 256; palindex++)
     {
-    	ULONG red   = pal[palindex*3] * 4;
-	ULONG green = pal[palindex*3+1] * 4;
-	ULONG blue  = pal[palindex*3+2] * 4;
+        ULONG red   = pal[palindex*3] * 4;
+        ULONG green = pal[palindex*3+1] * 4;
+        ULONG blue  = pal[palindex*3+2] * 4;
 
-    	if (truecolor)
-	{
-	    cgfx_coltab[palindex] = (red << 16) + (green << 8) + blue;
-    	}
-	else if (mustremap)
-	{
-	    red   *= 0x01010101;
-	    green *= 0x01010101;
-	    blue  *= 0x01010101;
+        if (truecolor)
+        {
+            cgfx_coltab[palindex] = (red << 16) + (green << 8) + blue;
+        }
+        else if (mustremap)
+        {
+            red   *= 0x01010101;
+            green *= 0x01010101;
+            blue  *= 0x01010101;
 
-     	    remaptable[palindex] = ObtainBestPen(scr->ViewPort.ColorMap,
-      	    	    	    			 red,
-						 green,
-						 blue,
-						 OBP_Precision, PRECISION_IMAGE,
-						 OBP_FailIfBad, FALSE,
-						 TAG_DONE);
-      	    remapped = TRUE;
+            remaptable[palindex] = ObtainBestPen(scr->ViewPort.ColorMap,
+                                            red,
+                                            green,
+                                            blue,
+                                            OBP_Precision, PRECISION_IMAGE,
+                                            OBP_FailIfBad, FALSE,
+                                            TAG_DONE);
+            remapped = TRUE;
 
-	}
-	else
-	{
-	    red   *= 0x01010101;
-	    green *= 0x01010101;
-	    blue  *= 0x01010101;
-	    
-      	    SetRGB32(&scr->ViewPort, palindex, red, green, blue);
-	    
-	}
+        }
+        else
+        {
+            red   *= 0x01010101;
+            green *= 0x01010101;
+            blue  *= 0x01010101;
+
+            SetRGB32(&scr->ViewPort, palindex, red, green, blue);
+        
+        }
     }
 }
-
 
 /***********************************************************************************/
 
@@ -183,96 +180,96 @@ static void ticker(void)
     static ULONG tick;
     static ULONG ebx;
     static ULONG si = 0x100;
-    
+
     WORD x, y;
     ULONG ecx;
-    
+
     tick++;
-    
+
     if (!(tick & 3))
     {
-    	/* New explosion */
-	
-	x = RANDOM / 2;
-	y = RANDOM / 2;
-	
-    	for(ecx = 256; ecx; ecx--)
-	{
-	    struct Particle *p;
-	    float f;
-	    WORD xspeed;
-	    WORD yspeed;
-	    
-	    p = &particles[ebx];
-	    ebx = (ebx + 1) & (PARTICLES - 1);
-	    
-	    p->speed = RANDOM >> 6;
-	    p->counter = ecx;
-	    
-	    #warning: endianess
-	    
-	    f = (float)(*(ULONG *)(&p->speed));
-	    
-	    p->ypos = y;
-	    
-	    xspeed = sin(f) * p->speed;
-	    yspeed = cos(f) * p->speed;
-	    
-	    p->xpos = x;
+        /* New explosion */
 
-	    p->particlecolor = 58;
-	    p->yspeed = ((UWORD)yspeed) * 2;
-	    p->xspeed = xspeed;
-	    	    	    
-	}	
+        x = RANDOM / 2;
+        y = RANDOM / 2;
+
+        for(ecx = 256; ecx; ecx--)
+        {
+            struct Particle *p;
+            float f;
+            WORD xspeed;
+            WORD yspeed;
+
+            p = &particles[ebx];
+            ebx = (ebx + 1) & (PARTICLES - 1);
+
+            p->speed = RANDOM >> 6;
+            p->counter = ecx;
+
+            #warning: endianess
+
+            f = (float)(*(ULONG *)(&p->speed));
+
+            p->ypos = y;
+
+            xspeed = sin(f) * p->speed;
+            yspeed = cos(f) * p->speed;
+
+            p->xpos = x;
+
+            p->particlecolor = 58;
+            p->yspeed = ((UWORD)yspeed) * 2;
+            p->xspeed = xspeed;
+
+        }	
     }
-    
+
     /* Draw/move all particles */
-    
+
     for(ecx = PARTICLES; ecx; ecx--)
     {
-    	struct Particle *p = &particles[si];
-	WORD xp, yp;
-	
-	p->yspeed += 16;
-    	p->ypos += p->yspeed; p->yspeed -= (p->yspeed / 16);
-	p->xpos += p->xspeed; p->xspeed -= (p->xspeed / 16);
-	
-	yp = p->ypos / 256;
-	xp = p->xpos / 128;
+        struct Particle *p = &particles[si];
+        WORD xp, yp;
 
-	if ((yp >= -99) && (yp <= 99) && (xp >= -159) && (xp <= 159))
-	{
-	    ULONG dest = (yp + 100) * W + xp + 160;
-	    
-	    UBYTE col = p->particlecolor--;
-	    
-	    if (col > chunkybuffer[dest])
-	    {
-	    	chunkybuffer[dest] = col;
-	    }
-	}
-	
-	si = (si + 1) & (PARTICLES - 1);
+        p->yspeed += 16;
+        p->ypos += p->yspeed; p->yspeed -= (p->yspeed / 16);
+        p->xpos += p->xspeed; p->xspeed -= (p->xspeed / 16);
+
+        yp = p->ypos / 256;
+        xp = p->xpos / 128;
+
+        if ((yp >= -99) && (yp <= 99) && (xp >= -159) && (xp <= 159))
+        {
+            ULONG dest = (yp + 100) * W + xp + 160;
+
+            UBYTE col = p->particlecolor--;
+
+            if (col > chunkybuffer[dest])
+            {
+                chunkybuffer[dest] = col;
+            }
+        }
+
+        si = (si + 1) & (PARTICLES - 1);
     }
-    
+
     /* blur */
-    
+
     for(ecx = 0; ecx < W * H; ecx++)
     {
-    	UBYTE col = chunkybuffer[ecx];
-	
-	col += chunkybuffer[ecx + 1];
-	col += chunkybuffer[ecx - W];
-	col += chunkybuffer[ecx + W];
-	col /= 4;
-	
-	if ((col > 0) && (tick & 1))
-	{
-	    col--;
-	}
-	
-	chunkybuffer[ecx] = col;
+        UBYTE col = chunkybuffer[ecx];
+
+        col += chunkybuffer[ecx + 1];
+        col += chunkybuffer[ecx - W];
+        col += chunkybuffer[ecx + W];
+        col /= 4;
+
+        if ((col > 0) && (tick & 1))
+        {
+            col--;
+        }
+
+        chunkybuffer[ecx] = col;
     }
 }
 
@@ -280,36 +277,35 @@ static void ticker(void)
 
 static void cleanup(char *msg)
 {
-    
     if (msg)
     {
         printf("Firework: %s\n",msg);
     }
-    
+
     if (win) CloseWindow(win);
- 
+
     if (remapped)
     {
-    	WORD i;
-	
-	for(i = 0; i < 256; i++)
-	{
-	    ReleasePen(scr->ViewPort.ColorMap, remaptable[i]);
-	}
+        WORD i;
+
+        for(i = 0; i < 256; i++)
+        {
+            ReleasePen(scr->ViewPort.ColorMap, remaptable[i]);
+        }
     }
-    
+
     if (scr)
     {
-    	if (wbscreen)
-	    UnlockPubScreen(0, scr);
-	else
-	    CloseScreen(scr);
+        if (wbscreen)
+            UnlockPubScreen(0, scr);
+        else
+            CloseScreen(scr);
     }
-     
+
     if (CyberGfxBase) CloseLibrary(CyberGfxBase);
     if (GfxBase) CloseLibrary((struct Library *)GfxBase);
     if (IntuitionBase) CloseLibrary((struct Library *)IntuitionBase);
-    
+
     exit(0);
 }
 
@@ -327,18 +323,18 @@ static IPTR args[NUM_ARGS];
 static void getarguments(void)
 {
     struct RDArgs *myargs;
-    
+
     if ((myargs = ReadArgs(ARG_TEMPLATE, args, NULL)))
     {
-    	if (args[ARG_SCR])
-	    forcescreen = TRUE;
-	else if (args[ARG_WIN])
-	    forcewindow = TRUE;
-	    
-	if (args[ARG_X]) winx = *(IPTR *)args[ARG_X];
-	if (args[ARG_Y]) winy = *(IPTR *)args[ARG_Y];
-	
-    	FreeArgs(myargs);
+        if (args[ARG_SCR])
+            forcescreen = TRUE;
+        else if (args[ARG_WIN])
+            forcewindow = TRUE;
+
+        if (args[ARG_X]) winx = *(IPTR *)args[ARG_X];
+        if (args[ARG_Y]) winy = *(IPTR *)args[ARG_Y];
+
+        FreeArgs(myargs);
     }
 }
 
@@ -350,18 +346,17 @@ static void openlibs(void)
     {
         cleanup("Can't open intuition.library V39!");
     }
-    
+
     if (!(GfxBase = (struct GfxBase *)OpenLibrary("graphics.library", 39)))
     {
         cleanup("Can't open graphics.library V39!");
     }
-    
+
     if (!(CyberGfxBase = OpenLibrary("cybergraphics.library",0)))
     {
         cleanup("Can't open cybergraphics.library!");
     }
 }
-
 
 /***********************************************************************************/
 
@@ -371,33 +366,34 @@ static void getvisual(void)
     {
         cleanup("Can't lock pub screen!");
     }
-    
+
     if (GetBitMapAttr(scr->RastPort.BitMap, BMA_DEPTH) <= 8)
     {
-    	if (!forcewindow)
-	{
-	    wbscreen = FALSE;
-	}
-	else
-	{
-	    mustremap = TRUE;
-	}
+        if (!forcewindow)
+        {
+            wbscreen = FALSE;
+        }
+        else
+        {
+            mustremap = TRUE;
+        }
     }
-    
+
     if (forcescreen) wbscreen = FALSE;
-    
+
     if (!wbscreen)
     {
-    	UnlockPubScreen(NULL, scr);
+        UnlockPubScreen(NULL, scr);
         wbscreen = FALSE;
-	
-        scr = OpenScreenTags(NULL, SA_Width	, W	,
-				   SA_Height	, H	,
-				   SA_Depth	, 8	,
-				   TAG_DONE);
-    	if (!scr) cleanup("Can't open screen!");
+
+        scr = OpenScreenTags(NULL, SA_Width, W,
+                   SA_Height, H,
+                   SA_Depth, 8,
+                   TAG_DONE);
+
+        if (!scr) cleanup("Can't open screen!");
     }
-    
+
     truecolor = (GetBitMapAttr(scr->RastPort.BitMap, BMA_DEPTH) >= 15) ? TRUE : FALSE;
 }
 
@@ -407,33 +403,32 @@ static void makewin(void)
 {
     struct TagItem winonwbtags[] =
     {
-    	{WA_DragBar	, TRUE	    	    },
-	{WA_DepthGadget	, TRUE	    	    },
-	{WA_CloseGadget	, TRUE	    	    },
-	{WA_Title	, (IPTR)"Firework"  },
-	{TAG_DONE   	    	    	    }
+        {WA_DragBar, TRUE           },
+        {WA_DepthGadget, TRUE       },
+        {WA_CloseGadget, TRUE       },
+        {WA_Title, (IPTR)"Firework" },
+        {TAG_DONE                   }
     };
-    
+
     struct TagItem winonscrtags[] =
     {
-    	{WA_Borderless, TRUE },
-	{TAG_DONE   	     }
+        {WA_Borderless, TRUE },
+        {TAG_DONE            }
     };
 
     if (winx == -1) winx = (scr->Width - W - scr->WBorLeft - scr->WBorRight) / 2;
     if (winy == -1) winy = (scr->Height - H - scr->WBorTop - scr->WBorTop - scr->Font->ta_YSize - 1) / 2;
 
-    win = OpenWindowTags(NULL, WA_CustomScreen	, (IPTR)scr,
-    			       WA_Left		, winx, 
-			       WA_Top		, winy,
-    			       WA_InnerWidth	, W,
-    			       WA_InnerHeight	, H,
-			       WA_AutoAdjust	, TRUE,
-			       WA_Activate	, TRUE,
-			       WA_IDCMP		, IDCMP_CLOSEWINDOW |
-			       			  IDCMP_RAWKEY,
-			       TAG_MORE, wbscreen ? winonwbtags : winonscrtags);
-			       
+    win = OpenWindowTags(NULL, WA_CustomScreen, (IPTR)scr,
+                        WA_Left, winx,
+                        WA_Top, winy,
+                        WA_InnerWidth, W,
+                        WA_InnerHeight, H,
+                        WA_AutoAdjust, TRUE,
+                        WA_Activate, TRUE,
+                        WA_IDCMP, IDCMP_CLOSEWINDOW | IDCMP_RAWKEY,
+                        TAG_MORE, wbscreen ? winonwbtags : winonscrtags);
+                   
    if (!win) cleanup("Can't open window");
 
    rp = win->RPort; 
@@ -456,21 +451,18 @@ static void getevents(void)
     while ((msg = (struct IntuiMessage *)GetMsg(win->UserPort)))
     {
         switch(msg->Class)
-	{
-	    case IDCMP_CLOSEWINDOW:
-	        Keys[KC_ESC] = 1;
-		break;
-		
-	    case IDCMP_RAWKEY:
-	        {
-		    WORD code = msg->Code & ~IECODE_UP_PREFIX;
-		    
-		    Keys[code] = (code == msg->Code) ? 1 : 0;
+        {
+            case IDCMP_CLOSEWINDOW:
+                Keys[KC_ESC] = 1;
+                break;
 
-		}
-	        break;
-
-	}
+            case IDCMP_RAWKEY:
+                {
+                    WORD code = msg->Code & ~IECODE_UP_PREFIX;
+                    Keys[code] = (code == msg->Code) ? 1 : 0;
+                }
+                break;
+        }
         ReplyMsg((struct Message *)msg);
     }
 
@@ -481,16 +473,15 @@ static void getevents(void)
 static void action(void)
 {
     initpalette();
-    
+
     while (!Keys[KC_ESC])
     {
         getevents();
 
-    	ticker();
+        ticker();
         refresh();
-    	WaitTOF();
+        WaitTOF();
     }
-
 }
 
 /***********************************************************************************/
@@ -503,9 +494,8 @@ int main(void)
     makewin();
     action();
     cleanup(0);
-    
+
     return 0;
 }
 
 /***********************************************************************************/
-
