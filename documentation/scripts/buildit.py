943bbf30e0debe4fc7a193cb5155ccaa6806019a
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-05-28 22:40:11 +0000

    Updated copyright.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@28843 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/scripts/buildit.py
+++ documentation/scripts/buildit.py
@@ -1,5 +1,5 @@
 # -*- coding: iso-8859-1 -*-
-# Copyright © 2002-2007, The AROS Development Team. All rights reserved.
+# Copyright © 2002-2008, The AROS Development Team. All rights reserved.
 # $Id$
 
 import os, sys, shutil, glob
