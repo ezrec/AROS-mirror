5c2b3ed0972de772aa5ee44212b1f871024b3433
D_A_R_K <D_A_R_K@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-07-21 12:41:10 +0000

    Aros Polish Team - D_A_R_K, ISO-8859-2
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29054 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/acknowledgements.pl
@@ -0,0 +1,15 @@
+================
+Podziêkowania
+================
+
+Ten produkt zawiera oprogramowanie opracowane przez Uniwersytet Kalifornijski,
+Berkeley i jego wspó³pracowników.
+
+To oprogramowanie jest czê¶ciowo oparte na pracach grupy Independent JPEG Group.
+
+To oprogramowanie jest czê¶ciowo oparte na pracach FreeType Team.
+
+To oprogramowanie jest czê¶ciowo oparte na pracach Catharon Productions, Inc
+
+Graphics Interchange Format (c) Copyright jest w³asno¶ci± CompuServe Incorporated.
+GIF jako marka produktu jest w³asno¶ci CompuServe Incorporated.
