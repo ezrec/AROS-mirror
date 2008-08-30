af2821c93767d3b1415c6534d1132fb7c24fce77
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-08-30 23:14:34 +0000

    Removed genlibdoc.py and genshelldoc.py
    Improved autodoc.py:
    * It's now possible to call it from the main build script, i.e. additionally to "./build html"
      and "./build www" there are now the new targes "alldocs", "libdocs" and "shelldocs"
    
    * The Shell docs are nearly unchaned, but the library docs had some major flaws:
      - When the text contained title strings like "NAME", "NOTES" etc. the output was mixed up
      - Sorting order of library functions was case sensitive
      - The old script couldn't handle files with more than one autodoc header
    
    TODO:
    - Prototypes for variadic functions
    - Cross references ("see also") as hyper links
    - autodocs of static libraries
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29330 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/scripts/autodoc.py
+++ documentation/scripts/autodoc.py
@@ -5,86 +5,101 @@
 # This script creates files in ReST format from autodoc headers
 
 import glob
+import os
+import re
 
 # Relative path from the main build script to the root of the AROS source
 topdir = ".."
 
-# All defined autodoc titles
-titles = ("NAME","FORMAT","SYNOPSIS","LOCATION","FUNCTION","INPUTS","RESULT","EXAMPLE",
-    "NOTES","BUGS","SEE ALSO","INTERNALS","HISTORY","TEMPLATE")
+# Regex for autodoc block (without surrounding comments)
+ad_regx = re.compile(r"""
+^/\*{6,}
+(
+.*?
+^[/*\s]{4,4}NAME
+.*?
+)
+^\*{7,}/
+""", re.VERBOSE | re.MULTILINE | re.DOTALL)
 
-state_waitblock=1
-state_inblock=2
-state_intitle=3
+# Regex for a title
+titles_regx = re.compile(r"""
+^[/\*\s]{4,4}
+(NAME|FORMAT|SYNOPSIS|LOCATION|FUNCTION|INPUTS|RESULT|EXAMPLE|NOTES|BUGS|SEE\ ALSO|INTERNALS|HISTORY|TEMPLATE)[/\*\s]*?$
+""", re.VERBOSE)
 
+# Regex for AROS library functions
+libfunc_regx = re.compile(r'AROS_.*\(\s*(.*?)\s*\,\s*(.*?)\s*\,')
+
+# don't generate autodocs from this files
+# don't generate autodocs from this files
+#blacklist = ("buildeasyrequestargs.c", "buildeasyrequestargs_morphos.c",
+#		"buildsysrequest.c", "buildsysrequest_morphos.c",
+#		"refreshwindowframe.c", "refreshwindowframe_morphos.c",
+#		"setiprefs.c", "setiprefs_morphos.c",
+#		"sysreqhandler.c", "sysreqhandler_morphos.c",
+#		"match_old.c")
+
+
+# don't generate library autodocs from this files
+blacklist = ("buildeasyrequestargs_morphos.c",
+                "buildsysrequest_morphos.c",
+                "refreshwindowframe_morphos.c",
+                "setiprefs_morphos.c",
+                "sysreqhandler_morphos.c",
+                "match_old.c")
 
 class autodoc:
-
-    def __init__(self):
+    def __init__(self, content):
         self.reset()
-
+        current_title = None
+        for line in content.splitlines():
+            match = titles_regx.match(line)
+            if match:
+                current_title = match.group(1)
+                self.titles[current_title] = ""
+            elif current_title:
+                self.titles[current_title] += (" " + line.expandtabs())[4:] + "\n"
+        
+        # check for empty chapters, because we don't want to print them
+        for title, content in self.titles.iteritems():
+            if content.strip() == "":
+                self.titles[title] = ""
 
     def __cmp__(self, other):
         return cmp(self.docfilename, other.docfilename)
 
-
     def reset(self):
-        self.titles = {} # dict with table with strings for each title
-        for title in titles:
-            self.titles[title] = []
+        self.titles = {} # dict for each title
         self.docname = "" # function or command name
         self.docfilename = "" # filename without ".en"
-        self.prevdocfilename = ""
-        self.nextdocfilename = ""
 
-    def read(self, filehandle):
-        self.reset()
-        state = state_waitblock
-        while 1:
-            line = filehandle.readline().expandtabs()
-            if line == "":
-                # end of file
-                return False
-            if state == state_waitblock:
-                if line[:7] == "/******":
-                    state = state_inblock
-            elif state == state_inblock:
-                if line[:7] == "*******":
-                    # we've reached the end marker without any title
-                    self.reset()
-                    state = state_waitblock
-                elif line.strip() in titles:
-                    state = state_intitle
-                    current_title = line.strip()
-            elif state == state_intitle:
-                if line[:7] == "*******":
-                    break
-                elif line.strip() in titles:
-                    state = state_intitle
-                    current_title = line.strip()
-                else:
-                    self.titles[current_title].append(line)
-        
-        # check for empty chapters, because we don't want to print them
-        # replace them by an empty list
-        for title in self.titles:
-            content = ""
-            for line in self.titles[title]:
-                content = content + line
-            content = content.strip()
-            if content == "":
-                self.titles[title] = []
-                
+    def write(self, filehandle, titles):
+        for title in titles:
+            title_key = title.upper()
+            if self.titles.has_key(title_key):
+                lines = self.titles[title_key]
+                if len(lines) > 0:
+                    filehandle.write(title + "\n")
+                    filehandle.write("~" * len(title) + "\n")
+                    filehandle.write("::\n\n")
+                    filehandle.write(lines)
+                    filehandle.write("\n")
+
+
+class shellautodoc(autodoc):
+    def __init__(self, content):
+        autodoc.__init__(self, content)
         # get docname
-        for line in self.titles["NAME"]:
-            self.docname = self.docname + line
-        self.docname = self.docname.split()[0]
+        self.docname = self.titles["NAME"].split()[0]
         if self.docname == "":
             raise ValueError("docname is empty")
         self.docfilename = self.docname.lower()
-        
-        return True
 
+    def reset(self):
+        autodoc.reset(self)
+        self.prevdocfilename = ""
+        self.nextdocfilename = ""
 
     def write(self, filehandle, titles):
         underline="=" * len(self.docname) + "\n"
@@ -101,40 +116,66 @@ class autodoc:
             filehandle.write("`Next <" + self.nextdocfilename + ">`_ ")
 
         filehandle.write("\n\n---------------\n\n")
+        
+        autodoc.write(self, filehandle, titles)
 
-        for title in titles:
-            title_key = title.upper()
-            if self.titles.has_key(title_key):
-                if len(self.titles[title_key]) > 0:
-                    filehandle.write(title + "\n")
-                    filehandle.write("~" * len(title) + "\n")
-                    filehandle.write("::\n\n")
-                    for line in self.titles[title_key]:
-                        if len(line)>4:
-                            # one space to keep as literal block
-                            filehandle.write(" " + line[4:])
-                        else:
-                            filehandle.write("\n")    
-                    filehandle.write("\n")
+
+class libautodoc(autodoc):
+    def __init__(self, content):
+        self.reset()
+        autodoc.__init__(self, content)
+        m = libfunc_regx.search(content)
+        if m:
+            self.docname = m.group(2)
+            self.docfilename = self.docname.lower()
+            self.rettype = m.group(1)
+        if self.titles.has_key("SYNOPSIS"):
+            for par in libfunc_regx.findall(self.titles["SYNOPSIS"]):
+                self.parameters.append(par)
+
+        # create new Synopsis
+        syn = " " + self.rettype + " " + self.docname
+        if len(self.parameters) == 0:
+            syn += "();\n"
+        else:
+            syn += "(\n"
+            for linenr in range(len(self.parameters)):
+                line = self.parameters[linenr]
+                syn += "          " + line[0] + " " + line[1]
+                if linenr < len(self.parameters) - 1:
+                    syn += ",\n"
+                else:
+                    syn += " );\n"
+        self.titles["SYNOPSIS"] = syn
+
+    def reset(self):
+        autodoc.reset(self)
+        self.rettype = ""
+        self.parameters = []
+
+
+    def write(self, filehandle, titles):
+        filehandle.write(self.docname + "\n")
+        filehandle.write("=" * len(self.docname) + "\n\n")
+        autodoc.write(self, filehandle, titles)
+        filehandle.write("\n")
 
 
 class shelldoclist:
-
     def __init__(self):
         self.doclist = []
-        self.targetdir = "."
-
 
     def read(self, srcdir):
-        filenames = glob.glob(srcdir + "/*.c")
+        filenames = glob.glob(os.path.join(srcdir, "*.c"))
         for filename in filenames:
             print "Reading from file", filename
-            file = open(filename)
-            ad = autodoc()
-            ad.read(file)
-            if ad.docname != "":
-                self.doclist.append(ad)
-            file.close()
+            filehandle = open(filename)
+            content = filehandle.read() # read whole file
+            for ad_entry in ad_regx.findall(content):
+                ad = shellautodoc(ad_entry)
+                if ad.docname != "":
+                    self.doclist.append(ad)
+            filehandle.close()
         self.doclist.sort()
 
         # set prev/next fields
@@ -144,12 +185,11 @@ class shelldoclist:
             if docnr<len(self.doclist) - 1:
                 self.doclist[docnr].nextdocfilename = self.doclist[docnr + 1].docfilename
 
-
     def write_index(self, filehandle):
         print "Creating index file"
         filehandle.write(".. This document is automatically generated. Don't edit it!\n\n")
         filehandle.write("==============\n")
-        filehandle.write("Shell commands\n")
+        filehandle.write("Shell Commands\n")
         filehandle.write("==============\n\n")
         filehandle.write("`Introduction <introduction>`_\n\n")
 
@@ -158,39 +198,134 @@ class shelldoclist:
         filehandle.write("\n")
         for doc in self.doclist:    
             filehandle.write("__ " + doc.docfilename + "\n")
-        print "Done"
-
 
     def write(self, targetdir, titles):
         for doc in self.doclist:
-            filename = targetdir + "/" + doc.docfilename + ".en"
+            filename = os.path.join(targetdir, doc.docfilename + ".en")
             print "Writing to file", filename
             filehandle = open(filename, "w")
             doc.write(filehandle, titles)
             filehandle.close()
         
         # create index page
-        filehandle = open(targetdir + "/index.en", "w")
+        filehandle = open(os.path.join(targetdir, "index.en"), "w")
         self.write_index(filehandle)
         filehandle.close()
 
+
+class libdoclist:
+    def __init__(self):
+        self.doclist = []
+        self.docfilename = ""
+
+    def read(self, srcdir):
+        self.docfilename = os.path.basename(srcdir) # rightmost part of the path
+        cfiles = os.listdir(srcdir)
+        for file in cfiles:
+            if not (file in blacklist) and file[-2:]==".c":
+                filename = os.path.join(srcdir, file)
+                print "Reading from file", filename
+                filehandle = open(filename)
+                content = filehandle.read() # read whole file
+                for ad_entry in ad_regx.findall(content):
+                    ad = libautodoc(ad_entry)
+                    if ad.docname != "":
+                        self.doclist.append(ad)
+                filehandle.close()
+        self.doclist.sort()
+
+    def write(self, targetdir, titles):
+        if len(self.doclist) > 0:
+            filename = os.path.join(targetdir, self.docfilename + ".en")
+            print "Writing to file", filename
+            filehandle = open(filename, "w")
+
+            #create header
+            underline = "=" * len(self.docfilename)
+            filehandle.write(underline + "\n")
+            filehandle.write(self.docfilename + "\n")
+            filehandle.write(underline + "\n\n")
+
+            filehandle.write(".. This document is automatically generated. Don't edit it!\n\n")
+            filehandle.write("`Index <index>`_\n\n")
+            filehandle.write("----------\n\n")
+
+            # create toc
+            tablesep = (("=" * 39) + " ") * 4
+            filehandle.write(tablesep + "\n")
+            for docnr in range(len(self.doclist)):
+                tocname = "`" + self.doclist[docnr].docname + "`_"
+                tocname = tocname.ljust(40)
+                filehandle.write(tocname)
+                if (docnr + 1) % 4 == 0:
+                    filehandle.write("\n")
+            filehandle.write("\n" + tablesep)
+            filehandle.write("\n\n-----------\n\n")            
+     
+            for doc in self.doclist:
+                doc.write(filehandle, titles)
+            filehandle.close()
+
+
 def create_lib_docs():
-    pass
+    targetdir = os.path.join("documentation", "test")
+    targetdir = os.path.join("documentation", "developers", "autodocs")
+    srcdirs = ( os.path.join(topdir, "rom"), os.path.join(topdir, "workbench/libs") )
+    for dir in srcdirs:
+        create_lib_docs_dir(dir, targetdir)
+
+    # print index file
+    files = os.listdir(targetdir)
+    files.sort()
+
+    filehandle = open(os.path.join(targetdir, "index.en"), "w")
+    print "Creating index.en"
+    filehandle.write("======================\n")
+    filehandle.write("Autodocs for Libraries\n")
+    filehandle.write("======================\n\n")
+
+    for doc in files:
+        if doc[0:5] != "index" and doc != ".svn":
+            docname = doc[0:-3]
+            filehandle.write("+ `%s <%s>`_\n" %(docname, docname))
+    filehandle.close()
+    
+
+def create_lib_docs_dir(srcdir, targetdir):
+    subdirs = os.listdir(srcdir)
+    lib_titles = ("Synopsis","Template","Function",
+        "Inputs","Result","Example","Notes","Bugs","See also")  # The titles we want
+                                                                # to be print
+    print subdirs
+    for dir in subdirs: # exec ,graphics etc.
+        docpath = os.path.join(srcdir, dir)
+        print "dir docpath", dir, docpath
+        if (dir != ".svn") and os.path.isdir(docpath) :
+            libdocs = libdoclist()
+            libdocs.read(docpath)
+            libdocs.write(targetdir, lib_titles)
 
 def create_shell_docs():
-    srcdirs = ( topdir + "/workbench/c",
-                topdir + "/workbench/c/shellcommands",
-                topdir + "/workbench/c/Identify",
-                topdir + "/workbench/c/iprefs" )
+    srcdirs = ( os.path.join(topdir, "workbench", "c"),
+                os.path.join(topdir, "workbench", "c", "shellcommands"),
+                os.path.join(topdir, "workbench", "c", "Identify"),
+                os.path.join(topdir, "workbench", "c", "iprefs") )
                 
-    targetdir = "documentation/users/shell" # relative to main build script
+    targetdir = os.path.join("documentation", "users", "shell") # relative to main build script
     shell_titles = ("Name","Format","Template","Synopsis","Location","Function",
-        "Inputs","Result","Example","Notes","Bugs","See also") #  The titles we want
+        "Inputs","Result","Example","Notes","Bugs","See also")  # The titles we want
                                                                 # to be print
     shelldocs = shelldoclist()
     for dir in srcdirs:
         shelldocs.read(dir)
     shelldocs.write(targetdir, shell_titles)
+    print "Done"
 
 def create_all_docs():
     create_shell_docs()
+    create_lib_docs()
+
+
+if __name__ == "__main__":
+    os.chdir("..") # because we have relative paths
+    create_all_docs()
