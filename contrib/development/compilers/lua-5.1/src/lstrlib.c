d3dc6c203cc729c2a53d37a103712be847e8ac42
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-09-03 04:38:07 +0000

    Updated to Lua 5.1.4 This is a bugfix release, see: http://www.lua.org/bugs.html#5.1.3
    Siamiga.lib: Use InnerWidth, InnerHeight to avoid that the borders
    make the drawing area too small.
    
    Small fixes in the siamiga examples.
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29394 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/development/compilers/lua-5.1/src/lstrlib.c
+++ contrib/development/compilers/lua-5.1/src/lstrlib.c
@@ -35,7 +35,8 @@ static int str_len (lua_State *L) {
 
 static ptrdiff_t posrelat (ptrdiff_t pos, size_t len) {
   /* relative string position: negative means back from end */
-  return (pos>=0) ? pos : (ptrdiff_t)len+pos+1;
+  if (pos < 0) pos += (ptrdiff_t)len + 1;
+  return (pos >= 0) ? pos : 0;
 }
 
 
--- contrib/development/compilers/lua-5.1/src/luaconf.h
+++ contrib/development/compilers/lua-5.1/src/luaconf.h
@@ -440,10 +440,10 @@
 @* can use.
 ** CHANGE it if you need lots of (Lua) stack space for your C
 ** functions. This limit is arbitrary; its only purpose is to stop C
-** functions to consume unlimited stack space.
+** functions to consume unlimited stack space. (must be smaller than
+** -LUA_REGISTRYINDEX)
 */
-#define LUAI_MCS_AUX	((int)(INT_MAX / (4*sizeof(LUA_NUMBER))))
-#define LUAI_MAXCSTACK	(LUAI_MCS_AUX > SHRT_MAX ? SHRT_MAX : LUAI_MCS_AUX)
+#define LUAI_MAXCSTACK	8000
 
 
 
