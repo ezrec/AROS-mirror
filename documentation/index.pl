171e616a7f33b9d0283a363b6a240a4446b54803
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-07 16:08:44 +0000

    Update PL index page to current page format.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29694 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/index.pl
+++ documentation/index.pl
@@ -1,10 +1,62 @@
+.. raw:: html
+
+   <table style="text-align: justify; width: 100%;" background="/images/bgcolormain.png" border="0" cellpadding="1" cellspacing="1"><tr><td style="vertical-align: top;">
 
 .. Include:: introduction/index-abstract.pl
 
-`Czytaj wiêcej... <introduction/index>`__
+`Wiêcej... <introduction/index>`__
+
+.. raw:: html
+
+   </td></tr></tbody></table><br>
+
+
+.. raw:: html
+
+   <table style="width: 100%; text-align: justify; margin-left: auto; margin-right: auto;" background="/images/bgcolormain.png" border="0" cellpadding="1" cellspacing="1"><tr><td>
 
 .. Include:: documentation/developers/contribute-abstract.pl
 
-`Czytaj wiêcej... <documentation/developers/contribute>`__
+`Wiêcej... <documentation/developers/contribute>`__
+
+.. raw:: html
+
+   </td></tr></table>
+
+
+.. raw:: html
+
+   <table style="width: 100%; text-align: justify; margin-left: auto; margin-right: auto;" border="0" cellpadding="1" cellspacing="1"><tr><td>
 
 .. Include:: news/index.pl
+
+.. raw:: html
+
+   </td></tr></table><br>
+
+
+.. raw:: html
+
+   <td style="width: 243px; vertical-align: top;"><table style="text-align: justify; width:100%;" background="/images/bgcolorright.png" border="0" cellpadding="1" cellspacing="1"><tr><td>
+   <?php if ($win_ie56) { echo "<img alt=\"Direct download Icon\" src=\"/images/directdownloadicon.gif\" align=\"middle\">";} 
+   else { echo "<img alt=\"Direct download Icon\" src=\"/images/directdownloadicon.png\" align=\"middle\">";} ?>Direct Downloads:<br>
+   <img style="width: 238px; height: 2px;" alt="spacer" src="/images/sidespacer.png"><br>
+   Nocne buildy budowane s± automatycznie ka¿nej nocy z najnowszych ¼róde³. Aby wypróbowaæ Live CD AROSa, ¶ci±gnij "boot ISO" dla twojej platformy.<br><br>
+   <?php virtual("/cgi-bin/files2") ?> <br></td></tr></tbody></table>
+   <table style="text-align: justify; width: 100%;" background="/images/bgcolorright.png" border="0" cellpadding="1" cellspacing="1"><tr><td>
+   <?php if ($win_ie56) { echo "<img alt=\"Archive Icon\" src=\"/images/archivedownloadicon.gif\" align=\"middle\">"; } 
+   else { echo "<img alt=\"Archive Icon\" src=\"/images/archivedownloadicon.png\" align=\"middle\">"; } ?>
+   Najnowsze uploady do Archiwum:<br><img style="width: 238px; height: 2px;" alt="spacer" src="/images/sidespacer.png"><br>
+   <a href=http://archives.aros-exec.org>Archiwa AROSa </a> zawieraj± najnowsze aplikacje, grafiki i dodatkow± dokumentacjê przygotowan± przez spo³eczno¶æ AROS.<br><br>
+   <?php include("../rsfeed/archives.php"); ?><br></td></tr></table>
+   <table style="text-align: justify; width: 100%;" background="/images/bgcolorright.png" border="0" cellpadding="1" cellspacing="1"><tr><td>
+   <?php if ($win_ie56) { echo "<img alt=\"Community Icon\" src=\"/images/communityicon.gif\" align=\"middle\">"; } 
+   else { echo "<img alt=\"Community Icon\" src=\"/images/communityicon.png\" align=\"middle\">"; } ?> 
+   Najnowsze posty na AROS-EXEC:<br><img style="width: 238px; height: 2px;" alt="spacer" src="/images/sidespacer.png"><br>
+   <a href=http://www.aros-exec.org>AROS-EXEC</a> jest g³ównym serwisem spo³eczno¶ci AROS. Odwied¼ go je¶li szukasz pomocy lub chcesz sie dowiedzieæ wiêcej o systemie AROS.<br><br>
+   <?php include("../rsfeed/aeforum.php"); ?><br></td></tr></tbody></table>
+   <table style="width: 100%; text-align: justify; margin-left: auto; margin-right: auto;" background="/images/bgcolorright.png" border="0" cellpadding="1" cellspacing="1"><tr><td style="vertical-align: top;">
+   <?php if ($win_ie56) { echo "<img alt=\"Syndication Icon\" src=\"/images/rssicon1.gif\" align=\"middle\">"; } else { echo "<img alt=\"Syndication Icon\" src=\"/images/rssicon1.png\" align=\"middle\">"; } ?>
+   Kana³y RSS:<br><img style="width: 177px; height: 2px;" alt="spacer" src="/images/sidespacer.png"><br>
+   <?php  include("../rsfeed/synfeed.php");  ?><br></td></tr></table>
+   </td></tr></table>
--- documentation/introduction/index-abstract.pl
+++ documentation/introduction/index-abstract.pl
@@ -1,5 +1,10 @@
-Wprowadzenie
-============
+.. raw:: html
+
+   <h1>Wprowadzenie<br><img style="width: 238px; height: 2px;" alt="spacer" src="/images/sidespacer.png"></h1>
+
+.. raw:: html
+
+  <?php include('rsfeed/randimg.php');  random_image("/images/thubs/","100","76"); ?>
 
 AROS jest przeno¶nym i ogólnodostêpnym systemem operacyjnym, posiadaj±cym graficzny interfejs u¿ytkownika, d±¿±cym do kompatybilno¶ci z AmigaOS 3.1, jednocze¶nie przewy¿szaj±cym go w wielu obszarach. Kod ¼ród³owy jest dostêpny pod otwart± licencj±, 
 która pozwala go dowolnie modyfikowaæ.
