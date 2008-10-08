9e64895874d0687c6aac4297d86f05635dc27803
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-10-08 16:22:15 +0000

    Correction for php includes
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29696 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/introduction/index-abstract.pl
+++ documentation/introduction/index-abstract.pl
@@ -4,7 +4,7 @@
 
 .. raw:: html
 
-  <?php include('rsfeed/randimg.php');  random_image("/images/thubs/","100","76"); ?>
+  <?php include('../rsfeed/randimg.php');  random_image("/images/thubs/","100","76"); ?>
 
 AROS jest przeno¶nym i ogólnodostêpnym systemem operacyjnym, posiadaj±cym graficzny interfejs u¿ytkownika, d±¿±cym do kompatybilno¶ci z AmigaOS 3.1, jednocze¶nie przewy¿szaj±cym go w wielu obszarach. Kod ¼ród³owy jest dostêpny pod otwart± licencj±, 
 która pozwala go dowolnie modyfikowaæ.
