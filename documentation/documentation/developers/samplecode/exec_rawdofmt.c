767cc894ab9e8535b34d3af2e06c9ef18f0f6e2c
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-11-05 21:11:24 +0000

    New examples for EasyRequest and RawDoFmt
    git-svn-id: https://svn.aros.org/svn/aros/trunk@29993 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/documentation/developers/samplecode/exec_rawdofmt.c
@@ -0,0 +1,61 @@
+/*
+    Example for RawDoFmt formatting
+*/
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    /*
+	RawDoFmt() expects WORD alignment but the C compiler
+	aligns to LONG by default. GCC can be forced to use
+	WORD alignment by #pragma pack(2)
+    */
+#pragma pack(2)
+    struct Data
+    {
+	LONG longval;
+	WORD wordval;
+	STRPTR str;
+    } data = {10000000,1001,"Hello"};
+#pragma pack()
+
+    /* Format string with placeholders. */
+    CONST_STRPTR formatstring = "TEST LONG %ld WORD %d STRING %s";
+    
+    /* The data which will be inserted in the placeholders. */
+    APTR datastream = &data;
+    
+    /*	Storage place for result. Note that there is no boundary check. */
+    TEXT putchdata[1000]; 
+    
+    RawDoFmt(
+	formatstring, // CONST_STRPTR FormatString
+	datastream,   // APTR DataStream
+        NULL,         // VOID_FUNC PutChProc
+	putchdata     // APTR PutChData
+    );
+
+    puts(putchdata);
+
+    
+    /*
+	The pragma trick doesn't work with variadic functions like
+	Printf(), EasyRequest() etc. Here you have to use for integers
+	%ld/%lu for the placeholders and the type must be converted
+	to LONG/ULONG.
+    */
+    
+    WORD wordval = 1000;
+    LONG longval = 100000;
+    
+    Printf("Printf: %ld %ld\n", (LONG)wordval, longval);
+    
+    return 0;
+}
---
+++ documentation/documentation/developers/samplecode/intuition_easyreq.c
@@ -0,0 +1,61 @@
+/*
+    Example for Easyrequester
+*/
+
+#include <proto/exec.h>
+#include <proto/dos.h>
+#include <proto/intuition.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void)
+{
+    LONG result;
+    
+    struct EasyStruct es_simple = {
+	sizeof(struct EasyStruct),      // es_StructSize
+        0,                              // es_Flags
+        "Window Title",                 // es_Title
+        "Example for EasyRequest.\n"
+	"Lines can be split with \\n.", // es_TextFormat
+        "Yes|No|Cancel"                 // es_GadgetFormat
+    };
+
+    /*
+	The return value is the number of the clicked button. Note that the numbering
+	has a strange order: 1,2,3,...,N,0. (Reason is compatibility with AutoRequest()).
+    */
+    result = EasyRequest(NULL, &es_simple, NULL);
+    printf("Result of EasyRequest %d\n", result);
+
+    /*
+	EasyRequest allows RawDoFmt()-like formatting. RawDoFmt() expects WORD
+	alignment but the C compiler aligns to LONG. Thus we have to use for
+	integer variables %ld or %lu and convert to LONG or ULONG.
+    */
+    
+    STRPTR string = "Inserted string";
+    LONG longnumber = 2147483647;
+    WORD wordnumber = 32767;
+    
+    struct EasyStruct es_rawdofmt = {
+	sizeof(struct EasyStruct),
+        0,
+        "WindowTitle",
+	"Test for RawDoFmt()-like formatting.\n\n"
+	"String: %s\n"
+	"WORD: %ld\n"
+	"LONG: %ld\n",
+        "WORD: %ld"
+    };
+
+    /*
+	The arguments beginning with the 4th are used for the placeholders. First for es_TextFormat
+	then es_GadgetFormat.
+    */
+    EasyRequest(NULL, &es_rawdofmt, NULL, string, (LONG)wordnumber, longnumber, (LONG)wordnumber);
+    
+    return 0;
+}
