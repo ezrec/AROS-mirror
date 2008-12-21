fb42813cc6dd107b44e7f2c32e84c9e35f957975
schulz <schulz@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2008-12-21 21:56:30 +0000

    changes in two previously forgotten files
    git-svn-id: https://svn.aros.org/svn/aros/trunk@30145 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/necessary/USB/classes/MassStorage/storage_device.c
+++ contrib/necessary/USB/classes/MassStorage/storage_device.c
@@ -5,7 +5,596 @@
  *      Author: misc
  */
 
-#define DEBUG 1
-#include <aros/debug.h>
-#include "storage.h"
+/*
+ * This file contains exec style device, usbmss.device, which is available as
+ * soon as the mass storage class gets initialized. The device translates the
+ * trackdisk commands into mass storage methods.
+ */
+#include <aros/symbolsets.h>
+#include <aros/macros.h>
 
+#include <exec/types.h>
+#include <exec/nodes.h>
+#include <exec/devices.h>
+#include <exec/io.h>
+#include <exec/errors.h>
+#include <devices/trackdisk.h>
+#include <devices/scsidisk.h>
+#include <devices/newstyle.h>
+#include <oop/oop.h>
+#include <dos/dos.h>
+#include <dos/dosextens.h>
+
+#include <hidd/hidd.h>
+#include <usb/usb.h>
+#include <usb/storage.h>
+
+#define DEBUG 1
+
+#include <proto/exec.h>
+#include <proto/oop.h>
+#include <proto/dos.h>
+#include <aros/debug.h>
+
+#include "storage.h"
+#include LC_LIBDEFS_FILE
+
+#undef SD
+#define SD(x) (&LIBBASE->sd)
+
+/* Invalid command - reports error */
+static void cmd_Invalid(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	D(bug("[MSS-dev] Invalid command %d for unit %04x\n", io->io_Command, unit->msu_unitNum));
+    io->io_Error = IOERR_NOCMD;
+}
+
+/* NULL commad - always success. */
+static void cmd_NULL(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	IOStdReq(io)->io_Actual = 0;
+}
+
+static void cmd_Reset(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	HIDD_USBStorage_Reset(unit->msu_object);
+	IOStdReq(io)->io_Actual = 0;
+}
+
+static void cmd_AddChangeInt(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+    Forbid();
+    AddHead(&unit->msu_diskChangeList, (struct Node *)io);
+    Permit();
+
+    io->io_Flags &= ~IOF_QUICK;
+    unit->msu_unit.unit_flags &= ~UNITF_ACTIVE;
+}
+
+static void cmd_RemChangeInt(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+    Forbid();
+    Remove((struct Node *)io);
+    Permit();
+}
+
+static void cmd_GetGeometry(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+    if (IOStdReq(io)->io_Length == sizeof(struct DriveGeometry))
+    {
+        struct DriveGeometry *dg = (struct DriveGeometry *)IOStdReq(io)->io_Data;
+
+        dg->dg_SectorSize       = unit->msu_blockSize;
+        dg->dg_TotalSectors		= unit->msu_blockCount;
+        dg->dg_Cylinders		= unit->msu_blockCount / (255*63);
+        dg->dg_Heads			= 255;
+        dg->dg_TrackSectors		= 63;
+        dg->dg_CylSectors		= 255*63;
+        dg->dg_BufMemType		= MEMF_PUBLIC;
+        dg->dg_DeviceType		= DG_DIRECT_ACCESS;
+        dg->dg_Flags			= 0;
+        dg->dg_Reserved			= 0;
+
+        IOStdReq(io)->io_Actual = sizeof(struct DriveGeometry);
+    }
+    else
+    	io->io_Error = TDERR_NotSpecified;
+}
+
+static void cmd_Read32(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	uint32_t block = IOStdReq(io)->io_Offset;
+	uint32_t count = IOStdReq(io)->io_Length;
+
+	D(bug("[MSS-dev] Read32(%08x, %08x) with bs %d\n", IOStdReq(io)->io_Offset, IOStdReq(io)->io_Length, unit->msu_blockSize));
+
+	if (!IOStdReq(io)->io_Data)
+	{
+		D(bug("[MSS-dev] No buffer!\n"));
+		io->io_Error = IOERR_BADADDRESS;
+	}
+	else if ((block % unit->msu_blockSize) || (count % unit->msu_blockSize))
+	{
+		D(bug("[MSS-dev] offset or length not sector-aligned\n"));
+		cmd_Invalid(io, dev, unit);
+	}
+	else
+	{
+		block /= unit->msu_blockSize;
+		count /= unit->msu_blockSize;
+		uint32_t cnt = 0;
+
+		if (block + count > unit->msu_blockCount)
+		{
+			D(bug("[MSS-dev] ERROR! Requested read outside the available area\n"));
+			io->io_Error = IOERR_BADADDRESS;
+		}
+		else
+		{
+			HIDD_USBStorage_Read(unit->msu_object, unit->msu_unitNum & 0xf, IOStdReq(io)->io_Data, block, count);
+			IOStdReq(io)->io_Actual = IOStdReq(io)->io_Length;
+		}
+	}
+}
+
+static void cmd_Read64(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	uint64_t block = (uint64_t)(IOStdReq(io)->io_Offset) | ((uint64_t)(IOStdReq(io)->io_Actual) << 32);
+	uint32_t count = IOStdReq(io)->io_Length;
+
+	D(bug("[MSS-dev] Read64(%04x%08x, %08x) with bs %d\n", IOStdReq(io)->io_Actual, IOStdReq(io)->io_Offset, IOStdReq(io)->io_Length, unit->msu_blockSize));
+
+	io->io_Error = 0;
+
+	if (!IOStdReq(io)->io_Data)
+	{
+		D(bug("[MSS-dev] No buffer!\n"));
+		io->io_Error = IOERR_BADADDRESS;
+	}
+	else if ((block % unit->msu_blockSize) || (count % unit->msu_blockSize))
+	{
+		D(bug("[MSS-dev] offset or length not sector-aligned\n"));
+		cmd_Invalid(io, dev, unit);
+	}
+	else
+	{
+		block /= unit->msu_blockSize;
+		count /= unit->msu_blockSize;
+		uint32_t cnt = 0;
+
+		if (block + count > unit->msu_blockCount)
+		{
+			D(bug("[MSS-dev] ERROR! Requested read outside the available area\n"));
+			io->io_Error = IOERR_BADADDRESS;
+		}
+		else
+		{
+			HIDD_USBStorage_Read(unit->msu_object, unit->msu_unitNum & 0xf, IOStdReq(io)->io_Data, block, count);
+			IOStdReq(io)->io_Actual = IOStdReq(io)->io_Length;
+		}
+	}
+
+	D(bug("[MSS-dev] Read64 returns %d\n", io->io_Error));
+}
+
+static void cmd_Write32(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	uint32_t block = IOStdReq(io)->io_Offset;
+	uint32_t count = IOStdReq(io)->io_Length;
+
+	D(bug("[MSS-dev] Write32(%08x, %08x) with bs %d\n", IOStdReq(io)->io_Offset, IOStdReq(io)->io_Length, unit->msu_blockSize));
+
+	if (!IOStdReq(io)->io_Data)
+	{
+		D(bug("[MSS-dev] No buffer!\n"));
+		io->io_Error = IOERR_BADADDRESS;
+	}
+	else if ((block % unit->msu_blockSize) || (count % unit->msu_blockSize))
+	{
+		D(bug("[MSS-dev] offset or length not sector-aligned\n"));
+		cmd_Invalid(io, dev, unit);
+	}
+	else
+	{
+		block /= unit->msu_blockSize;
+		count /= unit->msu_blockSize;
+		uint32_t cnt = 0;
+
+		if (block + count > unit->msu_blockCount)
+		{
+			D(bug("[MSS-dev] ERROR! Requested read outside the available area\n"));
+			io->io_Error = IOERR_BADADDRESS;
+		}
+		else
+		{
+			HIDD_USBStorage_Write(unit->msu_object, unit->msu_unitNum & 0xf, IOStdReq(io)->io_Data, block, count);
+			IOStdReq(io)->io_Actual = IOStdReq(io)->io_Length;
+		}
+	}
+}
+
+static void cmd_Write64(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit)
+{
+	uint64_t block = (uint64_t)(IOStdReq(io)->io_Offset) | ((uint64_t)(IOStdReq(io)->io_Actual) << 32);
+	uint32_t count = IOStdReq(io)->io_Length;
+
+	D(bug("[MSS-dev] Write64(%04x%08x, %08x) with bs %d\n", IOStdReq(io)->io_Actual, IOStdReq(io)->io_Offset, IOStdReq(io)->io_Length, unit->msu_blockSize));
+
+	io->io_Error = 0;
+
+	if (!IOStdReq(io)->io_Data)
+	{
+		D(bug("[MSS-dev] No buffer!\n"));
+		io->io_Error = IOERR_BADADDRESS;
+	}
+	else if ((block % unit->msu_blockSize) || (count % unit->msu_blockSize))
+	{
+		D(bug("[MSS-dev] offset or length not sector-aligned\n"));
+		cmd_Invalid(io, dev, unit);
+	}
+	else
+	{
+		block /= unit->msu_blockSize;
+		count /= unit->msu_blockSize;
+		uint32_t cnt = 0;
+
+		if (block + count > unit->msu_blockCount)
+		{
+			D(bug("[MSS-dev] ERROR! Requested read outside the available area\n"));
+			io->io_Error = IOERR_BADADDRESS;
+		}
+		else
+		{
+			HIDD_USBStorage_Write(unit->msu_object, unit->msu_unitNum & 0xf, IOStdReq(io)->io_Data, block, count);
+			IOStdReq(io)->io_Actual = IOStdReq(io)->io_Length;
+		}
+	}
+
+	D(bug("[MSS-dev] Write64 returns %d\n", io->io_Error));
+}
+
+static const uint16_t supported_commands[] = {
+		CMD_RESET, CMD_READ, CMD_WRITE, CMD_UPDATE, CMD_CLEAR, CMD_START, CMD_STOP, CMD_FLUSH,
+		TD_MOTOR, TD_SEEK, TD_FORMAT, TD_REMOVE, TD_CHANGENUM, TD_CHANGESTATE, TD_PROTSTATUS,
+		TD_GETNUMTRACKS, TD_ADDCHANGEINT, TD_REMCHANGEINT, TD_GETGEOMETRY, TD_EJECT,
+		TD_READ64, TD_WRITE64, TD_SEEK64, TD_FORMAT64,
+		HD_SCSICMD, TD_GETDRIVETYPE,
+		NSCMD_DEVICEQUERY, NSCMD_TD_READ64, NSCMD_TD_WRITE64, NSCMD_TD_SEEK64, NSCMD_TD_FORMAT64,
+		0
+};
+
+static const void (*map32[HD_SCSICMD+1])(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit) = {
+		[CMD_INVALID]		= cmd_Invalid,
+	    [CMD_RESET]     	= cmd_Reset,
+	    [CMD_READ]      	= cmd_Read32,
+	    [CMD_WRITE]     	= cmd_Write32,
+	    [CMD_UPDATE]    	= cmd_Invalid,
+	    [CMD_CLEAR]     	= cmd_Invalid,
+	    [CMD_STOP]      	= cmd_Invalid,
+	    [CMD_START]     	= cmd_Invalid,
+	    [CMD_FLUSH]     	= cmd_Invalid,
+	    [TD_MOTOR]      	= cmd_NULL,
+	    [TD_SEEK]       	= cmd_NULL,
+	    [TD_FORMAT]     	= cmd_Write32,
+	    [TD_REMOVE]     	= cmd_Invalid,
+	    [TD_CHANGENUM]  	= cmd_Invalid,
+	    [TD_CHANGESTATE]	= cmd_Invalid,
+	    [TD_PROTSTATUS] 	= cmd_Invalid,
+	    [TD_RAWREAD]    	= cmd_Invalid,
+	    [TD_RAWWRITE]   	= cmd_Invalid,
+	    [TD_GETNUMTRACKS]   = cmd_Invalid,
+	    [TD_ADDCHANGEINT]   = cmd_AddChangeInt,
+	    [TD_REMCHANGEINT]   = cmd_RemChangeInt,
+	    [TD_GETGEOMETRY]	= cmd_GetGeometry,
+	    [TD_EJECT]      	= cmd_Invalid,
+	    [TD_READ64]     	= cmd_Read64,
+	    [TD_WRITE64]    	= cmd_Write64,
+	    [TD_SEEK64]     	= cmd_NULL,
+	    [TD_FORMAT64]   	= cmd_Write64,
+	    [HD_SCSICMD]    	= cmd_Invalid,
+};
+
+static const void (*map64[4])(struct IORequest *io, mss_device_t *dev, mss_unit_t *unit) = {
+		[NSCMD_TD_READ64 - 0xc000] 		= cmd_Read64,
+		[NSCMD_TD_WRITE64 - 0xc000] 	= cmd_Write64,
+		[NSCMD_TD_SEEK64 - 0xc000] 		= cmd_NULL,
+		[NSCMD_TD_FORMAT64 - 0xc000]	= cmd_Write64,
+};
+
+/*
+ * IO handler. It may be either called directly, or through the mass storage
+ * unit task.
+ */
+void HandleIO(struct IORequest *io, mss_device_t *device, mss_unit_t *unit)
+{
+	io->io_Error = 0;
+
+	/* Some commands will be handled directly */
+	switch (io->io_Command)
+	{
+		/*
+			New Style Devices query. Introduce self as trackdisk and provide list of
+			commands supported
+		 */
+		case NSCMD_DEVICEQUERY:
+		{
+			struct NSDeviceQueryResult *nsdq = (struct NSDeviceQueryResult *)IOStdReq(io)->io_Data;
+			nsdq->DevQueryFormat    = 0;
+			nsdq->SizeAvailable     = sizeof(struct NSDeviceQueryResult);
+			nsdq->DeviceType        = NSDEVTYPE_TRACKDISK;
+			nsdq->DeviceSubType     = 0;
+			nsdq->SupportedCommands = (uint16_t *)supported_commands;
+			IOStdReq(io)->io_Actual = sizeof(struct NSDeviceQueryResult);
+			break;
+		}
+
+		/*
+			New Style Devices report here the 'NSTY' - only if such value is
+			returned here, the NSCMD_DEVICEQUERY might be called. Otherwice it should
+			report error.
+		 */
+		case TD_GETDRIVETYPE:
+			IOStdReq(io)->io_Actual = DRIVE_NEWSTYLE;
+			break;
+
+		default:
+			if (io->io_Command <= HD_SCSICMD)
+			{
+				if (map32[io->io_Command])
+					map32[io->io_Command](io, device, unit);
+				else
+					cmd_Invalid(io, device, unit);
+			}
+			else if (io->io_Command >= NSCMD_TD_READ64 && io->io_Command <= NSCMD_TD_FORMAT64)
+			{
+				if (map64[io->io_Command - NSCMD_TD_READ64])
+					map64[io->io_Command - NSCMD_TD_READ64](io, device, unit);
+				else
+					cmd_Invalid(io, device, unit);
+			}
+			break;
+	}
+}
+
+
+
+/*
+ * Exec device functions
+ */
+
+static
+AROS_LH3 (void, dev_OpenLib,
+    AROS_LHA(struct IORequest *, ioreq, A1),
+    AROS_LHA(ULONG, unitnum, D0),
+    AROS_LHA(ULONG, flags, D1),
+    mss_device_t *, lh, 1, Storage)
+{
+	AROS_LIBFUNC_INIT
+
+	mss_unit_t *unit, *found = NULL;
+
+	ioreq->io_Error = IOERR_OPENFAIL;
+
+	D(bug("[MSS-dev] Open(%p, %x, %x)\n", ioreq, unitnum, flags));
+
+	/* Lock the whole mass storage class and search for a requested unit */
+	ObtainSemaphore(&lh->mss_static->Lock);
+	ForeachNode(&lh->mss_static->unitList, unit)
+	{
+		if (unit->msu_unitNum == unitnum)
+		{
+			found = unit;
+			break;
+		}
+	}
+	if (found)
+	{
+		ioreq->io_Device = &lh->mss_device;
+		ioreq->io_Unit = found;
+		ioreq->io_Error = 0;
+
+		found->msu_unit.unit_OpenCnt++;
+	}
+	ReleaseSemaphore(&lh->mss_static->Lock);
+
+	D(bug("[MSS-dev] %sfound unit %d at %p\n", found?"":"not ", unitnum, found));
+
+	AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH1(BPTR, dev_CloseLib,
+    AROS_LHA(struct IORequest *, ioreq, A1),
+    mss_device_t *, lh, 2, Storage)
+{
+    AROS_LIBFUNC_INIT
+
+    /* Release the unit */
+    ObtainSemaphore(&lh->mss_static->Lock);
+	mss_unit_t *unit = (mss_unit_t *)ioreq->io_Unit;
+
+	ioreq->io_Unit = (struct Unit *)~0;
+	if (unit && unit->msu_unit.unit_OpenCnt)
+		unit->msu_unit.unit_OpenCnt--;
+
+	ReleaseSemaphore(&lh->mss_static->Lock);
+
+    return NULL;
+
+    AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH1 (BPTR, dev_ExpungeLib,
+    AROS_LHA(mss_device_t *, extralh, D0),
+    mss_device_t *, lh, 3, Storage)
+{
+    AROS_LIBFUNC_INIT
+    return NULL;
+    AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH0 (LIBBASETYPEPTR, dev_ExtFuncLib,
+    mss_device_t *, lh, 4, Storage)
+{
+    AROS_LIBFUNC_INIT
+    return NULL;
+    AROS_LIBFUNC_EXIT
+}
+
+/*
+ * Helper function used to test, whether the specified command is slow or not.
+ * Slow commands will be transfered to the unit task, all others will be handled
+ * immediately.
+ *
+ * NOTE: Every command performing *any* USB transfer/operation is considered slow.
+ */
+static const uint64_t immediate_cmds = (
+			(1 << CMD_INVALID) |
+			(1 << CMD_CLEAR) |
+			(1 << TD_MOTOR) |
+			(1 << TD_SEEK) |
+			(1 << TD_RAWREAD) |
+			(1 << TD_RAWWRITE) |
+			(1 << TD_ADDCHANGEINT) |
+			(1 << TD_REMCHANGEINT) |
+			(1 << TD_GETGEOMETRY) |
+			(1 << TD_SEEK64) |
+			(1 << TD_GETDRIVETYPE)
+		);
+
+static BOOL isSlow(uint32_t cmd)
+{
+	BOOL slow = TRUE;
+
+	if (cmd < 32)
+	{
+		if (immediate_cmds & (1 << cmd))
+			slow = FALSE;
+	}
+	else if (cmd == NSCMD_TD_SEEK64 || cmd == NSCMD_DEVICEQUERY)
+		slow = FALSE;
+
+	D(bug("[MSS-dev] isSlow(%04x) = %d\n", cmd, slow));
+
+	return slow;
+}
+
+static
+AROS_LH1(void, dev_BeginIO,
+    AROS_LHA(struct IORequest *, io, A1),
+    mss_device_t *, mss_dev, 5, Storage)
+{
+    AROS_LIBFUNC_INIT
+
+    D(bug("[MSS-dev] BeginIO. CMD=%04x\n", io->io_Command));
+
+    if (io)
+    {
+    	mss_unit_t *unit = (mss_unit_t *)io->io_Unit;
+
+    	if (unit && unit != (void *)0xffffffff)
+    	{
+    		if (isSlow(io->io_Command))
+    		{
+				D(bug("[MSS-dev] Forwarding IO call to device task\n"));
+				unit->msu_unit.unit_flags |= UNITF_INTASK;
+				io->io_Flags &= ~IOF_QUICK;
+
+				PutMsg(&unit->msu_unit.unit_MsgPort, &io->io_Message);
+
+				unit->msu_unit.unit_flags &= ~UNITF_INTASK;
+    		}
+    		else
+    			HandleIO(io, mss_dev, unit);
+    	}
+    }
+
+    AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH1(LONG, dev_AbortIO,
+    AROS_LHA(struct IORequest *, io, A1),
+    mss_device_t *, mss_dev, 6, Storage)
+{
+    AROS_LIBFUNC_INIT
+
+    /* Cannot Abort IO */
+    return 0;
+
+    AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH1(ULONG, dev_GetRdskLba,
+    AROS_LHA(struct IORequest *, io, A1),
+    mss_device_t *, mss_dev, 7, Storage)
+{
+    AROS_LIBFUNC_INIT
+
+    return 0;
+
+    AROS_LIBFUNC_EXIT
+}
+
+static
+AROS_LH1(ULONG, dev_GetBlkSize,
+    AROS_LHA(struct IORequest *, io, A1),
+    mss_device_t *, mss_dev, 8, Storage)
+{
+    AROS_LIBFUNC_INIT
+
+    mss_unit_t *unit = (mss_unit_t *)io->io_Unit;
+
+    if (unit->msu_blockSize)
+    	return AROS_LEAST_BIT_POS(unit->msu_blockSize);
+    else
+    	return 9;
+
+    AROS_LIBFUNC_EXIT
+}
+
+static const APTR Storage_dev_FuncTable[]=
+{
+    &AROS_SLIB_ENTRY(dev_OpenLib,Storage),
+    &AROS_SLIB_ENTRY(dev_CloseLib,Storage),
+    &AROS_SLIB_ENTRY(dev_ExpungeLib,Storage),
+    &AROS_SLIB_ENTRY(dev_ExtFuncLib,Storage),
+    &AROS_SLIB_ENTRY(dev_BeginIO,Storage),
+    &AROS_SLIB_ENTRY(dev_AbortIO,Storage),
+    &AROS_SLIB_ENTRY(dev_GetRdskLba,Storage),
+    &AROS_SLIB_ENTRY(dev_GetBlkSize,Storage),
+    (void *)-1
+};
+
+static int StorageDev_Init(LIBBASETYPEPTR LIBBASE)
+{
+	D(bug("[MSS] USB Mass Storage device layer init\n"));
+
+	mss_device_t *dev = (mss_device_t *)
+		((intptr_t)AllocPooled(SD(LIBBASE)->MemPool,
+				sizeof(mss_device_t) + sizeof(Storage_dev_FuncTable)) + sizeof(Storage_dev_FuncTable));
+
+	dev->mss_static = SD(LIBBASE);
+
+	dev->mss_device.dd_Library.lib_Node.ln_Name = "usbmss.device";
+	dev->mss_device.dd_Library.lib_Node.ln_Type = NT_DEVICE;
+	dev->mss_device.dd_Library.lib_Node.ln_Pri = 0;
+	dev->mss_device.dd_Library.lib_Version = 1;
+	dev->mss_device.dd_Library.lib_Revision = 0;
+
+	MakeFunctions(dev, Storage_dev_FuncTable, NULL);
+	AddDevice(dev);
+
+	return TRUE;
+}
+
+static int StorageDev_Expunge(LIBBASETYPEPTR LIBBASE)
+{
+	return FALSE;
+}
+
+ADD2INITLIB(StorageDev_Init, 1)
+ADD2EXPUNGELIB(StorageDev_Expunge, 1)
--- contrib/necessary/USB/classes/MassStorage/storage_task.c
+++ contrib/necessary/USB/classes/MassStorage/storage_task.c
@@ -7,54 +7,172 @@
 
 #define DEBUG 1
 #include <aros/debug.h>
+#include <devices/timer.h>
+#include <devices/trackdisk.h>
+#include <devices/scsidisk.h>
+#include <devices/newstyle.h>
+#include <exec/io.h>
+#include <exec/ports.h>
+#include <exec/devices.h>
+#include <exec/errors.h>
 #include "storage.h"
 
-void StorageTask(OOP_Class *cl, OOP_Object *o, uint8_t lun, struct Task *parent)
+/*
+ * Unit task.
+ * Every single mass storage unit in the system becomes it's own handler task.
+ * The task's job is to request periodically the media sense and report any potential
+ * disk change soft interrupts. Additionally, it is used to perform all *slow*
+ * IO operations.
+ *
+ * NOTE: Multi-lun usb devices will get more than one unit helper task.
+ */
+void StorageTask(OOP_Class *cl, OOP_Object *o, uint32_t unitnum, struct Task *parent)
 {
 	const char *name = FindTask(NULL)->tc_Node.ln_Name;
 	StorageData *mss = OOP_INST_DATA(cl, o);
+	uint8_t lun = unitnum & 0x0f;
 	uint32_t sigset = SIGBREAKF_CTRL_C;
 	uint32_t rcvd;
-	uint32_t capacity[2];
+	mss_unit_t *unit;
+	struct timerequest *timer_io;
 
-	D(bug("[%s] Good morning!\n", name));
+	/* Make sure the access is exclusive as long as the init is not ready */
+	ObtainSemaphore(&mss->lock);
 
-	if (HIDD_USBStorage_ReadCapacity(o, lun, &capacity[0], &capacity[1]))
+	/* Initialize the unit */
+	unit = (mss_unit_t *)AllocVecPooled(SD(cl)->MemPool, sizeof(mss_unit_t));
+	unit->msu_unit.unit_MsgPort.mp_SigBit = AllocSignal(-1);
+	unit->msu_unit.unit_MsgPort.mp_Flags = PA_SIGNAL;
+	unit->msu_unit.unit_MsgPort.mp_SigTask = FindTask(NULL);
+	unit->msu_unit.unit_MsgPort.mp_Node.ln_Type = NT_MSGPORT;
+	unit->msu_class = cl;
+	unit->msu_object = o;
+	NEWLIST(&unit->msu_unit.unit_MsgPort.mp_MsgList);
+	NEWLIST(&unit->msu_diskChangeList);
+
+	sigset |= 1 << unit->msu_unit.unit_MsgPort.mp_SigBit;
+
+	if (unit)
 	{
-		D(bug("[MSS] Detected capacity: %dMB\n", (((capacity[0]+1) >> 20)*capacity[1])));
-	}
+		D(bug("[%s] Good morning!\n", name));
 
-	uint8_t buff[1024];
-	D(bug("[MSS] trying Read\n"));
-
-	HIDD_USBStorage_Read(o, lun, buff, 10, 2);
-
-	int x,y;
-	for (y=0; y < 1024/16; y++)
-	{
-		D(bug("[MSS] %04x:  ", y*16));
-		for (x=0; x < 16; x++)
+		if (HIDD_USBStorage_ReadCapacity(o, unitnum & 0xf, &unit->msu_blockCount, &unit->msu_blockSize))
 		{
-			D(bug("%02x ", buff[16*y + x]));
+			D(bug("[MSS] Detected capacity: %dMB\n", (((unit->msu_blockCount+1) >> 10)*unit->msu_blockSize) >> 10));
 		}
-		D(bug("    "));
-		for (x=0; x < 16; x++)
-		{
-			if (buff[16*y + x] >= 0x20 && buff[16*y + x] <= 0x7e)
-				D(bug("%c", (buff[16*y + x]) ));
+
+		unit->msu_unitNum = unitnum;
+
+		/* timer request will be used to pool periodically for disk changes */
+		timer_io = CreateIORequest(CreateMsgPort(), sizeof(struct timerequest));
+		OpenDevice("timer.device", UNIT_MICROHZ, timer_io, 0);
+
+		sigset |= 1 << timer_io->tr_node.io_Message.mn_ReplyPort->mp_SigBit;
+
+		AddTail(&SD(cl)->unitList, unit);
+
+		/* Get rid of exclusive access */
+		ReleaseSemaphore(&mss->lock);
+
+		/* Set up idle timer */
+		timer_io->tr_node.io_Command = TR_ADDREQUEST;
+		timer_io->tr_time.tv_secs = 2;
+		timer_io->tr_time.tv_micro = 0;
+		SendIO(timer_io);
+
+		/* Tell the initiator that we're done with setup */
+		Signal(parent, SIGF_SINGLE);
+
+		/* Enter almost endless loop */
+		do {
+			rcvd = Wait(sigset);
+
+			D(bug("[%s] sigset=%08x, got %08x\n", name, sigset, rcvd));
+
+			/* Got a timer request? Handle it (TestUnitReady) now */
+			if (rcvd & (1 << timer_io->tr_node.io_Message.mn_ReplyPort->mp_SigBit))
+			{
+				/* Timer request. Perform periodic media sense */
+
+				while (GetMsg(timer_io->tr_node.io_Message.mn_ReplyPort));
+
+				uint8_t unit_ready;
+				/* Device is idling. Do media change sense */
+				D(bug("[%s] Idle: Sensing...\n", name));
+
+				unit_ready = HIDD_USBStorage_TestUnitReady(o, unitnum & 0xf);
+
+				if (!unit_ready)
+				{
+					uint8_t sense[19];
+					int i;
+					D(bug("[%s] Unit was not ready. Requesting sense.\n", name));
+
+					HIDD_USBStorage_RequestSense(o, unitnum & 0xf, sense, 19);
+
+					D(bug("[%s] Sense: ", name));
+					for (i=0; i < 19; i++)
+						D(bug("%02x ", sense[i]));
+					D(bug("\n"));
+				}
+			}
 			else
-				D(bug("."));
-		}
-		D(bug("\n"));
+			{
+				/*
+				 * Something else appeared? Cancel the timer request.
+				 */
+				D(bug("[%s] Aborting timer io\n", name));
+				if (!CheckIO(timer_io))
+					AbortIO(timer_io);
+				WaitIO(timer_io);
+				SetSignal(0, 1 << timer_io->tr_node.io_Message.mn_ReplyPort->mp_SigBit);
+			}
+
+			/* Message in IORequest queue? */
+			if (rcvd & (1 << unit->msu_unit.unit_MsgPort.mp_SigBit))
+			{
+				/* If unit is active, do not handle the IO request */
+				if (!(unit->msu_unit.unit_flags & UNITF_ACTIVE))
+				{
+					unit->msu_unit.unit_flags |= UNITF_ACTIVE;
+					struct IORequest *msg = NULL;
+
+					/* Empty the queue */
+					while ((msg = (struct IORequest *)GetMsg(&unit->msu_unit.unit_MsgPort)))
+					{
+						D(bug("[%s] CMD=%04x\n", name, msg->io_Command));
+
+						HandleIO(msg, msg->io_Device, unit);
+
+						if (msg->io_Command != TD_ADDCHANGEINT)
+							ReplyMsg((struct Message *)msg);
+					}
+
+					unit->msu_unit.unit_flags &= ~(UNITF_ACTIVE);
+				}
+			}
+
+			/*
+			 * Set up idle timer. The main loop of the task will be waken up
+			 * after two seconds
+			 */
+			timer_io->tr_node.io_Command = TR_ADDREQUEST;
+			timer_io->tr_time.tv_secs = 2;
+			timer_io->tr_time.tv_micro = 0;
+			SendIO(timer_io);
+		} while(!(rcvd & SIGBREAKF_CTRL_C));
+
+		if (!CheckIO(timer_io))
+			AbortIO(timer_io);
+		WaitIO(timer_io);
+
+		/* CTRL_C sent? Make a nice cleanup. */
+		DeleteMsgPort(timer_io->tr_node.io_Message.mn_ReplyPort);
+		DeleteIORequest(timer_io);
+
+		Remove(unit);
+		FreeVecPooled(SD(cl)->MemPool, unit);
 	}
-
-	/* Tell the initiator that we're done with setup */
-	Signal(parent, SIGF_SINGLE);
-	/* Enter almost endless loop */
-	do {
-		rcvd = Wait(SIGBREAKF_CTRL_C);
-
-	} while(!(rcvd & SIGBREAKF_CTRL_C));
 
 	D(bug("[%s] Goodbye!\n", name));
 }
