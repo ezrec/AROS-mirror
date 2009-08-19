a978081db49df1c89f5c837f3debc18eafdb145a
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2009-08-19 05:44:23 +0000

    Delete the 'now obsolete' 7.0.3 port of Mesa
    git-svn-id: https://svn.aros.org/svn/aros/trunk@31724 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/gfx/mesa7/lib/src/mesa/drivers/aros/aros_context_functions.h
+++
@@ -1,9 +0,0 @@
-#if !defined(AROS_CONTEXT_FUNCTIONS_H)
-#define AROS_CONTEXT_FUNCTIONS_H
-
-#include <GL/gl.h>
-#include "AROSMesa_intern.h"
-
-void aros_delete_context(AROSMesaContext aros_ctx);
-
-#endif /* AROS_CONTEXT_FUNCTIONS_H */
