718178954e02b57c1cf4a7310455e3b6c5e5a908
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-02-02 23:23:38 +0000

    Use strerror on AROS to get the errno descriptions.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@32561 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/Networking/libs/caf/include/caf/errors.h
+++ contrib/Networking/libs/caf/include/caf/errors.h
@@ -1,6 +1,7 @@
 #ifndef ERRORS_H
 #define ERRORS_H
 
+#ifdef __MORPHOS__
 char *GetErrnoDesc(int err_code);
 
 struct ErrnoEntry 
@@ -8,5 +9,9 @@ struct ErrnoEntry
 	int error_code;
 	char *error_desc;
 };
+#else
+#include <string.h>
+#define GetErrnoDesc strerror
+#endif
 
 #endif
