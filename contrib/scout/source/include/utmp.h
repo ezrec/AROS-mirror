55a2a4c8b31bd473c8153d46d16a7a6a35c23fdd
sonic <sonic@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2010-04-16 05:52:49 +0000

    Added forgotten file
    git-svn-id: https://svn.aros.org/svn/aros/trunk@33003 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/scout/source/include/utmp.h
@@ -0,0 +1,40 @@
+/*
+ *	Definitions of utmp and lastlog structures for 32 bit C compilers
+ *
+ *      Copyright © 1994 AmiTCP/IP Group,
+ *                       Network Solutions Development, Inc.
+ *                       All rights reserved.
+ *
+ *	    $Id: utmp.h,v 1.1 2006/12/15 08:51:40 sonic_amiga Exp $
+ */
+
+#ifndef	_UTMP_H_
+#define	_UTMP_H_
+
+#define	_PATH_UTMP	"AmiTCP:log/utmp"
+#define	_PATH_WTMP	"AmiTCP:log/wtmp"
+#define	_PATH_LASTLOG	"AmiTCP:log/lastlog"
+
+#define	UT_NAMESIZE	32
+#define	UT_LINESIZE	32
+#define	UT_HOSTSIZE	64
+
+struct lastlog {
+  long	ll_time;		      /* the login time */
+  uid_t ll_uid;		          /* user ID */
+  char  ll_name[UT_NAMESIZE]; /* the login name */
+  char	ll_host[UT_HOSTSIZE]; /* where the login originated */
+};
+
+#define ll_line ll_host
+
+struct utmp {
+  long	  ut_time;	            /* the login time */
+  long    ut_sid;               /* session ID */
+  char    ut_name[UT_NAMESIZE];	/* the login name */
+  char    ut_host[UT_HOSTSIZE];	/* where the login originated */
+};
+
+#define ut_line ut_host
+
+#endif /* !_UTMP_H_ */
