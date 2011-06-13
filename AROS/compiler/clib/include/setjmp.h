0bec838ca1a64bb37290f253e2fa1d805b422cbd
Staf Verhaegen <verhaegs@hpdc7100.(none)>
2011-06-13 20:03:57 +0200

    jmpbuf size on m68k

--- AROS/compiler/clib/include/setjmp.h
+++ AROS/compiler/clib/include/setjmp.h
@@ -12,7 +12,10 @@
 #include <aros/system.h>
 
 #ifdef __mc68000__
-#   define _JMPLEN 12
+    /* %d2-%d7/%a2-%a7, and Relbase Depth from 
+     * *(ULONG *)(FindTask(NULL)->tc_SPLower)
+     */
+#   define _JMPLEN 12+1
 #elif __i386__
 #   define _JMPLEN 8
 #elif __x86_64__
