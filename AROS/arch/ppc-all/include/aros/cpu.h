60d10e7452858a43f2602eb8e9a2040695b4b019
Jason S. McMullan <jason.mcmullan@gmail.com>
2011-06-13 22:07:25 -0400

    cpu.h: Fix compile errors in 'makecountry' when cross-compiling
    We need the host CPU's version of cpu.h when building 'makecountry'.
    
    Signed-off-by: Jason S. McMullan <jason.mcmullan@netronome.com>

--- AROS/arch/ppc-all/include/aros/cpu.h
+++ AROS/arch/ppc-all/include/aros/cpu.h
@@ -126,7 +126,9 @@ struct JumpVec
    unused junk code but otherwise we can't pass input arguments
    to the asm statement
 */
+#ifdef __AROS__
 #error the __AROS_LIBFUNCSTUB needs to be update for RELBASE handling
+#endif
 #define __AROS_LIBFUNCSTUB(fname, libbasename, lvo) \
     void __ ## fname ## _ ## libbasename ## _wrapper(void) \
     { \
