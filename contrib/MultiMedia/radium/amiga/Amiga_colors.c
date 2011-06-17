edd407334a3e43479a9c54a1c2f9bd621dd42e67
weissms <weissms@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-06-17 23:59:56 +0000

    Build fix.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@39351 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/MultiMedia/radium/amiga/Amiga_colors.c
+++ contrib/MultiMedia/radium/amiga/Amiga_colors.c
@@ -36,7 +36,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA. */
 extern struct Screen *mainscreen;
 
 #ifndef rtPaletteRequest
-LONG rtPaletteRequest(char *title,struct rtReqInfo *reqinfo,Tag tag, ...){
+LONG rtPaletteRequest(const char *title,struct rtReqInfo *reqinfo,Tag tag, ...){
 	return rtPaletteRequestA(title,reqinfo,(struct TagItem *)&tag);
 }
 #endif
