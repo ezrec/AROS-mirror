9acaf2d2bbe600ef7b8a6dfb526627addb309a82
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-07-02 15:16:25 +0000

    Implement emulated gettimeofday
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@39727 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/workbench/hidds/hidd.nouveau/arosc_emul.c
+++ AROS/workbench/hidds/hidd.nouveau/arosc_emul.c
@@ -5,6 +5,7 @@
 
 #include <aros/symbolsets.h>
 #include <proto/dos.h>
+#include <proto/timer.h>
 
 #define DEBUG 0
 #include <aros/debug.h>
@@ -137,7 +138,27 @@ char *getenv (const char *name)
 
 int gettimeofday (struct timeval * tv,struct timezone * tz)
 {
-    IMPLEMENT();
+    struct MsgPort * timerport = CreateMsgPort();
+    struct timerequest * timereq = (struct timerequest *)CreateIORequest(timerport, sizeof(*timereq));
+
+
+    if (timereq)
+    {
+        if (OpenDevice("timer.device", UNIT_VBLANK, (struct IORequest *)timereq, 0) == 0)
+        {
+            #define TimerBase ((struct Device *)timereq->tr_node.io_Device)
+
+            GetSysTime(tv);
+            
+            #undef TimerBase
+            
+            CloseDevice((struct IORequest *)timereq);
+        }
+    }
+    
+    DeleteIORequest((struct IORequest *)timereq);
+    DeleteMsgPort(timerport);
+
     return 0;
 }
 
