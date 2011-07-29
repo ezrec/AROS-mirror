c1d6d401ed14005036fea79b625eb9fc165d0c89
Jason S. McMullan <jason.mcmullan@gmail.com>
2011-07-29 10:00:08 -0400

    mount.library: WIP
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

---
+++ AROS/rom/mount/include/libraries/mount.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2011, The AROS Development Team.  All rights reserved.
+ * Author: Jason S. McMullan <jason.mcmullan@gmail.com>
+ *
+ * Licensed under the AROS PUBLIC LICENSE (APL) Version 1.1
+ */
+
+#ifndef MOUNT_H
+#define MOUNT_H
+
+
+#endif /* MOUNT_H */
