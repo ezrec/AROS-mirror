08a734386c2132a8608143d6c8c3753f85bbd264
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-08-08 15:59:28 +0000

    Use MUIOBJMACRO_START so that it can be used with MUIMASTER_YES_INLINE_STDARG Add "ul" to all attributes for 64-bit compatibility (untested)
    git-svn-id: https://svn.aros.org/svn/aros/trunk@40599 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/mui/classes/urltext/Include/MUI/Urltext_mcc.h
+++ contrib/mui/classes/urltext/Include/MUI/Urltext_mcc.h
@@ -24,28 +24,28 @@
 /***********************************************************************/
 
 #define MUIC_Urltext       "Urltext.mcc"
-#define UrltextObject      MUI_NewObject(MUIC_Urltext
+#define UrltextObject      MUIOBJMACRO_START(MUIC_Urltext)
 
 /***********************************************************************/
 
 /* Attributes - Scheme is: [ISGN] */
-#define MUIA_Urltext_Url            0xFEC900D0 /* [I.GN] (STRPTR) */
-#define MUIA_Urltext_Text           0xFEC900D1 /* [I.G.] (STRPTR) */
-#define MUIA_Urltext_Active         0xFEC900D2 /* [..G.] (BOOL)   */
-#define MUIA_Urltext_Visited        0xFEC900D3 /* [..GN] (BOOL)   */
-#define MUIA_Urltext_Underline      0xFEC900D4 /* [I...] (BOOL)   */
-#define MUIA_Urltext_FallBack       0xFEC900D5 /* [I...] (BOOL)   */
-#define MUIA_Urltext_DoVisitedPen   0xFEC900D6 /* [I...] (BOOL)   */
-#define MUIA_Urltext_SetMax         0xFEC900D7 /* [I...] (BOOL)   */
-#define MUIA_Urltext_DoOpenURL      0xFEC900D8 /* [I...] (BOOL)   */
-#define MUIA_Urltext_NoMenu         0xFEC900D9 /* [I...] (BOOL)   */
-#define MUIA_Urltext_NoOpenURLPrefs 0xFEC900DC /* [I...] (BOOL)   */
+#define MUIA_Urltext_Url            0xFEC900D0ul /* [I.GN] (STRPTR) */
+#define MUIA_Urltext_Text           0xFEC900D1ul /* [I.G.] (STRPTR) */
+#define MUIA_Urltext_Active         0xFEC900D2ul /* [..G.] (BOOL)   */
+#define MUIA_Urltext_Visited        0xFEC900D3ul /* [..GN] (BOOL)   */
+#define MUIA_Urltext_Underline      0xFEC900D4ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_FallBack       0xFEC900D5ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_DoVisitedPen   0xFEC900D6ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_SetMax         0xFEC900D7ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_DoOpenURL      0xFEC900D8ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_NoMenu         0xFEC900D9ul /* [I...] (BOOL)   */
+#define MUIA_Urltext_NoOpenURLPrefs 0xFEC900DCul /* [I...] (BOOL)   */
 
 /***********************************************************************/
 
 /* Methods */
-#define MUIM_Urltext_OpenURL        0xFEC900C9 /* struct MUIP_Urltext_OpenURL */
-#define MUIM_Urltext_Copy           0xFEC900CA /* struct MUIP_Urltext_Copy    */
+#define MUIM_Urltext_OpenURL        0xFEC900C9ul /* struct MUIP_Urltext_OpenURL */
+#define MUIM_Urltext_Copy           0xFEC900CAul /* struct MUIP_Urltext_Copy    */
 
 struct MUIP_Urltext_OpenURL
 {
