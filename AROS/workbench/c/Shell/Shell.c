ezrec/AROS-mirror

commit 9714cd1ac6d9bdb4d8d43a3da2f78d277ca6b1b0

Removed unneeded command line parsing, command lines are now always injected. (This also makes Shell more compatible with some AOS commands)
git-svn-id: https://svn.aros.org/svn/aros/trunk@40788 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

AROS/workbench/c/Shell/Shell.c
@@ -80,10 +80,6 @@
 #define DEBUG 0
 #include <aros/debug.h>
 
-#define SH_GLOBAL_SYSBASE 1
-#define SH_GLOBAL_DOSBASE 1
-#include <aros/shcommands.h>
-
 #include "Shell.h"
 
 static void PrintBanner(struct DosLibrary *DOSBase)
@@ -106,19 +102,25 @@ BOOL isInteractive(struct CommandLineInterface *cli)
     return !cli->cli_Background && cli->cli_CurrentInput == cli->cli_StandardInput;
 }
 
-AROS_SH1(Shell, 41.3,
-	 AROS_SHA(STRPTR, , COMMAND, /F, NULL))
+AROS_ENTRY(__startup ULONG, ShellStart,
+	   AROS_UFHA(char *, argstr, A0),
+	   AROS_UFHA(ULONG, argsize, D0),
+	   struct ExecBase *, SysBase)
 {
-    AROS_SHCOMMAND_INIT
+    AROS_USERFUNC_INIT
 
-    struct Process *me = (struct Process *)FindTask(NULL);
-    STRPTR cmdline = SHArg(COMMAND);
+    struct Process *me;
     ShellState ss = {0};
     LONG error;
     BOOL isBootShell;
     BOOL isBannerDone;
+    APTR DOSBase;
 
     D(bug("[Shell] executing\n"));
+    
+    me = (struct Process *)FindTask(NULL);
+    DOSBase = TaggedOpenLibrary(TAGGEDOPEN_DOS);
+    
     setPath(BNULL, DOSBase);
 
     ss.cliNumber = me->pr_TaskNum;
@@ -139,28 +141,15 @@ AROS_SH1(Shell, 41.3,
 	CloseLibrary((struct Library *)ExpansionBase);
     }
 
-    if (cmdline && cmdline[0] != '\0')
-    {
-	Buffer in = { cmdline, cliLen(cmdline), 0, 0 };
-	Buffer out = {0};
-
-	if ((error = Redirection_init(&ss)) == 0)
-	{
-	    D(bug("[Shell] running command: %s\n", cmdline));
-	    error = checkLine(&ss, &in, &out, TRUE, DOSBase);
-	    Redirection_release(&ss, DOSBase);
-
-	    bufferFree(&in);
-	    bufferFree(&out);
-	}
-    }
-    else
-	error = interact(&ss, isBootShell, isBannerDone, DOSBase);
+    error = interact(&ss, isBootShell, isBannerDone, DOSBase);
 
     D(bug("[Shell] exiting, error = %d\n", error));
+    
+    CloseLibrary(DOSBase);
+
     return error ? RETURN_FAIL : RETURN_OK;
 
-    AROS_SHCOMMAND_EXIT
+    AROS_USERFUNC_EXIT
 }
 
 /* First we execute the script, then we interact with the user */

AROS/workbench/c/Shell/shell_init.c
@@ -22,11 +22,13 @@ DEFINESET(SHCOMMANDS)
 #include LC_LIBDEFS_FILE
 
 #undef DOSBase
+
+extern void ShellStart(void);
+
 static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR LIBBASE)
 {
-    struct shcommand *sh;
     APTR DOSBase;
-    int pos;
+    BPTR seg;
 
     D(bug("[Shell] Init\n"));
 
@@ -36,12 +38,10 @@ static int GM_UNIQUENAME(Init)(LIBBASETYPEPTR LIBBASE)
     	return FALSE;
     }
 
-    ForeachElementInSet(SETNAME(SHCOMMANDS), 1, pos, sh) {
-    	BPTR seg = CreateSegList((APTR)sh->sh_Command);
-    	if (seg != BNULL)
-    	    AddSegment(sh->sh_Name, seg, CMD_INTERNAL);
-    }
-
+    seg = CreateSegList(ShellStart);
+    if (seg != BNULL)
+	AddSegment("Shell", seg, CMD_INTERNAL);
+ 
     CloseLibrary(DOSBase);
 
     return TRUE;

commit 4c1492ce430dc7fb637dfdaea5ae97f6c990835b

Fixed Shell problem introduced in r40788.
We can't ignore Shell command line parameters because we don't have (and probably don't want to have) AOS strange and undocumented shell startup packet support.


git-svn-id: https://svn.aros.org/svn/aros/trunk@40811 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

AROS/workbench/c/Shell/Shell.c
@@ -141,7 +141,22 @@ AROS_ENTRY(__startup ULONG, ShellStart,
 	CloseLibrary((struct Library *)ExpansionBase);
     }
 
-    error = interact(&ss, isBootShell, isBannerDone, DOSBase);
+    if (argsize > 0)
+    {
+	Buffer in = { argstr, argsize, 0, 0 };
+	Buffer out = {0};
+
+	if ((error = Redirection_init(&ss)) == 0)
+	{
+	    D(bug("[Shell] running command: %s\n", in->buf));
+	    error = checkLine(&ss, &in, &out, TRUE, DOSBase);
+	    Redirection_release(&ss, DOSBase);
+
+	    bufferFree(&in);
+	    bufferFree(&out);
+	}
+    } else
+	error = interact(&ss, isBootShell, isBannerDone, DOSBase);
 
     D(bug("[Shell] exiting, error = %d\n", error));
     