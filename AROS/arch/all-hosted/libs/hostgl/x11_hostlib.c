12a98ba9f403c299a41876d40514cdb5c19357e7
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-08 05:09:36 +0000

    Leave just the needed functions in X11 host lib.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@41189 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/arch/all-hosted/libs/hostgl/x11_hostlib.c
+++ AROS/arch/all-hosted/libs/hostgl/x11_hostlib.c
@@ -12,132 +12,23 @@
 #include <aros/debug.h>
 
 void *x11_handle = NULL;
-void *libc_handle = NULL;
-void *xf86vm_handle = NULL;
 
 struct x11_func x11_func;
-struct libc_func libc_func;
-struct xf86vm_func xf86vm_func;
-
-static const char *xf86vm_func_names[] = {
-    "XF86VidModeGetAllModeLines",
-    "XF86VidModeSwitchToMode",
-    "XF86VidModeSetViewPort",
-    "XF86VidModeQueryVersion",
-    "XF86VidModeQueryExtension"
-};
-
-#define XF86VM_NUM_FUNCS (5)
 
 static const char *x11_func_names[] = {
-    "XCreateImage",
-    "XInitImage",
-    "XGetImage",
     "XOpenDisplay",
-    "XDisplayName",
-    "XInternAtom",
     "XCreateColormap",
-    "XCreatePixmapCursor",
-    "XCreateFontCursor",
-    "XCreateGC",
-    "XCreatePixmap",
-    "XCreateSimpleWindow",
     "XCreateWindow",
-    "XLookupKeysym",
-    "XMaxRequestSize",
-    "XVisualIDFromVisual",
-    "XSetErrorHandler",
-    "XSetIOErrorHandler",
-    "XSetWMHints",
-    "XSetWMNormalHints",
-    "XSetWMProtocols",
-    "XAutoRepeatOff",
-    "XAutoRepeatOn",
-    "XChangeGC",
-    "XChangeProperty",
-    "XChangeWindowAttributes",
-    "XClearArea",
     "XCloseDisplay",
-    "XConfigureWindow",
-    "XConvertSelection",
-    "XCopyArea",
-    "XCopyPlane",
-    "XDefineCursor",
-    "XDeleteProperty",
     "XDestroyWindow",
-    "XDrawArc",
-    "XDrawLine",
-    "XDrawPoint",
-    "XDrawString",
-    "XEventsQueued",
-    "XFillRectangle",
     "XFlush",
     "XFree",
-    "XFreeColormap",
-    "XFreeGC",
-    "XFreePixmap",
-    "XGetErrorText",
-    "XGetVisualInfo",
-    "XGetWindowProperty",
-    "XGetWindowAttributes",
-    "XGrabKeyboard",
-    "XGrabPointer",
-    "XMapRaised",
-    "XMapWindow",
-    "XNextEvent",
-    "XParseGeometry",
-    "XPending",
-    "XPutImage",
-    "XRecolorCursor",
-    "XRefreshKeyboardMapping",
-    "XSelectInput",
-    "XSendEvent",
-    "XSetBackground",
-    "XSetClipMask",
-    "XSetClipRectangles",
-    "XSetFillStyle",
-    "XSetForeground",
-    "XSetFunction",
-    "XSetIconName",
-    "XSetSelectionOwner",
-    "XStoreColor",
-    "XStoreName",
-    "XSync",
-    "XAllocColor",
-    "XLookupString",
-    "XQueryExtension",
-    "XDefaultScreen",
-    "XRootWindow"
-#if DEBUG_X11_SYNCHRON
-    , "XSynchronize"
-#endif
+    "XMapWindow"
 };
 
-#if DEBUG_X11_SYNCHRON
-#define X11_NUM_FUNCS (79)
-#else
-#define X11_NUM_FUNCS (78)
-#endif
+#define X11_NUM_FUNCS (8)
 
-
-static const char *libc_func_names[] = {
-#if USE_XSHM
-    "ftok",
-    "shmctl",
-    "shmget",
-    "shmat",
-    "shmdt",
-#endif
-    "raise"
-};
-
-#if USE_XSHM
-#define LIBC_NUM_FUNCS (6)
-#else
-#define LIBC_NUM_FUNCS (1)
-#endif
-
-void *HostLibBase;
+APTR HostLibBase;
 
 void *x11_hostlib_load_so(const char *sofile, const char **names, int nfuncs, void **funcptr) {
     void *handle;
@@ -173,13 +64,8 @@ static int x11_hostlib_init(LIBBASETYPEPTR LIBBASE) {
         kprintf("[x11] couldn't open hostlib.resource\n");
         return FALSE;
     }
-    if ((xf86vm_handle = x11_hostlib_load_so(XF86VM_SOFILE, xf86vm_func_names, XF86VM_NUM_FUNCS, (void **) &xf86vm_func)) == NULL)
-        return FALSE;
 
-    if ((x11_handle = x11_hostlib_load_so(X11_SOFILE, x11_func_names, X11_NUM_FUNCS, (void **) &x11_func)) == NULL)
-        return FALSE;
-
-    if ((libc_handle = x11_hostlib_load_so(LIBC_SOFILE, libc_func_names, LIBC_NUM_FUNCS, (void **) &libc_func)) == NULL) {
+    if ((x11_handle = x11_hostlib_load_so(X11_SOFILE, x11_func_names, X11_NUM_FUNCS, (void **) &x11_func)) == NULL) {
         HostLib_Close(x11_handle, NULL);
         return FALSE;
     }
@@ -190,14 +76,8 @@ static int x11_hostlib_init(LIBBASETYPEPTR LIBBASE) {
 static int x11_hostlib_expunge(LIBBASETYPEPTR LIBBASE) {
     D(bug("[x11] hostlib expunge\n"));
 
-    if (xf86vm_handle != NULL)
-        HostLib_Close(xf86vm_handle, NULL);
-
     if (x11_handle != NULL)
         HostLib_Close(x11_handle, NULL);
-
-    if (libc_handle != NULL)
-        HostLib_Close(libc_handle, NULL);
 
     return TRUE;
 }
--- AROS/arch/all-hosted/libs/hostgl/x11_hostlib.h
+++ AROS/arch/all-hosted/libs/hostgl/x11_hostlib.h
@@ -7,174 +7,34 @@
 #ifndef _XLIB_H_
 #   include <X11/Xlib.h>
 #endif
-
-#ifndef _XUTIL_H
-#   include <X11/Xutil.h>
-#endif
 #undef timeval
 
-#if USE_XSHM
-#include <sys/types.h>
-#include <sys/shm.h>
-#endif
-
-// Set to 1 if you want to disable the asynchronic nature of the X11 HIDD.
-// This makes it easier to find the real reason of a misbehavior.
-#define DEBUG_X11_SYNCHRON 0
-
 struct x11_func {
-    XImage * (*XCreateImage) ( Display* , Visual* , unsigned int , int , int , char* , unsigned int , unsigned int , int , int );
-    int (*XInitImage) ( XImage* );
-    XImage * (*XGetImage) ( Display* , Drawable , int , int , unsigned int , unsigned int , unsigned long , int );
     Display * (*XOpenDisplay) ( const char* );
-    char * (*XDisplayName) ( const char* );
-    Atom (*XInternAtom) ( Display* , const char* , int );
     Colormap (*XCreateColormap) ( Display* , Window , Visual* , int );
-    Cursor (*XCreatePixmapCursor) ( Display* , Pixmap , Pixmap , XColor* , XColor* , unsigned int , unsigned int );
-    Cursor (*XCreateFontCursor) ( Display* , unsigned int );
-    GC (*XCreateGC) ( Display* , Drawable , unsigned long , XGCValues* );
-    Pixmap (*XCreatePixmap) ( Display* , Drawable , unsigned int , unsigned int , unsigned int );
-    Window (*XCreateSimpleWindow) ( Display* , Window , int , int , unsigned int , unsigned int , unsigned int , unsigned long , unsigned long );
     Window (*XCreateWindow) ( Display* , Window , int , int , unsigned int , unsigned int , unsigned int , int , unsigned int , Visual* , unsigned long , XSetWindowAttributes* );
-    KeySym (*XLookupKeysym) ( XKeyEvent* , int );
-    long (*XMaxRequestSize) ( Display* );
-    VisualID (*XVisualIDFromVisual) ( Visual* );
-    XErrorHandler (*XSetErrorHandler) ( XErrorHandler );
-    XIOErrorHandler (*XSetIOErrorHandler) ( XIOErrorHandler );
-    int (*XSetWMHints) ( Display* , Window , XWMHints* );
-    void (*XSetWMNormalHints)(Display *display, Window w, XSizeHints *hints);
-    int (*XSetWMProtocols) ( Display* , Window , Atom* , int );
-    int (*XAutoRepeatOff) ( Display* );
-    int (*XAutoRepeatOn) ( Display* );
-    int (*XChangeGC) ( Display* , GC , unsigned long , XGCValues* );
-    int (*XChangeProperty) ( Display* , Window , Atom , Atom , int , int , const unsigned char* , int );
-    int (*XChangeWindowAttributes) ( Display* , Window , unsigned long , XSetWindowAttributes* );
-    int (*XClearArea) ( Display* , Window , int , int , unsigned int , unsigned int , int );
     int (*XCloseDisplay) ( Display* );
-    int (*XConfigureWindow) ( Display* , Window , unsigned int , XWindowChanges* );
-    int (*XConvertSelection) ( Display* , Atom , Atom , Atom , Window , Time );
-    int (*XCopyArea) ( Display* , Drawable , Drawable , GC , int , int , unsigned int , unsigned int , int , int );
-    int (*XCopyPlane) ( Display* , Drawable , Drawable , GC , int , int , unsigned int , unsigned int , int , int , unsigned long );
-    int (*XDefineCursor) ( Display* , Window , Cursor );
-    int (*XDeleteProperty) ( Display* , Window , Atom );
     int (*XDestroyWindow) ( Display* , Window );
-    int (*XDrawArc) ( Display* , Drawable , GC , int , int , unsigned int , unsigned int , int , int );
-    int (*XDrawLine) ( Display* , Drawable , GC , int , int , int , int );
-    int (*XDrawPoint) ( Display* , Drawable , GC , int , int );
-    int (*XDrawString) ( Display* , Drawable , GC , int , int , const char* , int );
-    int (*XEventsQueued) ( Display* , int );
-    int (*XFillRectangle) ( Display* , Drawable , GC , int , int , unsigned int , unsigned int );
     int (*XFlush) ( Display* );
     int (*XFree) ( void* );
-    int (*XFreeColormap) ( Display* , Colormap );
-    int (*XFreeGC) ( Display* , GC );
-    int (*XFreePixmap) ( Display* , Pixmap );
-    int (*XGetErrorText) ( Display* , int , char* , int );
-    XVisualInfo * (*XGetVisualInfo) ( Display* , long , XVisualInfo* , int* );
-    int (*XGetWindowProperty) ( Display* , Window , Atom , long , long , int , Atom , Atom* , int* , unsigned long* , unsigned long* , unsigned char** );
-    int (*XGetWindowAttributes) ( Display* , Window , XWindowAttributes* );
-    int (*XGrabKeyboard) ( Display* , Window , int , int , int , Time );
-    int (*XGrabPointer) ( Display* , Window , int , unsigned int , int , int , Window , Cursor , Time );
-    int (*XMapRaised) ( Display* , Window );
     int (*XMapWindow) ( Display* , Window );
-    int (*XNextEvent) ( Display* , XEvent* );
-    int (*XParseGeometry) ( const char* , int* , int* , unsigned int* , unsigned int* );
-    int (*XPending) ( Display* );
-    int (*XPutImage) ( Display* , Drawable , GC , XImage* , int , int , int , int , unsigned int , unsigned int );
-    int (*XRecolorCursor) ( Display* , Cursor , XColor* , XColor* );
-    int (*XRefreshKeyboardMapping) ( XMappingEvent* );
-    int (*XSelectInput) ( Display* , Window , long );
-    int (*XSendEvent) ( Display* , Window , int , long , XEvent* );
-    int (*XSetBackground) ( Display* , GC , unsigned long );
-    int (*XSetClipMask) ( Display* , GC , Pixmap );
-    int (*XSetClipRectangles) ( Display* , GC , int , int , XRectangle* , int , int );
-    int (*XSetFillStyle) ( Display* , GC , int );
-    int (*XSetForeground) ( Display* , GC , unsigned long );
-    int (*XSetFunction) ( Display* , GC , int );
-    int (*XSetIconName) ( Display* , Window , const char* );
-    int (*XSetSelectionOwner) ( Display* , Atom , Window , Time );
-    int (*XStoreColor) ( Display* , Colormap , XColor* );
-    int (*XStoreName) ( Display* , Window , const char* );
-    int (*XSync) ( Display* , int );
-    int (*XAllocColor) ( Display* , Colormap , XColor* );
-    int (*XLookupString) ( XKeyEvent* , char* , int , KeySym* , XComposeStatus* );
-    int (*XQueryExtension) (Display *, char*, int*, int*, int*);
-    int (*XDefaultScreen) (Display *);
-    Window (*XRootWindow) (Display *, int);
-#if DEBUG_X11_SYNCHRON
-    void (*XSynchronize)(Display *, Bool );
-#endif
 };
-
-#ifndef HAVE_XF86VMODE_H
-
-/* Taken from xf86vmode.h */
-typedef struct {
-    unsigned int	dotclock;
-    unsigned short	hdisplay;
-    unsigned short	hsyncstart;
-    unsigned short	hsyncend;
-    unsigned short	htotal;
-    unsigned short	hskew;
-    unsigned short	vdisplay;
-    unsigned short	vsyncstart;
-    unsigned short	vsyncend;
-    unsigned short	vtotal;
-    unsigned int	flags;
-    int			privsize;
-    long		*c_private;
-} XF86VidModeModeInfo;
-
-#endif
-
-struct xf86vm_func {
-    Bool (*XF86VidModeGetAllModeLines) (Display *, int, int *, XF86VidModeModeInfo***);
-    Bool (*XF86VidModeSwitchToMode) ( Display* , int , XF86VidModeModeInfo* );
-    Bool (*XF86VidModeSetViewPort) ( Display* , int , int , int );
-    Bool (*XF86VidModeQueryVersion) ( Display* , int* , int* );
-    Bool (*XF86VidModeQueryExtension) ( Display* , int* , int* );
-};
-
-struct libc_func {
-#if USE_XSHM
-    key_t (*ftok) (const char *, int);
-    int (*shmctl) (int, int, struct shmid_ds *);
-    int (*shmget) (key_t, size_t, int);
-    void * (*shmat) (int, const void *, int);
-    int (*shmdt) (const void *);
-#endif
-    int (*raise) (int);
-};
-
-extern void *xf86vm_handle;
-extern struct xf86vm_func xf86vm_func;
 
 extern void *x11_handle;
 extern struct x11_func x11_func;
 
-extern void *libc_handle;
-extern struct libc_func libc_func;
-
 #ifdef HOST_OS_linux
 #define X11_SOFILE    "libX11.so.6"
-#define LIBC_SOFILE   "libc.so.6"
-#define XF86VM_SOFILE "libXxf86vm.so.1"
 #endif
 
 #ifdef HOST_OS_darwin
 #define X11_SOFILE    "/usr/X11/lib/libX11.6.dylib"
-#define LIBC_SOFILE   "libSystem.dylib"
-#define XF86VM_SOFILE "/usr/X11/lib/libXxf86vm.1.dylib"
 #endif
 
 #ifndef X11_SOFILE
 #define X11_SOFILE    "libX11.so"
-#define LIBC_SOFILE   "libc.so"
-#define XF86VM_SOFILE "libXxf86vm.so"
 #endif
 
-#define XVMCALL(func,...) (xf86vm_func.func(__VA_ARGS__))
 #define XCALL(func,...) (x11_func.func(__VA_ARGS__))
-#define CCALL(func,...) (libc_func.func(__VA_ARGS__))
 
 #endif
