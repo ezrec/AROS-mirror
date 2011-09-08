562f0e4cdd460d01f9da616d46bdfdc456a2e08c
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-08 05:09:41 +0000

    Use a nasty hack to get access to x11.hidd semaphore.
    It seems that calls to X/GLX/GL API needs to be done exclusivelly, so
    hostgl will have to synchronize all its calls with x11.hidd
    
    This is just a quick "fix" for most frequent source of X11 lockups - in
    theory any gl function call issued during X call from another AROS process
    can cause lockup since AROS is just one thread to host X11/GLX and
    for a Linux thread should not be possible to do another X/GL call before
    its first one finishes.
    
    Proper solution to this problem needs to follow. The same problem will
    happen when more than AROS task tries using hostgl. On AROS it will be
    two processes, each with a separate GL context, however to Linux it will
    seem a the same thread (AROS) is trying to render with two "current" GL
    contexts at the same time which is not possible due to how OpenGL works.
    
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@41191 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/arch/all-hosted/libs/hostgl/hostgl_arosmesaswapbuffers.c
+++ AROS/arch/all-hosted/libs/hostgl/hostgl_arosmesaswapbuffers.c
@@ -5,10 +5,12 @@
 
 #include "hostgl_types.h"
 #include <proto/exec.h>
+#include <aros/debug.h>
 #if defined(RENDERER_PBUFFER_WPA)
 #include <proto/cybergraphics.h>
 #include <proto/graphics.h>
 #include <cybergraphx/cybergraphics.h>
+static struct SignalSemaphore * GetX11SemaphoreFromBitmap(struct BitMap * bm);
 #endif
 
 /*****************************************************************************
@@ -52,8 +54,11 @@
         LONG line = 0;
         LONG width = amesa->framebuffer->width;
         LONG height = amesa->framebuffer->height;
-        GLCALL(glFlush);
-        GLCALL(glReadPixels, 0, 0, width, height, GL_BGRA, GL_BYTE, amesa->swapbuffer);
+        
+        ObtainSemaphore(GetX11SemaphoreFromBitmap(amesa->visible_rp->BitMap));
+        GLCALL(glReadPixels, 0, 0, width, height, GL_BGRA, GL_UNSIGNED_BYTE, amesa->swapbuffer);
+        ReleaseSemaphore(GetX11SemaphoreFromBitmap(amesa->visible_rp->BitMap));
+
         /* Flip image */
         for (line = 0; line < height / 2; line++)
         {
@@ -62,8 +67,8 @@
             CopyMem(start, amesa->swapbufferline, width * 4);
             CopyMem(end, start, width * 4);
             CopyMem(amesa->swapbufferline, end, width * 4);
-
         }
+
         WritePixelArray(amesa->swapbuffer, 0, 0, width * 4, amesa->visible_rp, amesa->left, amesa->top, 
             width, height, RECTFMT_BGRA32);
 #endif
@@ -75,3 +80,58 @@
     AROS_LIBFUNC_EXIT
 }
 
+#if defined(RENDERER_PBUFFER_WPA)
+/* HACK: EVIL HACK TO GET ACCESS TO X11 HIDD INTERNALS */
+#include <proto/oop.h>
+#define HIDD_BM_OBJ(bitmap)     (*(OOP_Object **)&((bitmap)->Planes[0]))
+
+struct bitmap_data
+{
+    union
+    {
+        Window 	xwindow;
+        Pixmap  pixmap;
+    }               drawable;
+    Window          masterxwindow;    
+    Cursor          cursor;
+    unsigned long   sysplanemask;
+    Colormap        colmap;
+    GC              gc; /* !!! This is an X11 GC, NOT a HIDD gc */
+    Display         *display;
+    int             screen;
+    int             flags;    
+};
+
+struct fakefb_data
+{
+    OOP_Object *framebuffer;
+    OOP_Object *fakegfxhidd;
+};
+
+struct x11_staticdata
+{
+    /* These two members MUST be in the beginning of this structure
+       because they are exposed to disk-based part (see x11_class.h) */
+    UBYTE	   	     keycode2rawkey[256];
+    BOOL	   	     havetable;
+
+    struct SignalSemaphore   sema; /* Protecting this whole struct */
+    struct SignalSemaphore   x11sema;
+};
+
+struct x11clbase
+{
+    struct Library        library;
+    
+    struct x11_staticdata xsd;
+};
+
+#define XSD(cl)     	(&((struct x11clbase *)cl->UserData)->xsd)
+
+static struct SignalSemaphore * GetX11SemaphoreFromBitmap(struct BitMap * bm)
+{
+    OOP_Object * hiddbm = HIDD_BM_OBJ(bm);
+    struct fakefb_data * fakefb = (struct fakefb_data *)OOP_INST_DATA(OOP_OCLASS(hiddbm), hiddbm);
+    return (&XSD(OOP_OCLASS(fakefb->framebuffer))->x11sema);
+}
+#endif
