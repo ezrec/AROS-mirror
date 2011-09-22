c6bdc952f4609c1af4b4d3d465ae3fed75a6bd0b
verhaegs <verhaegs@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-09-22 19:41:56 +0000

    contrib/regina: link regina.library with arosc_rel and give it a peropener libbase
    git-svn-id: https://svn.aros.org/svn/aros/trunk@41565 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/regina/amilib.h
@@ -0,0 +1,31 @@
+#ifndef _AMILIB_H
+#define _AMILIB_H
+
+/*
+ *  Amiga REXX functions for regina
+ *  Copyright © 2002-2011, Staf Verhaegen
+ * 
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Library General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Library General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Library General Public
+ *  License along with this library; if not, write to the Free
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+#include <exec/libraries.h>
+
+struct ReginaBase
+{
+   struct Library _lib;
+   struct Library *_aroscbase;
+};
+
+#endif /* _AMILIB_H */
--- contrib/regina/regina_init.c
+++ contrib/regina/regina_init.c
@@ -12,6 +12,7 @@
 #include <aros/debug.h>
 #include <proto/exec.h>
 #include <proto/alib.h>
+#include <proto/dos.h>
 
 #include <stddef.h>
 #include "rexx.h"
@@ -19,23 +20,16 @@
 #include <aros/symbolsets.h>
 #include LC_LIBDEFS_FILE
 
-struct DosLibrary* DOSBase;
-struct Library* aroscbase;
 struct MinList *__regina_tsdlist = NULL;
 
+IPTR aroscbase_offset;
 
 static int InitLib(LIBBASETYPEPTR LIBBASE)
 {
    D(bug("Inside Init func of regina.library\n"));
 
-   if (!(DOSBase = (struct DosLibrary *)OpenLibrary("dos.library",0))) return FALSE;
-
-   if (!(aroscbase = OpenLibrary("arosc.library",41)))
-   {
-      CloseLibrary((struct Library *)DOSBase);
-      return FALSE;
-   }
-
+   aroscbase_offset = offsetof(LIBBASETYPE, _aroscbase);
+ 
    __regina_semaphorepool = CreatePool(MEMF_PUBLIC, 1024, 256);
   
    __regina_tsdlist = (struct MinList *)AllocPooled (__regina_semaphorepool, sizeof(struct MinList));
@@ -44,16 +38,36 @@ static int InitLib(LIBBASETYPEPTR LIBBASE)
    return TRUE;
 }
 
+static int OpenLib(LIBBASETYPEPTR LIBBASE)
+{
+   D(bug("Opening regina.library\n"));
+
+   LIBBASE->_aroscbase = OpenLibrary("arosc.library", 0);
+
+   D(bug("[regina.library::OpenLib] aroscbase=%p\n", LIBBASE->_aroscbase));
+
+   return LIBBASE->_aroscbase != NULL;
+}
+
+static int CloseLib(LIBBASETYPEPTR LIBBASE)
+{
+   D(bug("Closing regina.library\n"));
+
+   CloseLibrary(LIBBASE->_aroscbase);
+
+   return TRUE;
+}
+
 static int ExpungeLib(LIBBASETYPEPTR LIBBASE)
 {
    D(bug("Inside Expunge func of regina.library\n"));
 
    DeletePool(__regina_semaphorepool);
-   CloseLibrary(aroscbase);
-   CloseLibrary((struct Library *)DOSBase);
    
    return TRUE;
 }
 
 ADD2INITLIB(InitLib, 0);
+ADD2OPENLIB(OpenLib, 0);
+ADD2CLOSELIB(CloseLib, 0);
 ADD2EXPUNGELIB(ExpungeLib, 0);
