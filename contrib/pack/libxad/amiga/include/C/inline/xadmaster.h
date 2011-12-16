e1f6e1e4e1de42e2fedbb8446467f626fad7565d
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-16 22:25:50 +0000

    correction to lvo numbers
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@43001 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/pack/libxad/amiga/include/C/inline/xadmaster.h
+++ contrib/pack/libxad/amiga/include/C/inline/xadmaster.h
@@ -134,52 +134,52 @@
     	AROS_LC2(xadPTR, xadAllocObjectA, \
     		AROS_LCA(xadUINT32, type, D0), \
     		AROS_LCA(const struct TagItem *, tags, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x1e, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 5, xadmaster)
 
 #define xadFreeObjectA(object, tags) \
     	AROS_LC2NR(void, xadFreeObjectA, \
     		AROS_LCA(xadPTR, object, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x24, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 6, xadmaster)
 
 #define xadRecogFileA(size, memory, tags) \
     	AROS_LC3(struct xadClient *, xadRecogFileA, \
     		AROS_LCA(xadSize, size, D0), \
     		AROS_LCA(const void *, memory, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x2a, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 7, xadmaster)
 
 #define xadGetInfoA(ai, tags) \
     	AROS_LC2(xadERROR, xadGetInfoA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x30, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 8, xadmaster)
 
 #define xadFreeInfo(ai) \
     	AROS_LC1NR(void, xadFreeInfo, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x36, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 9, xadmaster)
 
 #define xadFileUnArcA(ai, tags) \
     	AROS_LC2(xadERROR, xadFileUnArcA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x3c, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 10, xadmaster)
 
 #define xadDiskUnArcA(ai, tags) \
     	AROS_LC2(xadERROR, xadDiskUnArcA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x42, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 11, xadmaster)
 
 #define xadGetErrorText(errnum) \
     	AROS_LC1(xadSTRPTR, xadGetErrorText, \
     		AROS_LCA(xadERROR, errnum, D0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x48, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 12, xadmaster)
 
 #define xadGetClientInfo() \
     	AROS_LC0(struct xadClient *, xadGetClientInfo, \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x4e, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 13, xadmaster)
 
 #define xadHookAccess(command, data, buffer, ai) \
     	AROS_LC4(xadERROR, xadHookAccess, \
@@ -187,12 +187,12 @@
     		AROS_LCA(xadSignSize, data, D1), \
     		AROS_LCA(xadPTR, buffer, A0), \
     		AROS_LCA(struct xadArchiveInfo *, ai, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x54, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 14, xadmaster)
 
 #define xadConvertDatesA(tags) \
     	AROS_LC1(xadERROR, xadConvertDatesA, \
     		AROS_LCA(const struct TagItem *, tags, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x5a, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 15, xadmaster)
 
 #define xadCalcCRC16(id, init, size, buffer) \
     	AROS_LC4(xadUINT16, xadCalcCRC16, \
@@ -200,7 +200,7 @@
     		AROS_LCA(xadUINT32, init, D1), \
     		AROS_LCA(xadSize, size, D2), \
     		AROS_LCA(const xadUINT8 *, buffer, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x60, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 16, xadmaster)
 
 #define xadCalcCRC32(id, init, size, buffer) \
     	AROS_LC4(xadUINT32, xadCalcCRC32, \
@@ -208,20 +208,20 @@
     		AROS_LCA(xadUINT32, init, D1), \
     		AROS_LCA(xadSize, size, D2), \
     		AROS_LCA(const xadUINT8 *, buffer, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x66, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 17, xadmaster)
 
 #define xadAllocVec(size, flags) \
     	AROS_LC2(xadPTR, xadAllocVec, \
     		AROS_LCA(xadSize, size, D0), \
     		AROS_LCA(xadUINT32, flags, D1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x6c, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 18, xadmaster)
 
 #define xadCopyMem(src, dest, size) \
     	AROS_LC3NR(void, xadCopyMem, \
     		AROS_LCA(const void *, src, A0), \
     		AROS_LCA(xadPTR, dest, A1), \
     		AROS_LCA(xadSize, size, D0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x72, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 19, xadmaster)
 
 #define xadHookTagAccessA(command, data, buffer, ai, tags) \
     	AROS_LC5(xadERROR, xadHookTagAccessA, \
@@ -230,44 +230,44 @@
     		AROS_LCA(xadPTR, buffer, A0), \
     		AROS_LCA(struct xadArchiveInfo *, ai, A1), \
     		AROS_LCA(const struct TagItem *, tags, A2), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x78, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 20, xadmaster)
 
 #define xadConvertProtectionA(tags) \
     	AROS_LC1(xadERROR, xadConvertProtectionA, \
     		AROS_LCA(const struct TagItem *, tags, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x7e, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 21, xadmaster)
 
 #define xadGetDiskInfoA(ai, tags) \
     	AROS_LC2(xadERROR, xadGetDiskInfoA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x84, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 22, xadmaster)
 
 #define xadGetHookAccessA(ai, tags) \
     	AROS_LC2(xadERROR, xadGetHookAccessA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x90, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 23, xadmaster)
 
 #define xadFreeHookAccessA(ai, tags) \
     	AROS_LC2NR(void, xadFreeHookAccessA, \
     		AROS_LCA(struct xadArchiveInfo *, ai, A0), \
     		AROS_LCA(const struct TagItem *, tags, A1), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x96, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 24, xadmaster)
 
 #define xadAddFileEntryA(fi, ai, tags) \
     	AROS_LC3(xadERROR, xadAddFileEntryA, \
     		AROS_LCA(struct xadFileInfo *, fi, A0), \
     		AROS_LCA(struct xadArchiveInfo *, ai, A1), \
     		AROS_LCA(const struct TagItem *, tags, A2), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0x9c, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 25, xadmaster)
 
 #define xadAddDiskEntryA(di, ai, tags) \
     	AROS_LC3(xadERROR, xadAddDiskEntryA, \
     		AROS_LCA(struct xadDiskInfo *, di, A0), \
     		AROS_LCA(struct xadArchiveInfo *, ai, A1), \
     		AROS_LCA(const struct TagItem *, tags, A2), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0xa2, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 26, xadmaster)
 
 #define xadGetFilenameA(buffersize, buffer, path, name, tags) \
     	AROS_LC5(xadERROR, xadGetFilenameA, \
@@ -276,22 +276,22 @@
     		AROS_LCA(const xadSTRING *, path, A1), \
     		AROS_LCA(const xadSTRING *, name, A2), \
     		AROS_LCA(const struct TagItem *, tags, A3), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0xa8, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 27, xadmaster)
 
 #define xadConvertNameA(charset, tags) \
     	AROS_LC2(xadSTRPTR, xadConvertNameA, \
     		AROS_LCA(xadUINT32, charset, D0), \
     		AROS_LCA(const struct TagItem *, tags, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0xae, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 28, xadmaster)
 
 #define xadGetDefaultNameA(tags) \
     	AROS_LC1(xadSTRPTR, xadGetDefaultNameA, \
     		AROS_LCA(const struct TagItem *, tags, A0), \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0xb4, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 29, xadmaster)
 
 #define xadGetSystemInfo() \
     	AROS_LC0(const struct xadSystemInfo *, xadGetSystemInfo, \
-    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 0xba, xadMaster)
+    		struct xadMasterBaseP *, XADMASTER_BASE_NAME, 30, xadmaster)
 #endif
 
 #ifndef NO_INLINE_STDARG
