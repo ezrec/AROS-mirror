f242b8c1a5cd011518a6192c094c5ac982afca42
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2011-12-28 20:02:17 +0000

    Exit draganddrop handling when target directory is NULL.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@43279 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/workbench/system/Wanderer/Classes/iconlist.c
+++ AROS/workbench/system/Wanderer/Classes/iconlist.c
@@ -6647,6 +6647,8 @@ IPTR IconList__MUIM_DragDrop(struct IClass *CLASS, Object *obj, struct MUIP_Drag
                     strcpy(dragDropEvent->drop_TargetPath, directory_path);
                     AddPart(dragDropEvent->drop_TargetPath, drop_target_node->ie_IconListEntry.label, fulllen);
                 }
+                else
+                    goto dragdropdone;
             }
             else
             {
