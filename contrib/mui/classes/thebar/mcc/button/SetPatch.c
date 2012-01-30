97c0f6da8d4416a8fbaae61229c2de16662b77ba
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-01-30 22:38:20 +0000

    Oops, forgot to commit these new files.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@43811 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ contrib/mui/classes/thebar/mcc/button/SetPatch.c
@@ -0,0 +1,48 @@
+/***************************************************************************
+
+ TheBar.mcc - Next Generation Toolbar MUI Custom Class
+ Copyright (C) 2003-2005 Alfonso Ranieri
+ Copyright (C) 2005-2009 by TheBar.mcc Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ TheBar class Support Site:  http://www.sf.net/projects/thebar
+
+ $Id$
+
+***************************************************************************/
+
+#if !defined(__amigaos4__) && !defined(__MORPHOS__) && !defined(__AROS__)
+#include <proto/exec.h>
+#include <exec/types.h>
+#include <exec/lists.h>
+#include <exec/semaphores.h>
+
+ULONG setPatchVersion;
+
+void GetSetPatchVersion(void)
+{
+  struct SetPatchSemaphore
+  {
+    struct SignalSemaphore semaphore;
+    struct MinList private;
+    UWORD version;
+    UWORD revision;
+  } *sem;
+
+  Forbid();
+  if((sem = (struct SetPatchSemaphore *)FindSemaphore((STRPTR)"« SetPatch »")) != NULL)
+  {
+    setPatchVersion = ((ULONG)sem->version << 16) | sem->revision;
+  }
+  Permit();
+}
+#endif
---
+++ contrib/mui/classes/thebar/mcc/button/SetPatch.h
@@ -0,0 +1,34 @@
+/***************************************************************************
+
+ TheBar.mcc - Next Generation Toolbar MUI Custom Class
+ Copyright (C) 2003-2005 Alfonso Ranieri
+ Copyright (C) 2005-2009 by TheBar.mcc Open Source Team
+
+ This library is free software; you can redistribute it and/or
+ modify it under the terms of the GNU Lesser General Public
+ License as published by the Free Software Foundation; either
+ version 2.1 of the License, or (at your option) any later version.
+
+ This library is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ Lesser General Public License for more details.
+
+ TheBar class Support Site:  http://www.sf.net/projects/thebar
+
+ $Id$
+
+***************************************************************************/
+
+#ifndef SETPATCH_H
+#define SETPATCH_H 1
+
+#if !defined(__amigaos4__) && !defined(__MORPHOS__) && !defined(__AROS__)
+extern ULONG setPatchVersion;
+
+void GetSetPatchVersion(void);
+#else
+#define GetSetPatchVersion() ((void)0)
+#endif
+
+#endif /* SETPATCH_H */
