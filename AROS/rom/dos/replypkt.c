1cb96000455600cae0f86a13161de38f58f15c96
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-03-19 23:17:26 +0000

    Backport of working ReplyPkt() from ABIv1.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@44436 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/rom/dos/replypkt.c
+++ AROS/rom/dos/replypkt.c
@@ -1,12 +1,16 @@
 /*
-    Copyright © 1995-2007, The AROS Development Team. All rights reserved.
+    Copyright © 1995-2012, The AROS Development Team. All rights reserved.
     $Id$
 
     Desc:
     Lang: english
 */
+
 #include <proto/exec.h>
+
 #include "dos_intern.h"
+
+void internal_ReplyPkt(struct DosPacket *dp, struct MsgPort *replyPort, SIPTR res1, LONG res2);
 
 /*****************************************************************************
 
@@ -18,7 +22,7 @@
 
 /*  SYNOPSIS */
 	AROS_LHA(struct DosPacket *, dp, D1),
-	AROS_LHA(LONG              , res1, D2),
+	AROS_LHA(SIPTR             , res1, D2),
 	AROS_LHA(LONG              , res2, D3),
 
 /*  LOCATION */
@@ -31,10 +35,6 @@
     RESULT
 
     NOTES
-      This function is mega-deprecated. Since all AROS drivers
-      use IORequests to perform operations the driver will most
-      probably not ever use or look at a packet. Also there might 
-      not even be a packet at all except for if SendPkt was used.
 
     EXAMPLE
 
@@ -50,22 +50,26 @@
 
     struct Process *me = (struct Process *)FindTask(NULL);
 
-    /* What should we do with res1? This function is not called by any
-       AROS functions, so I think we don't have to care. */
-    ((struct IOFileSys *)dp->dp_Arg7)->io_DosError = res2;
-
-    /*
-     * Just to be correct I write the res1/2 also into the packet
-     */
-    dp->dp_Res1 = res1;
-    dp->dp_Res2 = res2;
-
-    dp->dp_Port = &me->pr_MsgPort;
-    ((struct IOFileSys *)dp->dp_Arg7)->IOFS.io_Message.mn_ReplyPort = &me->pr_MsgPort;
-
-
-    /* I just love this casting... */
-    ReplyMsg(&((struct IOFileSys *)dp->dp_Arg7)->IOFS.io_Message);
+    internal_ReplyPkt(dp, &me->pr_MsgPort, res1, res2);
 
     AROS_LIBFUNC_EXIT
 } /* ReplyPkt */
+
+void internal_ReplyPkt(struct DosPacket *dp, struct MsgPort *replyPort, SIPTR res1, LONG res2)
+{
+    struct MsgPort *mp;
+    struct Message *mn;
+
+    mp = dp->dp_Port;
+    mn = dp->dp_Link;
+    mn->mn_Node.ln_Name = (char*)dp;
+    dp->dp_Port = replyPort;
+    dp->dp_Res1 = res1;
+    dp->dp_Res2 = res2;
+
+    /*
+     * Can't use ReplyMsg() here because mn_ReplyPort seems to be not set.
+     * Tested with SFS.
+     */
+    PutMsg(mp, mn);
+}
