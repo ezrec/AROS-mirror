40a14f3b96ce0dfe4c04af3b4cf49b1b21dbc363
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-04-03 09:22:53 +0000

    Merging lua-5.2.0 into the main branch.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@44506 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/development/compilers/lua/src/lfunc.h
+++ contrib/development/compilers/lua/src/lfunc.h
@@ -19,8 +19,8 @@
 
 
 LUAI_FUNC Proto *luaF_newproto (lua_State *L);
-LUAI_FUNC Closure *luaF_newCclosure (lua_State *L, int nelems, Table *e);
-LUAI_FUNC Closure *luaF_newLclosure (lua_State *L, int nelems, Table *e);
+LUAI_FUNC Closure *luaF_newCclosure (lua_State *L, int nelems);
+LUAI_FUNC Closure *luaF_newLclosure (lua_State *L, Proto *p);
 LUAI_FUNC UpVal *luaF_newupval (lua_State *L);
 LUAI_FUNC UpVal *luaF_findupval (lua_State *L, StkId level);
 LUAI_FUNC void luaF_close (lua_State *L, StkId level);
