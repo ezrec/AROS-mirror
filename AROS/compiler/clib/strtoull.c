682e6738638d708486875ccd4aed58a68b6f2dc3
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-05-26 09:45:04 +0000

    Bugfix for giving wrong results.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@44880 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/compiler/clib/strtoull.c
+++ AROS/compiler/clib/strtoull.c
@@ -146,7 +146,7 @@
             if (isdigit(digit))
                 digit -= '0';
             else if (isalpha(digit))
-                digit -= isupper(c) ? 'A' - 10 : 'a' - 10;
+                digit -= isupper(digit) ? 'A' - 10 : 'a' - 10;
             else
                 break;
 
