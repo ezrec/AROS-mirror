7c9d1bac8d39484b33789c3337c96b8ccbf5885a
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-01 13:04:57 +0000

    Timothy Deters fix for the problem with the rsfeed from archives.aros-exec.org.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45144 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/targets/www/rsfeed/archives.php
+++ documentation/targets/www/rsfeed/archives.php
@@ -1,13 +1,4 @@
-<?php
-
-$feed = file_get_contents('http://archives.aros-exec.org/modules/rssfeed.php');
-$rss = new SimpleXmlElement($feed);
-
-foreach($rss->channel->item as $entry) {
-echo "
-
-link' title='$entry->title'>" . $entry->title . "
-";
-}
-
-?>
+	<?php
+	require_once("rsslibar.php");
+	echo RSS_Display("http://archives.aros-exec.org/modules/rssfeed.php", 25);
+	?>
