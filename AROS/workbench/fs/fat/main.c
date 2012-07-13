91d284e12f1e99264e55cd698b7a80f375d679dc
neil <neil@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-07-13 01:37:32 +0000

    Backport of r45221: Speed-up finding next free cluster by starting the search from where the previous one was found. Makes writing a lot faster.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@45222 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/workbench/fs/fat/main.c
+++ AROS/workbench/fs/fat/main.c
@@ -2,7 +2,7 @@
  * fat.handler - FAT12/16/32 filesystem handler
  *
  * Copyright © 2006 Marek Szyprowski
- * Copyright © 2007-2011 The AROS Development Team
+ * Copyright © 2007-2012 The AROS Development Team
  *
  * This program is free software; you can redistribute it and/or modify it
  * under the same terms as AROS itself.
@@ -40,7 +40,7 @@ struct Library *IntuitionBase;
 struct Globals global_data;
 struct Globals *glob = &global_data;
 
-const TEXT version_string[] = "$VER: fat.handler 41.52 (28.12.2011)";
+const TEXT version_string[] = "$VER: fat.handler 41.53 (13.7.2012)";
 
 void handler(void) {
     struct Message *msg;
