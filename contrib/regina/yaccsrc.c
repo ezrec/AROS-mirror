2eb76fa1d28f089c4fd6af62dbc154d534cb6d27
jmcmullan <jmcmullan@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-08-19 04:51:07 +0000

    regina: compiler delint on linux-arm
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45593 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/regina/yaccsrc.c
+++ contrib/regina/yaccsrc.c
@@ -588,14 +588,22 @@ YYID (i)
 #   define YYMALLOC malloc
 #   if ! defined malloc && ! defined _STDLIB_H && (defined __STDC__ || defined __C99__FUNC__ \
      || defined __cplusplus || defined _MSC_VER)
+#ifdef __AROS__
+/* This came from malloc.h */
+#else
 void *malloc (YYSIZE_T); /* INFRINGES ON USER NAME SPACE */
+#endif
 #   endif
 #  endif
 #  ifndef YYFREE
 #   define YYFREE free
 #   if ! defined free && ! defined _STDLIB_H && (defined __STDC__ || defined __C99__FUNC__ \
      || defined __cplusplus || defined _MSC_VER)
+#ifdef __AROS__
+/* This came from malloc.h */
+#else
 void free (void *); /* INFRINGES ON USER NAME SPACE */
+#endif
 #   endif
 #  endif
 # endif
