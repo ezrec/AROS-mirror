0a0f5b62983a0b82e56c533aa06cc6eefb64038c
Krzysztof Smiechowicz <deadwood@wp.pl>
2012-09-27 09:30:48 +0200

    HostDisk: read-only partitions can also be reported as EROFS

--- AROS/arch/all-unix/devs/hostdisk/hostdisk_host.c
+++ AROS/arch/all-unix/devs/hostdisk/hostdisk_host.c
@@ -104,7 +104,7 @@ ULONG Host_Open(struct unit *Unit)
     if (err == EBUSY || err == EROFS)
     {
         /* This allows to work on Darwin, at least in read-only mode */
-        D(bug("hostdisk: EBUSY, retrying with read-only access\n", Unit->filename, Unit->file, err));
+        D(bug("hostdisk: EBUSY/EROFS, retrying with read-only access\n", Unit->filename, Unit->file, err));
         Unit->flags = UNIT_READONLY;
 
         Unit->file = hdskBase->iface->open(Unit->filename, O_RDONLY, 0755);
