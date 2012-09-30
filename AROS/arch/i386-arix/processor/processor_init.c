3ba643cc0c13c562fc5b4db55edb4ecad2124501
Michal Schulz <michal.schulz@gmx.de>
2012-09-30 23:53:26 +0200

    ARIX specific processor.resource

---
+++ AROS/arch/i386-arix/processor/processor_init.c
@@ -0,0 +1,39 @@
+/*
+    Copyright © 2010-2011, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+#include <aros/debug.h>
+
+#include <proto/exec.h>
+#include <proto/kernel.h>
+#include <aros/symbolsets.h>
+
+#include "processor_intern.h"
+#include "processor_arch_intern.h"
+
+LONG Processor_Init(struct ProcessorBase * ProcessorBase)
+{
+    struct X86ProcessorInformation **sysprocs;
+    unsigned int i;
+
+    D(bug("[processor.x86] :%s()\n", __PRETTY_FUNCTION__));
+
+    sysprocs = AllocVec(ProcessorBase->cpucount * sizeof(APTR), MEMF_ANY | MEMF_CLEAR);
+    if (sysprocs == NULL)
+        return FALSE;
+
+    for (i = 0; i < ProcessorBase->cpucount; i++)
+    {
+        sysprocs[i] = AllocMem(sizeof(struct X86ProcessorInformation), MEMF_CLEAR);
+        if (!sysprocs[i])
+            return FALSE;
+        ReadProcessorInformation(sysprocs[0]);
+    }
+
+    ProcessorBase->Private1 = sysprocs;
+
+    return TRUE;
+}
+
+ADD2INITLIB(Processor_Init, 1);
