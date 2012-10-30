c93c7cd431f08ce8f4b88109a5d9f9d6f708d593
desperado <desperado@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-10-30 16:33:12 +0000

    Swedish translation, provided by Stefan.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@45969 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ documentation/download-abstract.sv
@@ -0,0 +1,7 @@
+Distributioner
+==============
+
+Distributioner är konfigurerade och testade versioner av AROS.
+Dom innehåller en mängd användarprogram som inte finns med i dom vanliga AROS.org utgåvorna. Dessa distributioner riktar sig till främst till intresserade användare. Dessa har ev. inte dom senaste kärn uppdateringarna, men användar vänligheten och stabiliteten är betydligt större, än för dom nattliga utgåvorna. 
+
+Om du är en användare som är intresserad av AROS, så rekomenderar vi att du börjar med den senaste distributionen, för att få den bästa möjliga erfarenheten av AROS systemet.
