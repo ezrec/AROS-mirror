d156935b4e233085547dce2c4d0f587d2494ace7
deadwood <deadwood@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2012-11-22 20:06:38 +0000

    debug.library: Backport from ABI_V1
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@46081 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

---
+++ AROS/rom/debug/unregistermodule.c
@@ -0,0 +1,104 @@
+#include <aros/debug.h>
+#include <aros/kernel.h>
+#include <aros/libcall.h>
+#include <exec/lists.h>
+#include <proto/exec.h>
+
+#include <string.h>
+
+#include "debug_intern.h"
+
+/*****************************************************************************
+
+    NAME */
+#include <proto/debug.h>
+
+AROS_LH1(void, UnregisterModule,
+
+/*  SYNOPSIS */
+	AROS_LHA(BPTR, segList, A0),
+
+/*  LOCATION */
+	struct Library *, DebugBase, 6, Debug)
+
+/*  FUNCTION
+	Remove previously registered module from the debug information database
+
+    INPUTS
+	segList - DOS segment list for the module to remove
+
+    RESULT
+	None
+
+    NOTES
+	The function correctly supports partial removal of the module
+	(when an existing seglist is broken and only a part of the module
+	is unloaded).
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+******************************************************************************/
+{
+    AROS_LIBFUNC_INIT
+    
+    struct segment *seg;
+
+    D(bug("[Debug] UnregisterModule(0x%p)\n", segList));
+    ObtainSemaphore(&DBGBASE(DebugBase)->db_ModSem);
+
+    while (segList)
+    {
+	ForeachNode(&DBGBASE(DebugBase)->db_Modules, seg)
+	{
+	    if (seg->s_seg == segList)
+	    {
+		module_t *mod = seg->s_mod;
+
+	        D(bug("[Debug] Removing segment 0x%p\n", segList));
+		Remove((struct Node *)seg);
+
+		/* If module's segment count reached 0, remove the whole
+		   module information */
+		if (--mod->m_segcnt == 0)
+		{
+		    D(bug("[Debug] Removing module %s\n", mod->m_name));
+
+		    /* Free associated symbols */
+		    if (mod->m_symbols) {
+			D(bug("[Debug] Removing symbol table 0x%p\n", mod->m_symbols));
+			FreeVec(mod->m_symbols);
+		    }
+
+		    /* Free associated string tables */
+		    if (mod->m_str) {
+			D(bug("[Debug] Removing symbol name table 0x%p\n", mod->m_str));
+			FreeVec(mod->m_str);
+		    }
+		    if (mod->m_shstr) {
+			D(bug("[Debug] Removing section name table 0x%p\n", mod->m_str));
+			FreeVec(mod->m_shstr);
+		    }
+
+		    /* Free module descriptor at last */
+		    FreeVec(mod);
+		}
+
+		FreeMem(seg, sizeof(struct segment));
+
+		break;
+	    }
+	}
+	/* Advance to next DOS segment */
+	segList = *(BPTR *)BADDR(segList);
+    }
+
+    ReleaseSemaphore(&DBGBASE(DebugBase)->db_ModSem);
+
+    AROS_LIBFUNC_EXIT
+}
