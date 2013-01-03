e8ef4f2ce003060eafb40ef1c0d311125818b1da
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-01-03 18:32:20 +0000

    Backport from ABIv1:
    Bugfix. Resulting tags if "format" is not NULL must be
    
    TAG_IGNORE, value
    
    but not
    
    MUIA_List_Format, TAG_IGNORE.
    
    This fixes a crash in one of the Hollywood MUI examples.
    
    
    
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@46204 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/workbench/libs/muimaster/classes/volumelist.c
+++ AROS/workbench/libs/muimaster/classes/volumelist.c
@@ -132,9 +132,7 @@ IPTR Volumelist__OM_NEW(struct IClass *cl, Object *obj, struct opSet *msg)
     obj = (Object *)DoSuperNewTags
     (
         cl, obj, NULL,
-	MUIA_List_Format, format
-	    ? TAG_IGNORE
-	    : (IPTR)",,P=\33r,P=\33r,P=\33r",
+	format ? TAG_IGNORE : MUIA_List_Format, (IPTR)",,P=\33r,P=\33r,P=\33r",
         TAG_MORE, (IPTR) msg->ops_AttrList
     );
     
