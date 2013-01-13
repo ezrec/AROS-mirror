5ecc43e6c85ab1fdb6231cd37e765866cf168b8e
Krzysztof Smiechowicz <deadwood@wp.pl>
2013-01-13 15:21:09 +0100

    ARIX: hostdisk.device - use INT $80 on i386 to invoke clone.
    This solved slowdown when using clone with CLONE_VM.

--- AROS/arch/all-arix/devs/hostdisk/host_thread.h
+++ AROS/arch/all-arix/devs/hostdisk/host_thread.h
@@ -21,18 +21,18 @@ struct HostMMIO {
     ULONG   mmio_Size;
     int    mmio_File;
     int    mmio_Ret;
-    void * mmio_Task;
+    APTR    mmio_Task;
     int    mmio_Signal;
 };
 
 /* All information about and for the child unix thread */
 struct ThreadData {
-    int                         td_pid;        /* pid of the child */
-    char *                      td_stack;    /* stack of the child process */
-    void *                      td_irqHandler;    /* IRQ handler attached to the SIGUSR2 */
-    long                        td_stacksize;    /* Size of the stack */
-    struct HostInterface *      td_iface;    /* Host interface to do clib calls */
-    volatile struct HostMMIO *  td_mmio;    /* virtual MMIO interface */
+    int                         td_pid;         /* pid of the child */
+    BYTE *                      td_stack;       /* stack of the child process */
+    APTR                        td_irqHandler;  /* IRQ handler attached to the SIGUSR2 */
+    LONG                        td_stacksize;   /* Size of the stack */
+    struct HostInterface *      td_iface;       /* Host interface to do clib calls */
+    volatile struct HostMMIO *  td_mmio;        /* virtual MMIO interface */
 };
 
 int host_thread(struct ThreadData *td);
