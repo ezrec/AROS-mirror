49f65880aef8af56968408ff86c32afc70c1a847
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-03-26 14:01:45 +0000

    Backport from main trunk.
    git-svn-id: https://svn.aros.org/svn/aros/branches/ABI_V0@47083 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- AROS/test/runtests.c
+++ AROS/test/runtests.c
@@ -1,5 +1,53 @@
+/*
+    Copyright (C) 2013, The AROS Development Team. All rights reserved.
+    $Id$
+*/
+
+/******************************************************************************
+
+    NAME
+
+        runtests
+
+    SYNOPSIS
+
+        scriptname
+
+    LOCATION
+
+        SYS:Tests
+
+    FUNCTION
+
+        Executes all commands given by an input script. Reports memory loss
+        and summarizes the return codes. The commands shouldn't do any
+        output on success to avoid increasing of the shell's buffer.
+        The result is printed to the debugging console.
+        
+    INPUTS
+
+        scriptname -- script with the programs to be executed. Defaults to
+        "testscript".
+
+    RESULT
+
+    NOTES
+
+    EXAMPLE
+
+    BUGS
+
+    SEE ALSO
+
+    INTERNALS
+
+    HISTORY
+
+******************************************************************************/
+
 #include <aros/debug.h>
 #include <proto/dos.h>
+#include <proto/exec.h>
 
 BPTR scripthandle;
 CONST_STRPTR scriptname;
@@ -9,9 +57,19 @@ LONG failcnt;
 LONG errorcnt;
 LONG warncnt;
 LONG okcnt;
+LONG noshellcnt;
+LONG rubbishcnt;
+
+static ULONG checkmem(void)
+{
+    FreeVec(AllocVec((ULONG)(~0ul/2), MEMF_ANY)); // trigger expunges
+    return AvailMem(MEMF_ANY);
+}
 
 int main(int argc, char **argv)
 {
+    ULONG mem_old, mem;
+
     if (argc == 1)
     {
         scriptname = "testscript";
@@ -36,16 +94,29 @@ int main(int argc, char **argv)
     PutStr(scriptname);
     PutStr("\nOutput will be sent to the debugging console\n\n");
 
+    mem_old = checkmem();
+
     while (FGets(scripthandle, command, sizeof command))
     {
         if (command[0] != '#' && command[0] != '\n')
         {
             bug("====================================\n");
             bug("Running command: %s", command);
-            error = SystemTags(command, TAG_DONE);
+            error = SystemTagList(command, NULL);
             bug("returns: %d\n", error);
-            
-            if (error >= RETURN_FAIL)
+
+            mem = checkmem();
+            if (mem != mem_old)
+            {
+                bug("Memory loss %ul Bytes\n", mem_old - mem);
+                mem_old = mem;
+            }
+
+            if (error == -1)
+                noshellcnt++;
+            else if (error > 100 || error < 0)
+                rubbishcnt++;
+            else if (error >= RETURN_FAIL)
                 failcnt++;
             else if (error >= RETURN_ERROR)
                 errorcnt++;
@@ -56,7 +127,8 @@ int main(int argc, char **argv)
         }
     }
     bug("====================================\n");
-    bug("Summary: ok %d warn %d error %d fail %d\n", okcnt, warncnt, errorcnt, failcnt);
+    bug("Summary: ok %d, warn %d, error %d, fail %d, no Shell %d, rubbish %d\n",
+        okcnt, warncnt, errorcnt, failcnt, noshellcnt, rubbishcnt);
 
     Close(scripthandle);
 
