b5f47eae77b68a99fad44c09f908ca46ed72d549
Staf Verhaegen <staf@stafverhaegen.be>
2013-05-12 23:55:55 +0200

    [csplit] Additional preparation for proper replacement of arosc.library with split versions
     * Added rellib libraries in some module .conf file where arosc was already
       a rellib
     * Added -nostdc link option in addition to -noarosc for programs that
       don't want shared C library code.
     * Added stdc(io) to uselibs= of some modules where needed.
       Default is to not link modules with it but because these libraries
       are now peropenerbase they can be added to libraries when certain
       precautions are made.
     * Open stdc.library for some of the AHI drivers.
       Preparation for moving math and string functions into stdc.library.
       These drivers don't use autoopening of libraries.
     * Corrected usage of stdc name in comments in a few places where
       it krept into main trunk during previouw merging.

--- AROS/compiler/clib/__exec.c
+++ AROS/compiler/clib/__exec.c
@@ -1,5 +1,5 @@
 /*
-    Copyright © 2008-2012, The AROS Development Team. All rights reserved.
+    Copyright © 2008-2013, The AROS Development Team. All rights reserved.
     $Id$
 
     Support functions for POSIX exec*() functions.
@@ -57,7 +57,7 @@ APTR __exec_prepare(const char *filename, int searchpath, char *const argv[], ch
         goto error;
     }
 
-    /* Use own memory to allocate so that no arosstdc.library functions need to be called
+    /* Use own memory to allocate so that no stdc.library functions need to be called
        exec_pool can also be allocated in __exec_valist2array
     */
     if (!aroscbase->acb_exec_pool)
