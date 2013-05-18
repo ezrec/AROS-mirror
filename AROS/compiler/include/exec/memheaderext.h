babcb29ed4a6308dd1abc5f1ddffe57b39f22891
Michal Schulz <michal.schulz@gmx.de>
2013-05-18 18:25:34 +0200

    struct MemHeaderExt is extended in order to support custom managed memory pools

--- AROS/compiler/include/exec/memheaderext.h
+++ AROS/compiler/include/exec/memheaderext.h
@@ -20,15 +20,22 @@ struct MemHeaderExt
        region opaquely.  */
        
     APTR  mhe_UserData;
-    
-    APTR  (* mhe_Alloc)   (struct MemHeaderExt *, IPTR  size, ULONG *flags);
-    VOID  (* mhe_Free)    (struct MemHeaderExt *, APTR  mem,  IPTR   size);
-    APTR  (* mhe_AllocVec)(struct MemHeaderExt *, IPTR  size, ULONG *flags);
+
+    /* Pool initialization and destruction */
+    APTR  (* mhe_InitPool)(struct MemHeaderExt *, IPTR puddleSize, IPTR initialSize);
+    VOID  (* mhe_DestroyPool)(struct MemHeaderExt *);
+
+    /* Memory allocation functions */
+    APTR  (* mhe_Alloc)   (struct MemHeaderExt *, IPTR  size,  ULONG *flags);
+    VOID  (* mhe_Free)    (struct MemHeaderExt *, APTR  mem,   IPTR   size);
+    APTR  (* mhe_AllocVec)(struct MemHeaderExt *, IPTR  size,  ULONG *flags);
     VOID  (* mhe_FreeVec) (struct MemHeaderExt *, APTR  mem);
-    APTR  (* mhe_AllocAbs)(struct MemHeaderExt *, IPTR  size, APTR   addr);
-    APTR  (* mhe_ReAlloc) (struct MemHeaderExt *, APTR  old,  IPTR   size);
-    ULONG (* mhe_Avail)   (struct MemHeaderExt *, ULONG flags);
-    BOOL  (* mhe_InBounds)(struct MemHeaderExt *, APTR  mem);
+    APTR  (* mhe_AllocAbs)(struct MemHeaderExt *, IPTR  size,  APTR   addr);
+    APTR  (* mhe_ReAlloc) (struct MemHeaderExt *, APTR  old,   IPTR   size);
+
+    /* Query functions */
+    IPTR  (* mhe_Avail)   (struct MemHeaderExt *, ULONG flags);
+    BOOL  (* mhe_InBounds)(struct MemHeaderExt *, APTR  begin, APTR   end);
 };
 
 /* Indicates that the memory region is to be
