fad641c8a23f594ea45d4819dc02ce4a9d075c7a
Jason S. McMullan <jason.mcmullan@gmail.com>
2013-08-10 11:59:49 -0400

    AROSTCP: Use Exec/ScanTasks()
    Signed-off-by: Jason S. McMullan <jason.mcmullan@gmail.com>

--- AROS/workbench/network/stacks/AROSTCP/netlib/kill.c
+++ AROS/workbench/network/stacks/AROSTCP/netlib/kill.c
@@ -1,10 +1,34 @@
 #include <dos/dos.h>
 #include <exec/execbase.h>
 #include <proto/exec.h>
+#include <utility/hooks.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <errno.h>
 
+#if AROS_SMP
+/* Hook function to call in Exec/ScanTasks
+ */
+AROS_UFH3(IPTR, CheckTaskEntry,
+        AROS_UFHA(struct Hook *,    hook, A0),
+        AROS_UFHA(APTR,             object, A2),
+        AROS_UFHA(APTR,             message, A1))
+{
+    AROS_USERFUNC_INIT
+
+    pid_t pid = *(pid_t *)(hook->h_Data);
+    ULONG exec_sigs = *(ULONG *)message;
+
+    if ((pid_t)object == pid) {
+        Signal((struct Task *)object, exec_sigs);
+        return TRUE;
+    }
+
+    return FALSE;
+
+    AROS_USERFUNC_EXIT
+}
+#else
 static BOOL CheckTask(struct List *tl, pid_t pid)
 {
     struct Node *t;
@@ -13,6 +37,7 @@ static BOOL CheckTask(struct List *tl, pid_t pid)
 	    return TRUE;
     return FALSE;
 }
+#endif
 
 int kill(pid_t pid, int sigs)
 {
@@ -31,6 +56,16 @@ int kill(pid_t pid, int sigs)
 	return EINVAL;
     }
 
+#if AROS_SMP
+    if (exec_sigs) {
+        struct Hook hook = {
+            .h_Entry = (APTR)CheckTaskEntry,
+            .h_Data = &pid
+        };
+
+        task_valid = ScanTasks(&hook, &exec_sigs);
+    } while (0);
+#else
     Forbid();
 
     /*
@@ -47,6 +82,7 @@ int kill(pid_t pid, int sigs)
 	Signal((struct Task *)pid, exec_sigs);
 
     Permit();
+#endif
 
     return task_valid ? 0 : ESRCH;
 }
