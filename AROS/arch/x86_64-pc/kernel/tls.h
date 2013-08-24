b595c9f54acb5a240b332e64974276528862720b
Michal Schulz <michal.schulz@gmx.de>
2013-08-24 13:12:49 +0200

    small fix-ups and changes. TLS types are BYTE, LONG and QUAD now.

--- AROS/arch/x86_64-pc/kernel/tls.h
+++ AROS/arch/x86_64-pc/kernel/tls.h
@@ -4,8 +4,8 @@
 typedef struct tls
 {
     struct ExecBase     *SysBase;
-    void *              *KernelBase;    /* Base of kernel.resource */
-    void *              *CPUStorage;    /* Per-CPU private data */
+    void *              KernelBase;    /* Base of kernel.resource */
+    void **             CPUStorage;    /* Per-CPU private data */
 } tls_t;
 
 #define TLS_OFFSET(name) ((char *)&(((tls_t *)0)->name)-(char *)0)
@@ -15,9 +15,9 @@ typedef struct tls
         tls_t *__tls = (tls_t *)0; \
         typeof(__tls -> name) __ret; \
         switch (sizeof(__tls -> name)) { \
-case 2: asm volatile("movw %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
-case 4: asm volatile("movl %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
-case 8: asm volatile("movq %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memory"); break; \
+case 1: asm volatile("movb %%gs:%P1,%b0":"=q"(__ret):"0"(0),"i"(TLS_OFFSET(name)):"memory"); break; \
+case 4: asm volatile("movl %%gs:%P1,%0":"=r"(__ret):"i"(TLS_OFFSET(name)):"memory"); break; \
+case 8: asm volatile("movq %%gs:%P1,%q0":"=r"(__ret):"i"(TLS_OFFSET(name)):"memory"); break; \
         } \
         __ret;  \
     })
@@ -27,9 +27,9 @@ case 8: asm volatile("movq %%gs:%P1,%0":"=r"(__ret):"n"(TLS_OFFSET(name)):"memor
         tls_t *__tls = (tls_t *)0; \
         typeof(__tls -> name) __set = val; \
         switch (sizeof(__tls -> name)) { \
-case 2: asm volatile("movw %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
-case 4: asm volatile("movl %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
-case 8: asm volatile("movq %0,%%gs:%P1"::"r"(__set),"n"(TLS_OFFSET(name)):"memory"); break; \
+case 1: asm volatile("movb %b0,%%gs:%P1"::"iq"(__set),"i"(TLS_OFFSET(name)):"memory"); break; \
+case 4: asm volatile("movl %0,%%gs:%P1"::"ir"(__set),"i"(TLS_OFFSET(name)):"memory"); break; \
+case 8: asm volatile("movq %q0,%%gs:%P1"::"ir"(__set),"i"(TLS_OFFSET(name)):"memory"); break; \
         } \
     } while(0);
 
