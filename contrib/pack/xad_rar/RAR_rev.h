f3458b155c11a22b2775f18c759d135de632cd84
NicJA <NicJA@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2013-11-08 02:09:55 +0000

    oops missed a couple..
    git-svn-id: https://svn.aros.org/svn/aros/trunk@48392 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/pack/xad_rar/RAR_rev.h
+++ contrib/pack/xad_rar/RAR_rev.h
@@ -2,5 +2,5 @@
 #define REVISION	6
 #define DATE		"8.11.2013"
 #define VERS		"RAR 2.6"
-#define VSTRING		"RAR 2.5 (8.11.2013)\r\n"
-#define VERSTAG		"\0$VER: RAR 2.6 (5.2.2013)"
+#define VSTRING		"RAR 2.6 (8.11.2013)\r\n"
+#define VERSTAG		"\0$VER: RAR 2.6 (8.11.2013)"
