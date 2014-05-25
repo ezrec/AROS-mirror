47d764faa36e064ae7905b61871820609bf40ba5
Krzysztof Smiechowicz <deadwood@wp.pl>
2014-05-25 13:23:14 +0200

    ARIX: adjust arixloader to kernel organization changes
    ARCH_DIR is now passed as a parameter to linux kernel and init
    passes it to the bootloader.

--- AROS/arch/all-arix/arixloader/arixloader.c
+++ AROS/arch/all-arix/arixloader/arixloader.c
@@ -1,18 +1,22 @@
 #include <aros/symbolsets.h>
+#include <aros/bootloader.h>
 #include <proto/alib.h>
 #include <proto/exec.h>
 #include <proto/dos.h>
 #include <proto/utility.h>
+#include <proto/bootloader.h>
 
 #define DEBUG 0
 #include <aros/debug.h>
 
 #define TARGET_DIR  "EMU:boot-stage-2/"
 #define AROS_BOOT   "AROS.boot"
-#define AROSBS_CONF "boot/AROSBootstrap.conf"
-#define BUFFER_SIZE (256 * 1024)
+#define AROSBS_BIN  "AROSBootstrap"
+#define AROSBS_CONF "AROSBootstrap.conf"
+#define BUFFER_SIZE (64 * 1024)
+#define PATHBUFSIZE (128)
 
-static VOID createPath2(STRPTR volume, STRPTR filename, STRPTR buffer, LONG size)
+static VOID buildPath(CONST_STRPTR volume, CONST_STRPTR filename, STRPTR buffer, LONG size)
 {
     __sprintf(buffer, "%s", volume);
     AddPart(buffer, filename, size);
@@ -20,24 +24,37 @@ static VOID createPath2(STRPTR volume, STRPTR filename, STRPTR buffer, LONG size
 
 static STRPTR createPath(STRPTR volume, STRPTR filename)
 {
-    STRPTR _return = AllocVec(128, MEMF_ANY);
-    createPath2(volume, filename, _return, 128);
+    STRPTR _return = AllocVec(PATHBUFSIZE, MEMF_ANY);
+    buildPath(volume, filename, _return, PATHBUFSIZE);
     return _return;
 }
 
-static BOOL createDirIfNeeded(STRPTR dir)
+static BOOL createDirStructureIfNeeded(STRPTR dir)
 {
     BPTR l;
+    STRPTR p = dir;
 
-    l = Lock(dir, SHARED_LOCK);
-    if (l == BNULL)
+    while(*(++p) != ':');
+
+    while(TRUE)
     {
-        D(bug("Creating directory %s\n", dir));
-        l = CreateDir(dir);
+        while(*(++p) != '/')
+        {
+            if (*p == '\0') return TRUE;
+        }
+
+        *p = '\0';
+        l = Lock(dir, SHARED_LOCK);
+        if (l == BNULL)
+        {
+            D(bug("Creating directory %s\n", dir));
+            l = CreateDir(dir);
+        }
+        if (l == BNULL)
+            return FALSE;
+        UnLock(l);
+        *p = '/';
     }
-    if (l == BNULL)
-        return FALSE;
-    UnLock(l);
 
     return TRUE;
 }
@@ -63,8 +80,8 @@ static BOOL copyFile(STRPTR source, STRPTR destination)
 
     do
     {
-        read = FRead(src, buffer, 1, BUFFER_SIZE);
-        FWrite(dst, buffer, 1, read);
+        read = Read(src, buffer, BUFFER_SIZE);
+        Write(dst, buffer, read);
     } while(read == BUFFER_SIZE);
 
     FreeMem(buffer, BUFFER_SIZE);
@@ -79,50 +96,44 @@ static BOOL copyBootFiles(STRPTR * bootfiles)
     STRPTR file = NULL;
     STRPTR * p = bootfiles;
 
-    /* First, create needed directory */
-    createDirIfNeeded(TARGET_DIR);
-
     while((file = *p++) != NULL)
     {
-        STRPTR p= file;
-        TEXT dstpath[128];
+        STRPTR p = file;
+        TEXT dstpath[PATHBUFSIZE];
 
         while(*(p++) != ':');
 
-        createPath2(TARGET_DIR, p, dstpath, 128);
+        buildPath(TARGET_DIR, p, dstpath, PATHBUFSIZE);
 
-        /* Works only with one level deep directories */
-        p = dstpath;
-        while (*(++p) != '/');
-        while (*(++p) != '/');
-        *p = '\0';
-        createDirIfNeeded(dstpath);
-        *p = '/';
+        createDirStructureIfNeeded(dstpath);
 
         D(bug("Copying: %s to %s\n", file, dstpath));
 
         if (!copyFile(file, dstpath))
+        {
+            D(bug("Copying FAILED: %s to %s\n", file, dstpath));
             return FALSE;
+        }
 
     }
 
     return TRUE;
 }
 
-#define BUFSIZE (128)
-
-static STRPTR * findBootFiles(STRPTR volume)
+static STRPTR * findBootFiles(STRPTR volume, STRPTR archdir)
 {
     STRPTR * bootfiles = NULL;
     BPTR l; LONG i = 0;
-    TEXT buffer[BUFSIZE];
+    TEXT buffer[PATHBUFSIZE];
+    TEXT arosbsbinpath[PATHBUFSIZE];
+    TEXT arosbsconfpath[PATHBUFSIZE];
 
     /* Skip EMU */
     if (volume[0] == 'E' && volume[1] == 'M' && volume[2] == 'U')
         return NULL;
 
     /* Only if volume has AROS.boot */
-    createPath2(volume, AROS_BOOT, buffer, BUFSIZE);
+    buildPath(volume, AROS_BOOT, buffer, PATHBUFSIZE);
     l = Lock(buffer, SHARED_LOCK);
 
     if (l == BNULL)
@@ -131,7 +142,9 @@ static STRPTR * findBootFiles(STRPTR volume)
     UnLock(l);
 
     /* Only if volume has AROSBootstrap.conf */
-    createPath2(volume, AROSBS_CONF, buffer, BUFSIZE);
+    __sprintf(arosbsbinpath, "%s/%s", archdir, AROSBS_BIN);
+    __sprintf(arosbsconfpath, "%s/%s", archdir, AROSBS_CONF);
+    buildPath(volume, arosbsconfpath, buffer, PATHBUFSIZE);
     l = Open(buffer, MODE_OLDFILE);
 
     if (l == BNULL)
@@ -139,11 +152,11 @@ static STRPTR * findBootFiles(STRPTR volume)
 
     bootfiles = AllocVec(sizeof(STRPTR) * 64, MEMF_ANY | MEMF_CLEAR);
 
-    bootfiles[i++] = createPath(volume, "boot/AROSBootstrap");
-    bootfiles[i++] = createPath(volume, "boot/AROSBootstrap.conf");
+    bootfiles[i++] = createPath(volume, arosbsbinpath);
+    bootfiles[i++] = createPath(volume, arosbsconfpath);
 
     /* Naive parsing of configuration file in search of modules to copy */
-    while(FGets(l, buffer, BUFSIZE))
+    while(FGets(l, buffer, PATHBUFSIZE))
     {
         STRPTR p = buffer;
 
@@ -193,20 +206,44 @@ struct MyBootNode
 
 static int ARIXLoaderInit(APTR Base)
 {
-    struct List bootList;
-
     /*
      * 1. Scan available volumes in search of boot files
      * 2. If found, copy them (list taken from AROSBootstrap.conf) to initrd
      * 3. Exit
      */
 
+    struct List bootList;
+    STRPTR archdir = "";
     struct DosList *dl;
+    APTR BootLoaderBase;
 
     NEWLIST(&bootList);
 
     D(bug("ARIX Loader\n"));
 
+    /* Get information on arch directory */
+    BootLoaderBase = OpenResource("bootloader.resource");
+    if (BootLoaderBase)
+    {
+        struct Node * node;
+        struct List * list = (struct List *)GetBootInfo(BL_Args);
+        if (list)
+        {
+            ForeachNode(list, node)
+            {
+                if (strncmp(node->ln_Name,"archdir=", 8) == 0)
+                {
+                    archdir = &node->ln_Name[8];
+                    break;
+                }
+            }
+        }
+    }
+
+    D(bug("Found ARCH_DIR = %s\n", archdir));
+
+
+    /* Look for possible boot devices */
     dl = LockDosList(LDF_DEVICES | LDF_READ);
 
     while((dl = NextDosEntry(dl, LDF_DEVICES | LDF_READ)) != NULL)
@@ -228,7 +265,7 @@ static int ARIXLoaderInit(APTR Base)
 
             Enqueue(&bootList, (struct Node*)mbn);
 
-            D(bug("Found name %s\n", name));
+            D(bug("Found name %s\n", mbn->mbn_Name));
         }
     }
 
@@ -239,7 +276,7 @@ static int ARIXLoaderInit(APTR Base)
 
         ForeachNode(&bootList, mbn)
         {
-            if ((bootfiles = findBootFiles(mbn->mbn_Name)) != NULL)
+            if ((bootfiles = findBootFiles(mbn->mbn_Name, archdir)) != NULL)
             {
                 if (copyBootFiles(bootfiles))
                 {
