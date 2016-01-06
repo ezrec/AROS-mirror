17976dc15c485831a9e9f0884951c28f66306693
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-01-06 14:39:44 +0000

    Year 2016 added.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@51254 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/news/archive/index.cs
+++ documentation/news/archive/index.cs
@@ -5,6 +5,7 @@ Archiv zpráv
 Tady mù¾e¹ najít staré zpravodajské èlánky, které byly publikovány na tomto webu
 bìhem leto¹ního roku i døívìj¹ích let. K shlédnutí jsou zprávy z následujících let:
 
++ `2016 <2016>`__
 + `2015 <2015>`__
 + `2014 <2014>`__
 + `2012 <2012>`__
