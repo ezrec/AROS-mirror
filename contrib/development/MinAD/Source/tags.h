9776b03a8a7a0a22aadd6b8a63977b7a13580439
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2016-04-13 00:24:49 +0000

    Don't do an "or" operation of the return IDs with TAGBASE_NATMEG because that didn't work on x86_64.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@52625 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/development/MinAD/Source/tags.h
+++ contrib/development/MinAD/Source/tags.h
@@ -25,24 +25,24 @@
 #define TAGBASE_NATMEG									(TAG_USER | (MSERIAL_NATMEG << 16))
 
 /* Function tags */
-#define MUIV_ShowFunc									(TAGBASE_NATMEG | 0x0001)
-#define MUIV_DirRide										(TAGBASE_NATMEG | 0x0002)
-#define MUIV_UpdateListtree							(TAGBASE_NATMEG | 0x0003)
-#define MUIV_UpdateListtreeMode						(TAGBASE_NATMEG | 0x0004)
-#define MUIV_UpdateListtreeHitEnter					(TAGBASE_NATMEG | 0x0005)
-#define MUIV_Search										(TAGBASE_NATMEG | 0x0006)
-#define MUIV_SelectFromSearch							(TAGBASE_NATMEG | 0x0007)
-#define MUIV_Filter										(TAGBASE_NATMEG | 0x0008)
-#define MUIV_FilterClear								(TAGBASE_NATMEG | 0x0009)
-#define MUIV_OpenAll										(TAGBASE_NATMEG | 0x000A)
-#define MUIV_CloseAll									(TAGBASE_NATMEG | 0x000B)
+#define MUIV_ShowFunc									(0x0001)
+#define MUIV_DirRide										(0x0002)
+#define MUIV_UpdateListtree							(0x0003)
+#define MUIV_UpdateListtreeMode						(0x0004)
+#define MUIV_UpdateListtreeHitEnter					(0x0005)
+#define MUIV_Search										(0x0006)
+#define MUIV_SelectFromSearch							(0x0007)
+#define MUIV_Filter										(0x0008)
+#define MUIV_FilterClear								(0x0009)
+#define MUIV_OpenAll										(0x000A)
+#define MUIV_CloseAll									(0x000B)
 
 /* Menu tags */
-#define MUIV_mProjct_About								(TAGBASE_NATMEG | 0xF001)
-#define MUIV_mProjct_AboutMUI							(TAGBASE_NATMEG | 0xF002)
-#define MUIV_mProjct_Iconify							(TAGBASE_NATMEG | 0xF003)
-#define MUIV_mTools_Search								(TAGBASE_NATMEG | 0xF011)
-#define MUIV_mTools_MakeGuide							(TAGBASE_NATMEG | 0xF012)
+#define MUIV_mProjct_About								(0xF001)
+#define MUIV_mProjct_AboutMUI							(0xF002)
+#define MUIV_mProjct_Iconify							(0xF003)
+#define MUIV_mTools_Search								(0xF011)
+#define MUIV_mTools_MakeGuide							(0xF012)
 
 /*------------------------------------------------------------------------*/
 
