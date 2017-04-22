12e7daa95f192cc353c9a070803239b1482881fb
NicJA <NicJA@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-04-22 21:40:56 +0000

    don't print directory debug unless it is wanted.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54593 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/scalos/Modules/Delete.MUI/Source/Delete.module_files.c
+++ contrib/scalos/Modules/Delete.MUI/Source/Delete.module_files.c
@@ -759,9 +759,10 @@ LONG DirDelete(STRPTR DirName, BOOL Trash, APTR UndoStep)
 	BPTR              DirLock   = (BPTR)NULL;
 	IPTR              Confirm   = 0;
 
-	d1(kprintf("%s()/%ld: Trying to delete %s\n", __FUNCTION__, __LINE__, DirName);)
-
-	DEBUG_CURRENTDIR;
+	d1(
+            kprintf("%s()/%ld: Trying to delete %s\n", __FUNCTION__, __LINE__, DirName);
+            DEBUG_CURRENTDIR;
+        )
 
 	snprintf(TextBuffer, sizeof(TextBuffer),
 		GetLocString(MSG_BODY_REMOVE), FilePart(DirName));
@@ -829,7 +830,7 @@ LONG DirDelete(STRPTR DirName, BOOL Trash, APTR UndoStep)
 
 		OldDir = CurrentDir(DirLock);
 
-		DEBUG_CURRENTDIR;
+		d1(DEBUG_CURRENTDIR;)
 
 		if(Examine(DirLock, ExFIB))
 			{
@@ -856,14 +857,15 @@ LONG DirDelete(STRPTR DirName, BOOL Trash, APTR UndoStep)
 					}
 				}
 
-			d1(kprintf("%s()/%ld: Protection stage complete, checking contents\n", __FUNCTION__, __LINE__);)
-
-			DEBUG_CURRENTDIR;
+			d1(
+                            kprintf("%s()/%ld: Protection stage complete, checking contents\n", __FUNCTION__, __LINE__);
+                            DEBUG_CURRENTDIR;
+                        )
 
 			if(ExNext(DirLock, ExFIB))
 				{
 				do 	{
-					DEBUG_CURRENTDIR;
+					d1(DEBUG_CURRENTDIR;)
 
 			                DoMethod(MUI_App, MUIM_Application_InputBuffered);
 
@@ -871,8 +873,10 @@ LONG DirDelete(STRPTR DirName, BOOL Trash, APTR UndoStep)
 					OldFIB = *ExFIB;
 					ExNextRtn = ExNext(DirLock, ExFIB);
 
-					DEBUG_CURRENTDIR;
-					d1(kprintf("%s()/%ld: fib_DirEntryType=%ld\n", __FUNCTION__, __LINE__, OldFIB.fib_DirEntryType);)
+					d1(
+                                            DEBUG_CURRENTDIR;
+                                            kprintf("%s()/%ld: fib_DirEntryType=%ld\n", __FUNCTION__, __LINE__, OldFIB.fib_DirEntryType);
+                                        )
 
 					if (ST_SOFTLINK == OldFIB.fib_DirEntryType)
 					{
@@ -990,7 +994,7 @@ LONG DirDelete(STRPTR DirName, BOOL Trash, APTR UndoStep)
 	if(Trash)
 		FileBuffer[Position] = '\0';
 
-	DEBUG_CURRENTDIR;
+	d1(DEBUG_CURRENTDIR;)
 
 	if(Result == RESULT_HALT)
 		return(RESULT_HALT);
