403221982d1f56c451a1d14b658ee66ec5383983
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-05-02 01:39:20 +0000

    Some validator errors removed: * deleted wrong </tbody> * replaced deprecated background attribute by css style * added missing alt attribute
    Commented-out block removed.
    
    
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54636 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- documentation/targets/www/rsfeed/synfeed.php
+++ documentation/targets/www/rsfeed/synfeed.php
@@ -1,9 +1,9 @@
 <?php
-echo "<img src=\"/images/rsfeed.gif\"><a href 
+echo "<img src=\"/images/rsfeed.gif\" alt=\"rsfeed logo\"><a href 
 =\"http://archives.aros-exec.org/modules/rssfeed.php\">Archives - 
 Recent submissions</a>";
 echo "<br>";
-echo "<img src=\"/images/rsfeed.gif\"><a href 
+echo "<img src=\"/images/rsfeed.gif\" alt=\"rsfeed logo\"><a href 
 =\"http://aros-exec.org/modules/newbb/rss.php?c=0\">AROS-Exec 
 Forum</a>";
 ?>
