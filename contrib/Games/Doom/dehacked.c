f051fb79d32b5a80541044a05edbefb81f1a7b04
mazze <mazze@fb15a70f-31f2-0310-bbcc-cdcc74a49acc>
2017-08-09 15:41:20 +0000

    Fixed GCC 6 error about misleading indentation.
    git-svn-id: https://svn.aros.org/svn/aros/trunk@54846 fb15a70f-31f2-0310-bbcc-cdcc74a49acc

--- contrib/Games/Doom/dehacked.c
+++ contrib/Games/Doom/dehacked.c
@@ -311,36 +311,36 @@ void DE_Text(FILE* f,int len1,int len2)
   if(len1>6)
     return;
 
-    if(fread(s,len1+len2,1,f))
-    {
-      for(i=0;i<108;i++)
-        if(!strncmp(S_sfx[i].name,s,len1))
-        {
-          strncpy(S_sfx[i].name,&(s[len1]),len2);
-          return;
-        }
-      for(i=0;i<NUMSPRITES;i++)
-        if(!strncmp(sprnames[i],s,len1))
-        {
-          strncpy(sprnames[i],&(s[len1]),len2);
-          return;
-        }
-      for(i=0;i<NUMSPRITES;i++)
-        if(!strncmp(sprnames[i],s,len1))
-        {
-          strncpy(sprnames[i],&(s[len1]),len2);
-          return;
-        }
-      for(i=1;i<68;i++)
-        if(!strncmp(S_music[i].name,s,len1))
-        {
-          strncpy(S_music[i].name,&(s[len1]),len2);
-          return;
-        }
+  if(fread(s,len1+len2,1,f))
+  {
+    for(i=0;i<108;i++)
+      if(!strncmp(S_sfx[i].name,s,len1))
+      {
+        strncpy(S_sfx[i].name,&(s[len1]),len2);
+        return;
+      }
+    for(i=0;i<NUMSPRITES;i++)
+      if(!strncmp(sprnames[i],s,len1))
+      {
+        strncpy(sprnames[i],&(s[len1]),len2);
+        return;
+      }
+    for(i=0;i<NUMSPRITES;i++)
+      if(!strncmp(sprnames[i],s,len1))
+      {
+        strncpy(sprnames[i],&(s[len1]),len2);
+        return;
+      }
+    for(i=1;i<68;i++)
+      if(!strncmp(S_music[i].name,s,len1))
+      {
+        strncpy(S_music[i].name,&(s[len1]),len2);
+        return;
+      }
       /* more text to follow */
 
 
-    }
+  }
 }
 
 
